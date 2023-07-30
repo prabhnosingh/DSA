5aaaffc4f452001211736f0b69d33c6057a010a8
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-07-30 01:28:56 -0400

    Time: 2 ms (99.49%) | Memory: 45 MB (20.92%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,90 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        double median = 0.0;
+        int mid = 0;
+
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int len = len1 + len2;
+
+        int[] nums = new int[len];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        if(len2 == 0){
+            mid = len1/2;
+            if(mid !=0 ){
+                if( mid == 1 || len1 % 2 == 0){
+                  median = (double)(nums1[mid] + nums1[mid - 1]) / 2;
+             }
+            else{
+                 median = nums1[mid];
+                }
+                return median;
+              }
+              else{
+                  return nums1[mid];
+              }
+        }
+        else if(len1 == 0){
+             mid = len2/2;
+            if(mid !=0){
+                if(mid == 1 || len2 % 2 == 0){
+                    median = (double)(nums2[mid] + nums2[mid - 1]) / 2;
+             }
+                else{
+                median = nums2[mid];
+             }
+            return median;
+            }
+            else{
+                return nums2[mid];
+            }
+            
+        }
+
+
+
+
+        while(k < len){
+
+            if(i < len1 && j < len2){
+
+                if(nums1[i] < nums2[j]){
+                    nums[k++] = nums1[i++];
+                      
+                }
+                else{
+                    nums[k++] = nums2[j++];
+                    }   
+            }
+            else if(i >= len1 && j < len2){
+                nums[k++] = nums2[j++];
+
+            }
+            else if(i < len1 && j >= len2){
+                nums[k++] = nums1[i++];
+            }
+        }
+
+
+        mid = len/2;
+        if(mid != 0){
+        if(len % 2 == 0){
+            median = (float)(nums[mid] + nums[mid-1])/2;  
+            // median = nums[];
+        }
+        else{
+            median = nums[mid];
+        }
+
+        return median;
+    }
+    else{
+        return nums[mid];
+    }
+    }
+}
+
