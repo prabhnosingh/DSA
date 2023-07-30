94f5d7a3eef479332a92710a7df0457fe6562bc0
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-07-30 01:36:50 -0400

    Time: 2 ms (99.49%) | Memory: 45 MB (20.92%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -13,41 +13,6 @@ class Solution {
         int j = 0;
         int k = 0;
 
-        // if(len2 == 0){
-        //     mid = len1/2;
-        //     if(mid !=0 ){
-        //         if( mid == 1 || len1 % 2 == 0){
-        //           median = (double)(nums1[mid] + nums1[mid - 1]) / 2;
-        //      }
-        //     else{
-        //          median = nums1[mid];
-        //         }
-        //         return median;
-        //       }
-        //       else{
-        //           return nums1[mid];
-        //       }
-        // }
-        // else if(len1 == 0){
-        //      mid = len2/2;
-        //     if(mid !=0){
-        //         if(mid == 1 || len2 % 2 == 0){
-        //             median = (double)(nums2[mid] + nums2[mid - 1]) / 2;
-        //      }
-        //         else{
-        //         median = nums2[mid];
-        //      }
-        //     return median;
-        //     }
-        //     else{
-        //         return nums2[mid];
-        //     }
-            
-        // }
-
-
-
-
         while(k < len){
 
             if(i < len1 && j < len2){
@@ -84,32 +49,4 @@ class Solution {
     }
 }
 
-// class Solution {
-//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-//         int n1 = nums1.length;
-//         int n2 = nums2.length;
-//         int n = n1 + n2;
-//         int[] new_arr = new int[n];
 
-//         int i=0, j=0, k=0;
-
-//         while (i<=n1 && j<=n2) {
-//             if (i == n1) {
-//                 while(j<n2) new_arr[k++] = nums2[j++];
-//                 break;
-//             } else if (j == n2) {
-//                 while (i<n1) new_arr[k++] = nums1[i++];
-//                 break;
-//             }
-
-//             if (nums1[i] < nums2[j]) {
-//                 new_arr[k++] = nums1[i++];
-//             } else {
-//                 new_arr[k++] = nums2[j++];
-//             }
-//         }
-
-//         if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-//         else return new_arr[n/2];
-//     }
-// }
