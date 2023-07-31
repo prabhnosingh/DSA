cddbfa51c830ea0d7c4377be8a078d4df32803c6
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-07-30 20:03:34 -0400

    Time: 13 ms (54.31%) | Memory: 55.5 MB (52.70%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,35 +1,36 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
     
-        // HashSet<Integer> set = new HashSet<>();
-        // for(int i = 0; i < nums.length; i++){
-        //     if(set.contains(nums[i])){
-        //       return true;
-        //     }
-        //     else{
-        //         set.add(nums[i]);
-        //     }
-        // }
-        // return false;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        HashSet<Integer> set1= new HashSet<>();
-        for(int i=0; i< nums.length; i++){
-        int num = nums[i];
-                if(set1.contains(num)){
-                    return true;
-                
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int count = map.getOrDefault(nums[i], 0)+1;
+            map.put(nums[i], count);
+            if(count > 1){
+                return true;
             }
-            set1.add(num);
+            
+            
         }
         return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         HashSet<Integer> set1= new HashSet<>();
+//         for(int i=0; i< nums.length; i++){
+       
+//                 if(set1.contains(nums[i])){
+//                     return true;
+                
+//             }
+//             set1.add(nums[i]);
+//         }
+//         return false;
     }
 }
