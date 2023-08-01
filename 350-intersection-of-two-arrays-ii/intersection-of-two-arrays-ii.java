fd071c051c2acce1b9197992342ff2ecdd73a578
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-01 17:55:46 -0400

    Time: 1 ms (98.52%) | Memory: 43.3 MB (52.51%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -23,49 +23,7 @@ class Solution {
         
         return Arrays.copyOfRange(result, 0, idx);
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int len1 = nums1.length;
-        // int len2 = nums2.length;
-        // ArrayList<Integer> arr = new ArrayList<>();
-        
-    
-        
-//             for(int i = 0; i < len1; i++){
-//                 int count = 0;
-//                 int num = -1;
-//                 for(int j = 0; j < len2; j++){
-//                     if(nums1[i] == nums2[j]){
-//                         num = nums1[i];
-//                         count ++;
-//                     }
-                    
-//                 }
-//                 if(count > 0 && num != -1){
-//                         arr.add(num);
-//                     }
-//             }
-//   int[] arrFin = arr.stream().mapToInt(i -> i).toArray();
-       
-//         return arrFin;
+
         
     }
 }
