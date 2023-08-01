44f949d02594284240c58de05c1117907d0eaab1
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-01 11:24:43 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (99.59%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,25 +1,35 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 0; i < nums1.length; i++){
-            set.add(nums1[i]);
-        }
+        // HashSet<Integer> set = new HashSet<>();
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // for(int i = 0; i < nums1.length; i++){
+        //     set.add(nums1[i]);
+        // }
 
-        for(int j = 0; j < nums2.length; j++){
-            if(set.contains(nums2[j])){
-                arr.add(nums2[j]);
-                set.remove(nums2[j]);
+        // for(int j = 0; j < nums2.length; j++){
+        //     if(set.contains(nums2[j])){
+        //         arr.add(nums2[j]);
+        //         set.remove(nums2[j]);
+        //     }
+        // }
+
+        // int[] arrFin = new int[arr.size()];
+        // for(int k = 0; k < arr.size(); k++){
+        //     arrFin[k] = arr.get(k);
+        // }
+       
+        // return arrFin;
+boolean[] exist = new boolean[1001];
+        for(int v : nums1) exist[v] = true;
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+        int idx = 0;
+        for(int v : nums2) {
+            if(exist[v]) {
+                result[idx++] = v;
+                exist[v] = false;
             }
         }
-
-        int[] arrFin = new int[arr.size()];
-        for(int k = 0; k < arr.size(); k++){
-            arrFin[k] = arr.get(k);
-        }
-       
-        return arrFin;
-
+        return Arrays.copyOfRange(result, 0, idx);
         
     }
 }
