b6686d7d3405cf08426d9cf417c6e1e3e6b6d0d6
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-04 19:56:37 -0400

    Time: 1 ms (99.98%) | Memory: 44.5 MB (53.56%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int len = nums.length;
+        int goalPost = len - 1;
+
+        for(int i = len - 2; i >= 0; i--){
+
+            if(nums[i] >= goalPost - i){
+                goalPost = i;
+            }
+        }
+
+        if(goalPost == 0){
+            return true;
+        }
+        return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     if(nums.length == 1 ){
+    //         return true;
+    //     }
+    //     for(int i = 0; i< nums.length; i++){
+            
+    //         if(i == 0){
+    //             n[i] = nums[i];
+
+    //         }
+    //         else{
+    //             n[i] = nums[i] + i;
+    //         }
+
+    //   }
+    //   for(int ans : n){
+    //       if(ans == nums.length - 1){
+    //           return true;
+    //       }
+
+    //   }
+    //   return false;
+
+
+    }
+}
