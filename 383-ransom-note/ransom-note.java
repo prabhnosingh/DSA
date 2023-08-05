402e323fc1e5b684b08dec8c4f8c72b6fd3c98c4
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-05 10:33:56 -0400

    Time: 2 ms (98.56%) | Memory: 43.8 MB (69.03%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {   
 
+        if(ransomNote.length() > magazine.length()) return false;
         int[] count = new int[26];
 
         for(char c : magazine.toCharArray()){
