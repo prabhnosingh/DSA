92f8bbc07b519ef18b83142297e7ab6f69826ec0
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-05 01:49:09 -0400

    Time: 17 ms (50.05%) | Memory: 43.9 MB (61.63%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        char[] mag = magazine.toCharArray();
+        char[] ran = ransomNote.toCharArray();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char m : mag){
+            map.put(m, map.getOrDefault(m, 0)+1);
+        }
+
+        int i = 0;
+        // for(Map.Entry<Character, Integer> ent : map.entrySet()){
+        //     if(ran[i] <= ent.get)
+        // }
+        for(char r : ran){
+
+            if(map.get(r) ==  null ){
+                return false;
+            }
+            if(map.get(r) > 0){
+                map.put(r, map.get(r)-1);
+            }
+
+            else if(map.get(r) <= 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
