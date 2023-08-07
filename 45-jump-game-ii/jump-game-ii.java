6dc59bc5c6044f10640b03cb9c5ab0b0a27ac5a0
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-06 20:51:06 -0400

    Time: 1 ms (99.21%) | Memory: 44.9 MB (20.10%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,44 +1,44 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int jumps = 0;
-        int farthest = 0;
+        // int jumps = 0;
+        // int farthest = 0;
 
-        int end = 0;
-        for(int i = 0; i < nums.length - 1; i ++){
-            farthest = Math.max(farthest, i + nums[i]);
+        // int end = 0;
+        // for(int i = 0; i < nums.length - 1; i ++){
+        //     farthest = Math.max(farthest, i + nums[i]);
 
-            if(farthest >= nums.length - 1){
-                jumps ++;
-                break;
-            }
-            if(i == end){
-                jumps ++;
-                end = farthest;
-            }
-        }
-        return jumps;
+        //     if(farthest >= nums.length - 1){
+        //         jumps ++;
+        //         break;
+        //     }
+        //     if(i == end){
+        //         jumps ++;
+        //         end = farthest;
+        //     }
+        // }
+        // return jumps;
 
 
     //******************************************************************* */   
-        // int jumps = 0;
-        // int left = 0; 
-        // int right = 0;
+        int jumps = 0;
+        int left = 0; 
+        int right = 0;
             
-        //     while(right < nums.length - 1 ){
-        //         int farthest = 0;
+            while(right < nums.length - 1 ){
+                int farthest = 0;
 
-        //         for(int i = left; i < right + 1; i++){
-        //             farthest = Math.max(farthest, i + nums[i]);
+                for(int i = left; i < right + 1; i++){
+                    farthest = Math.max(farthest, i + nums[i]);
 
-        //         }
+                }
                 
-        //         left = right + 1;
-        //         right = farthest;
-        //         jumps ++;
-        //     }
+                left = right + 1;
+                right = farthest;
+                jumps ++;
+            }
 
         
-        // return jumps;
+        return jumps;
     }
 }
