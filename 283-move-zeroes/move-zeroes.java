41973e641696e235e372a138ab3b72457e3a1051
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-08 22:08:12 -0400

    Time: 3 ms (19.47%) | Memory: 45.3 MB (25.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,38 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
-//         int[] nums2 = new int[nums.length];
-        
-//         int idx = 0;
-//         for(int num : nums){
-//             if(num != 0){
-//                 nums2[idx ++] = num;
-//             }
-//         }
-        
-//         nums = Arrays.copyOfRange(nums2, 0, nums.length);
-        
-        
-        
-        
-        
-        
-        int pointer = 0;
-        for(int i = 0; i < nums.length; i++){
-            
-            if(nums[pointer] == 0 && nums[i] != 0){
-            
-                nums[pointer] = nums[i];
-                nums[i] = 0;
-                pointer ++;
-                 
-    
+        int first =0, second= 1;
+        while(second<nums.length){
+            if(nums[first]==0 && nums[second]!=0){
+                nums[first]= nums[second];
+                nums[second]= 0;
+                first++;
+                second++;
+
+            }            
+            else if(nums[second]==0 && nums[first]!=0){
+                first++;
+                second++;
             }
-            else if(nums[pointer] != 0){
-                pointer ++;                
+            else if(nums[first]==0 && nums[second]==0){
+                second++;
             }
-            
-           
+            else{
+                first++;
+                second++;
+            }
         }
+        
     }
 }
