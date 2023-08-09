5fc385225457f0609dd013e027e9f1789c476539
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-08 21:24:55 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (82.38%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,29 +1,32 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-        int[] exist1 = new int[1001];
-        int[] exist2 = new int[1001];
+        int[] exist = new int[1001];
+       
         
-        for(int i = 0; i < nums1.length; i++) exist1[nums1[i]]++;
-        for(int i = 0; i < nums2.length; i++) exist2[nums2[i]]++;
         
-        int[] result = new int[Math.max(nums1.length, nums2.length)];
         
-        int idx = 0;
         
-        for(int i = 0; i < nums2.length; i++){
-            if(exist1[nums2[i]] > 0 && exist2[nums2[i]] > 0) {
-               
-                result[idx++] = nums2[i]; 
-                exist1[nums2[i]] --;
-                exist2[nums2[i]] --;
-                
-            }
+        for(int num : nums1){
+            exist[num] ++;
         }
         
-        return Arrays.copyOfRange(result, 0, idx);
+       
         
-
+        int idx = 0;
+        int[] res = new int[Math.max(nums1.length, nums2.length)];
+        for(int num : nums2){
+          
+              if(exist[num] > 0){
+                  res[idx++] = num;
+                  exist[num] --;
+              }
+         
+           }
+        
+        
+        return Arrays.copyOfRange(res, 0, idx);
+        
         
     }
 }
