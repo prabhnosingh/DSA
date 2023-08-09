6f37ece5a9d7ec21f8ff241663bed79d1f83ce72
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-09 00:47:56 -0400

    Time: 1 ms (78.10%) | Memory: 40.4 MB (96.56%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,21 +1,27 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
-        String merged = "";
+StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return result.toString();        
+        // String merged = "";
 
-        // int p1 = 0;
-        // int p2 = 0;
 
-        // while(p1 < word1.length || p2 < word.length){
-        //     if(p1 )
+
+        // int len = Math.max(word1.length(), word2.length());
+
+        // for(int i = 0; i < len; i++){
+        //     merged = merged + (i >= word1.length() ? "" : word1.charAt(i)) + (i >= word2.length() ? "" : word2.charAt(i));
         // }
 
-        int len = Math.max(word1.length(), word2.length());
-
-        for(int i = 0; i < len; i++){
-            merged = merged + (i >= word1.length() ? "" : word1.charAt(i)) + (i >= word2.length() ? "" : word2.charAt(i));
-        }
-
-        return merged;
+        // return merged;
     }
 }
