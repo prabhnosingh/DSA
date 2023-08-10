5f84b4f7cfa82c750a0848715aee20fd78fd6c8c
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-10 12:46:09 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (96.40%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,21 +1,55 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        
-        int len = cost.length;
-        int[] dp = new int[len];
 
-        dp[len - 1] = cost[len - 1];
-        dp[len - 2] = Math.min(cost[len - 2], cost[len - 2] + cost[len - 1]);
-        
-        for(int i = len - 3; i >= 0; i--){
-            dp[i] = Math.min(cost[i]+ dp[i+1], cost[i] + dp[i + 2]);
+        int len = cost.length;
+        int first = cost[len-1];
+        int second = Math.min(cost[len - 2], first + cost[len - 2]); 
+        int i = len - 3;
+
+        while(i >=0){
+            int temp = second;
+            second = Math.min(cost[i] + first, cost[i] + second);
+            first = temp;
+            i --;
+
         }
 
-        int ans = dp[0];
-        if(dp[1] < dp[0]) ans = dp[1];
-        // for(int i = 0; i < len; i++){
-        //     if(ans > dp[i]) ans = dp[i];
+        return Math.min(first, second);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+//***************************************************************************************** */        
+        // int len = cost.length;
+        // int[] dp = new int[len];
+
+        // dp[len - 1] = cost[len - 1];
+        // dp[len - 2] = Math.min(cost[len - 2], cost[len - 2] + cost[len - 1]);
+        
+        // for(int i = len - 3; i >= 0; i--){
+        //     dp[i] = Math.min(cost[i] + dp[i+1], cost[i] + dp[i + 2]);
         // }
-        return ans;
+
+        // int ans = dp[0];
+        // if(dp[1] < dp[0]) ans = dp[1];
+        
+        // return ans;
     }
 }
