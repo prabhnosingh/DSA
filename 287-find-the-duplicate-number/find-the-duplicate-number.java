ce261ce1c5e7de6c36d82e5bd04ca2a45219ccef
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-10 21:01:21 -0400

    Time: 2 ms (98.72%) | Memory: 57.3 MB (22.98%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,26 +1,35 @@
 class Solution {
     public int findDuplicate(int[] nums) {
 
-        int slow = 0;
-        int fast = 0;
+    //     int slow = 0;
+    //     int fast = 0;
 
-        while(true){
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            if(slow == fast) break;
-        }
+    //     while(true){
+    //         slow = nums[slow];
+    //         fast = nums[nums[fast]];
+    //         if(slow == fast) break;
+    //     }
        
-       int slow2 = 0;
-       while(true){
+    //    int slow2 = 0;
+    //    while(true){
 
-           slow = nums[slow];
-           slow2 = nums[slow2];
-           if(slow == slow2) break;
-       }
+    //        slow = nums[slow];
+    //        slow2 = nums[slow2];
+    //        if(slow == slow2) break;
+    //    }
 
-       return slow;
+    //    return slow;
 
-//******************************************************************** */       
+//******************************************************************** */      
+        int len = nums.length;
+        int[] freq = new int[len];
+
+        for(int i = 0; i < len; i++){
+            if(freq[nums[i]] == 0) freq[nums[i]] ++;
+            else return nums[i];
+        }
+        return 0;
+//******************************************************************** */
     //    HashSet<Integer> set = new HashSet<>();
     //    int doubleNum = 0;
 
