6cdd42eed8dd7175bcff666649cd90623ca89a9b
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-13 11:17:36 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (22.67%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,39 @@
 class Solution {
     public int rob(int[] nums) {
 
-         if(nums.length==0) return 0;
-         if(nums.length==1) return nums[0];
-        
-        int[] ans = new int[nums.length];
-
-        ans[0] = nums[0];
-        ans[1] = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < nums.length; i++){
-
-            ans[i] = Math.max(nums[i] + ans[i-2], ans[i-1]);            
+   if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        return ans[nums.length-1];
+        int now = 0;
+        int last = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            int temp = last;
+            last = now;
+            now = Math.max(now, nums[i] + temp);
+        }
+        return now;
     }
+    
+//****************************************************************** */
+    //      if(nums.length==0) return 0;
+    //      if(nums.length==1) return nums[0];
+        
+    //     int[] ans = new int[nums.length];
+
+    //     ans[0] = nums[0];
+    //     ans[1] = Math.max(nums[0], nums[1]);
+
+    //     for(int i = 2; i < nums.length; i++){
+
+    //         ans[i] = Math.max(nums[i] + ans[i-2], ans[i-1]);            
+    //     }
+
+    //     return ans[nums.length-1];
+    // }
 
 }
