a1d7c91e408c3b1251d6a46468a3f2e8bb963864
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-14 23:00:36 -0400

    Time: 69 ms (66.66%) | Memory: 55.2 MB (81.06%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,5 +1,84 @@
 class Solution {
     public int[] dailyTemperatures(int[] t) {
+    
+        // Stack<Integer> stack = new Stack<>();
+        // int[] ans = new int[t.length];
+        
+        // int idx = 0;
+        // for(int temp : t){
+        //     if(stack.empty()){
+        //         stack.push(t);
+        //     }
+
+        //     else{
+        //         if(stack.seek() < t){
+        //             int days = 0;
+        //             while(stack.seek() < t){
+        //                 stack.pop();
+        //                 days ++;
+
+        //             }
+        //             stack.push(t);
+        //             ans[idx ++] = days;
+        //         }
+        //         else{
+        //             stack.push(t);
+        //         }
+        //     }
+
+
+        // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         Stack<Integer> st = new Stack<>();
         int len = t.length;
         int ans[] = new int[len];
