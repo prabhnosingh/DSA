26d847b7fa48141397ded52119a7dc90bdd452bf
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-15 21:03:18 -0400

    Time: 1 ms (82.73%) | Memory: 41.2 MB (70.66%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -2,12 +2,26 @@ class Solution {
     public String gcdOfStrings(String str1, String str2) {
         
        
-
        if((str1 + str2).equals(str2 + str1)){
-           return str1.substring(0, gcd(str1.length(), str2.length()));
+           int a = str1.length();
+           int b = str2.length();
+
+           while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+           }
+           return str1.substring(0, a);
        }
        return "";
-    }
+        }
+}
+// **************************************************************************
+    //    if((str1 + str2).equals(str2 + str1)){
+    //        return str1.substring(0, gcd(str1.length(), str2.length()));
+    //    }
+    //    return "";
+    // }
     //     private int gcd(int a, int b){ //Euclidean Algorithm
     //        while(b != 0){
     //         int temp = b;
@@ -19,10 +33,11 @@ class Solution {
     //    }
 
 
-    public int gcd(int a, int b){
-        if(b == 0){
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-}
+// recursion
+    // public int gcd(int a, int b){
+    //     if(b == 0){
+    //         return a;
+    //     }
+    //     return gcd(b, a % b);
+//     }
+// }
