547233259deac8b68ed590781ecc481a8a622d2c
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-22 00:33:23 -0400

    Time: 3 ms (89.24%) | Memory: 43.6 MB (93.70%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -5,7 +5,20 @@ class Solution {
            return s;
        }
 
-       String vowels = "aieouAIEOU";
+    //    String vowels = "aieouAIEOU";
+
+    HashSet<Character> set = new HashSet<>();
+
+    set.add('a');
+    set.add('e');
+    set.add('i');
+    set.add('o');
+    set.add('u');
+    set.add('A');
+    set.add('E');
+    set.add('O');
+    set.add('U');
+    set.add('I');
 
        char[] chars = s.toCharArray();
 
@@ -14,13 +27,20 @@ class Solution {
 
        while(start < end){
 
-           while(start < end && !vowels.contains(chars[start]+ "")){
+           while(start < end && !set.contains(chars[start])){
                start ++;
            }
 
-           while(start < end && !vowels.contains(chars[end]+ "")){
+           while(start < end && !set.contains(chars[end])){
                end --;
            }
+        //    while(start < end && !vowels.contains(chars[start]+ "")){
+        //        start ++;
+        //    }
+
+        //    while(start < end && !vowels.contains(chars[end]+ "")){
+        //        end --;
+        //    }
 
            char temp = chars[end];
            chars[end] = chars[start];
