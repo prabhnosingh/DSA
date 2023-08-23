a368ce2d543d1a20ca9de18594cb48ff9c6a157a
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-23 11:55:33 -0400

    Time: 0 ms (100.00%) | Memory: 39.9 MB (99.17%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,27 +1,37 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        
-        if(needle.length() > haystack.length()) return -1;
+
+        int idx = 0;
+
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }
+
+        return -1;
+
+//*************************************************************************************** */
+      //beats 5.31%  
+        // if(needle.length() > haystack.length()) return -1;
         
       
-        for(int j = 0; j < haystack.length(); j ++){
+        // for(int j = 0; j < haystack.length(); j ++){
 
-            String s = "";
-            if(haystack.charAt(j) == needle.charAt(0)){
-                if(haystack.length() - j < needle.length()) return -1;
-                for(int i = j; i < j + needle.length(); i ++){
-                    s += haystack.charAt(i);
-                }
-                if(s.equals(needle)){
-                    return j;
-                }
+        //     String s = "";
+        //     if(haystack.charAt(j) == needle.charAt(0)){
+        //         if(haystack.length() - j < needle.length()) return -1;
+        //         for(int i = j; i < j + needle.length(); i ++){
+        //             s += haystack.charAt(i);
+        //         }
+        //         if(s.equals(needle)){
+        //             return j;
+        //         }
                 
-            }
-            // start = 0;
+        //     }
+        //     // start = 0;
              
             
             
-        }
-        return -1;
+        // }
+        // return -1;
     }
 }
