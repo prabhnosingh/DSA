9b33d437ae644c13da6dddc3f8eedaf7bc2fb421
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-27 00:05:40 -0400

    Time: 1 ms (89.72%) | Memory: 41.1 MB (7.60%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -4,18 +4,16 @@ class Solution {
         if(s.length() == 0) return true;
         int idx = 0;
         boolean ans = false;
-        boolean flag = false;
+   
         for(int i = 0; i < t.length(); i ++){
  
            
             if(t.charAt(i) == s.charAt(idx)){
                 idx ++;
-                flag = true;
+               
             }
-            else{
-                flag = false;
-            }
-             if(flag && idx == s.length()) {
+            
+             if(  idx == s.length()) {
                 ans = true;
                 break;
             }
