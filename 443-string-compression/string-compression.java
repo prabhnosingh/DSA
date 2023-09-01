45ce6df7f28ff19d67b7bce6244d380fd3451aa7
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-08-31 23:06:46 -0400

    Time: 9 ms (14.20%) | Memory: 43.3 MB (43.55%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int compress(char[] chars) {
+        
+        if(chars.length == 1) return 1;
+        String s = "";
+        int idx = 0;
+        
+
+        int ans = 0;
+        for(int i = 0; i < chars.length; i ++){
+            if(i < chars.length - 1 && chars[i] == chars[i + 1]){
+                s += chars[i];
+                
+                 int count = 1;
+                while(i < chars.length - 1 && chars[i] == chars[i + 1]){
+                    count ++; 
+                    i ++;                  
+                }
+          
+                s += count;
+                for(char ch : s.toCharArray()){
+                    chars[idx ++] = ch;
+                   ans ++;
+                }
+                s = "";
+               
+            }
+            else{
+                chars[idx ++] = chars[i];
+                ans ++;
+            
+            }
+         
+        }
+
+               
+
+        return ans;
+    }
+}
