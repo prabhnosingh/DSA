e070eb858b94e1faf887333fca49955bf8290ee6
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-09-02 23:23:00 -0400

    Time: 20 ms (78.31%) | Memory: 59 MB (42.80%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,34 +1,58 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+
+        
+//         Arrays.sort(nums);
+       
+//         PriorityQueue<Integer> mq= new PriorityQueue<Integer>(Collections.reverseOrder());
+//         int m = 1;
+    
+//         if(nums.length==0){
+//             return 0;
+//         }
+
+//         for(int i = 0; i < nums.length - 1; i ++){
+            
+//             if(nums[i] == nums[i+1]) continue;
+       
+        
+//             else if(nums[i] == nums[i+1]-1){
+//                 m ++;
+//                 mq.add(m);
+//             }
+//             else{
+//                 m=1;
+//             }
+//         }
+//     int i = 1;
+//     if(mq.peek() != null){ 
+//         i = mq.poll();
+//         }
+//     return i;
+//     }
+    
+// }
+
 class Solution {
     public int longestConsecutive(int[] nums) {
+       if (nums.length == 0) return 0;
+       HashSet<Integer> hs = new HashSet<>();
+       for(int num:nums) hs.add(num);
+       int longest =1;
+       for(int num: nums ){
+           //check if the num is the start of a sequence by checking if left exists
+           if(!hs.contains(num-1)){ // start of a sequence
+                int count =1;
+                while(hs.contains(num + 1)){ // check if hs contains next no.
+                    num++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+                
+           }
+           if(longest > nums.length/2) break;
 
-        
-        Arrays.sort(nums);
-       
-         PriorityQueue<Integer> mq= new PriorityQueue<Integer>(Collections.reverseOrder());
-        int m=1;
-    
-        if(nums.length==0){
-            return 0;
-        }
-
-        for(int i=0; i<nums.length-1; i++){
-            
-            if(nums[i]==nums[i+1]) continue;
-       
-        
-            else if(nums[i]==nums[i+1]-1){
-                m++;
-                mq.add(m);
-            }
-            else{
-                m=1;
-            }
-        }
-    int i=1;
-    if(mq.peek()!=null){ 
-        i = mq.poll();
-        }
-    return i;
+       }
+       return longest;
     }
-    
 }
