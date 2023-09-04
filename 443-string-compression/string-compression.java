9394784b724d7fa00900abd24c500c3dec79d80e
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-09-03 22:25:13 -0400

    Time: 11 ms (11.59%) | Memory: 43.4 MB (34.62%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,58 +1,58 @@
 class Solution {
     public int compress(char[] chars) {
    
-         int indexAns = 0, index = 0;
-        while(index < chars.length){
-            char currentChar = chars[index];
-            int count = 0;
-            while(index < chars.length && chars[index] == currentChar){
-                index++;
-                count++;
-            }
-            chars[indexAns++] = currentChar;
-            if(count != 1)
-                for(char c : Integer.toString(count).toCharArray()) 
-                    chars[indexAns++] = c;
-        }
-        return indexAns;
+        //  int indexAns = 0, index = 0;
+        // while(index < chars.length){
+        //     char currentChar = chars[index];
+        //     int count = 0;
+        //     while(index < chars.length && chars[index] == currentChar){
+        //         index++;
+        //         count++;
+        //     }
+        //     chars[indexAns++] = currentChar;
+        //     if(count != 1)
+        //         for(char c : Integer.toString(count).toCharArray()) 
+        //             chars[indexAns++] = c;
+        // }
+        // return indexAns;
    
 //************************************************************************************* */   
      // beats 15 %     
-        // if(chars.length == 1) return 1;
-        // String s = "";
-        // int idx = 0;
+        if(chars.length == 1) return 1;
+        String s = "";
+        int idx = 0;
         
 
-        // int ans = 0;
-        // for(int i = 0; i < chars.length; i ++){
-        //     if(i < chars.length - 1 && chars[i] == chars[i + 1]){
-        //         s += chars[i];
+        int ans = 0;
+        for(int i = 0; i < chars.length; i ++){
+            if(i < chars.length - 1 && chars[i] == chars[i + 1]){
+                s += chars[i];
                 
-        //          int count = 1;
-        //         while(i < chars.length - 1 && chars[i] == chars[i + 1]){
-        //             count ++; 
-        //             i ++;                  
-        //         }
+                 int count = 1;
+                while(i < chars.length - 1 && chars[i] == chars[i + 1]){
+                    count ++; 
+                    i ++;                  
+                }
           
-        //         s += count;
-        //         for(char ch : s.toCharArray()){
-        //             chars[idx ++] = ch;
+                s += count;
+                for(char ch : s.toCharArray()){
+                    chars[idx ++] = ch;
             
-        //            ans ++;
-        //         }
-        //         s = "";
+                   ans ++;
+                }
+                s = "";
                
-        //     }
-        //     else{
-        //         chars[idx ++] = chars[i];
-        //         ans ++;
+            }
+            else{
+                chars[idx ++] = chars[i];
+                ans ++;
             
-        //     }
+            }
          
-        // }
+        }
 
                
 
-        // return ans;
+        return ans;
     }
 }
