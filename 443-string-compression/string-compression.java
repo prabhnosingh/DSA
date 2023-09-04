d6ffbe684fd5c4098b12ce9e64ae28521781827b
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-09-03 22:30:12 -0400

    Time: 1 ms (98.66%) | Memory: 43.5 MB (20.94%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -18,42 +18,72 @@ class Solution {
    
 //************************************************************************************* */   
      // beats 15 %     
-        if(chars.length == 1) return 1;
-        String s = "";
-        int idx = 0;
+        // if(chars.length == 1) return 1;
+        // String s = "";
+        // int idx = 0;
         
-        int len = chars.length;
+        // int len = chars.length;
 
-        int ans = 0;
-        for(int i = 0; i < len; i ++){
-            if(i < len - 1 && chars[i] == chars[i + 1]){
-                s += chars[i];
+        // int ans = 0;
+        // for(int i = 0; i < len; i ++){
+        //     if(i < len - 1 && chars[i] == chars[i + 1]){
+        //         s += chars[i];
                 
-                 int count = 1;
-                while(i < len - 1 && chars[i] == chars[i + 1]){
-                    count ++; 
-                    i ++;                  
-                }
+        //          int count = 1;
+        //         while(i < len - 1 && chars[i] == chars[i + 1]){
+        //             count ++; 
+        //             i ++;                  
+        //         }
           
-                s += count;
-                for(char ch : s.toCharArray()){
-                    chars[idx ++] = ch;
+        //         s += count;
+        //         for(char ch : s.toCharArray()){
+        //             chars[idx ++] = ch;
             
-                   ans ++;
-                }
-                s = "";
+        //            ans ++;
+        //         }
+        //         s = "";
                
-            }
-            else{
-                chars[idx ++] = chars[i];
-                ans ++;
+        //     }
+        //     else{
+        //         chars[idx ++] = chars[i];
+        //         ans ++;
             
-            }
+        //     }
          
-        }
+        // }
 
                
 
-        return ans;
+        // return ans;
+
+        if (chars == null || chars.length == 0) {
+        return 0;
+    }
+
+    int writeIdx = 0;
+    int count = 1;
+
+    for (int i = 1; i < chars.length; i++) {
+        if (chars[i] == chars[i - 1]) {
+            count++;
+        } else {
+            chars[writeIdx++] = chars[i - 1];
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[writeIdx++] = c;
+                }
+            }
+            count = 1;
+        }
+    }
+
+    chars[writeIdx++] = chars[chars.length - 1];
+    if (count > 1) {
+        for (char c : Integer.toString(count).toCharArray()) {
+            chars[writeIdx++] = c;
+        }
+    }
+
+    return writeIdx;
     }
 }
