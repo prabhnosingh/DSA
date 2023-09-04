2bd5546e51ab20acf6ad990645c7ac2020785ab9
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-09-03 23:29:08 -0400

    Time: 1 ms (98.66%) | Memory: 43.4 MB (34.62%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,20 +1,46 @@
 class Solution {
     public int compress(char[] chars) {
    
-        //  int indexAns = 0, index = 0;
-        // while(index < chars.length){
-        //     char currentChar = chars[index];
-        //     int count = 0;
-        //     while(index < chars.length && chars[index] == currentChar){
-        //         index++;
-        //         count++;
-        //     }
-        //     chars[indexAns++] = currentChar;
-        //     if(count != 1)
-        //         for(char c : Integer.toString(count).toCharArray()) 
-        //             chars[indexAns++] = c;
-        // }
-        // return indexAns;
+    if(chars.length == 0){
+        return 0;
+    }
+    int idx = 0;
+    int count = 1;
+
+    for(int i = 1; i < chars.length; i ++){
+        if(chars[i - 1] == chars[i]){
+            count ++;
+        }
+        else{
+           
+                chars[idx ++] = chars[i - 1];
+           
+            if(count > 1){
+                for(char c : Integer.toString(count).toCharArray()){
+                    chars[idx ++] = c;
+                }
+            }
+            count = 1;
+        }
+    }
+
+    chars[idx ++] = chars[chars.length - 1];
+
+    if(count != 1){
+        for(char c : Integer.toString(count).toCharArray()){
+            chars[idx ++] = c;
+        }
+    }
+
+    return idx;
+
+
+
+
+
+
+
+
    
 //************************************************************************************* */   
      // beats 15 %     
@@ -55,35 +81,35 @@ class Solution {
                
 
         // return ans;
+///////////////////////////////////
+    //     if (chars == null || chars.length == 0) {
+    //     return 0;
+    // }
 
-        if (chars == null || chars.length == 0) {
-        return 0;
-    }
+    // int writeIdx = 0;
+    // int count = 1;
 
-    int writeIdx = 0;
-    int count = 1;
+    // for (int i = 1; i < chars.length; i++) {
+    //     if (chars[i] == chars[i - 1]) {
+    //         count++;
+    //     } else {
+    //         chars[writeIdx++] = chars[i - 1];
+    //         if (count > 1) {
+    //             for (char c : Integer.toString(count).toCharArray()) {
+    //                 chars[writeIdx++] = c;
+    //             }
+    //         }
+    //         count = 1;
+    //     }
+    // }
 
-    for (int i = 1; i < chars.length; i++) {
-        if (chars[i] == chars[i - 1]) {
-            count++;
-        } else {
-            chars[writeIdx++] = chars[i - 1];
-            if (count > 1) {
-                for (char c : Integer.toString(count).toCharArray()) {
-                    chars[writeIdx++] = c;
-                }
-            }
-            count = 1;
-        }
-    }
+    // chars[writeIdx++] = chars[chars.length - 1];
+    // if (count > 1) {
+    //     for (char c : Integer.toString(count).toCharArray()) {
+    //         chars[writeIdx++] = c;
+    //     }
+    // }
 
-    chars[writeIdx++] = chars[chars.length - 1];
-    if (count > 1) {
-        for (char c : Integer.toString(count).toCharArray()) {
-            chars[writeIdx++] = c;
-        }
-    }
-
-    return writeIdx;
+    // return writeIdx;
     }
 }
