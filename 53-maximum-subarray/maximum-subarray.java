94b429077dfdec3d419399ae7f4cdaca5659029f
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-09-05 19:02:53 -0400

    Time: 1 ms (100.00%) | Memory: 56.7 MB (94.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -15,8 +15,8 @@ class Solution {
 
 
 
-//********************************************************** */
-       //beats 100 % 
+//*******************************************************************/
+       //beats 100 % Kadane Algo(best)
         // int maxSum = Integer.MIN_VALUE;
         // int sum = 0;
         // for(int i = 0; i < nums.length; i ++){
