17ebbf22dc1a6ad7a3a6afc8c88508cfb227afe3
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-11-05 16:06:42 -0500

    Time: 17 ms (99.83%) | Memory: 55.3 MB (48.19%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,57 +1,62 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         
-        // Arrays.sort(nums);
+        Arrays.sort(nums);
 
-        // int start = 0;
-        // int end = nums.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
 
-        // int op = 0;
-        // while(nums[end] >= k){
-        //     end --;
-        // }
-        // while(start < end ){
-        //     if(nums[start] + nums[end] == k){
-        //         op ++;
-        //         start ++;
-        //         end --;
-        //     }
-        //     else{
-        //         if(nums[end] > )
-        //       int s = start + 1;
-        //       while(s < end){
-        //           if(nums[s] + nums[end]){
-
-        //           }
-        //       }
-        //     }
-        // }
-
-        // return op;
         int op = 0;
-        HashMap <Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-           map.put(num, map.getOrDefault(num, 0) + 1);
+        while( end >= 0 && nums[end] >= k){
+            end --;
+        }
+        int sum = 0;
+        while(start < end ){
+
+            sum = nums[start] + nums[end];
+            if(sum == k){
+                op ++;
+                start ++;
+                end --;
+            }
+            else{
+                if(sum > k){
+                    end --;
+                }
+                else{
+                    start ++;
+                }
+           
+            }
         }
 
-        for(int num1 : nums){
-            // if (num1 >= k){
-            //     continue;
-            // }
-            int complement = k - num1;
-            if(complement == num1){
-                if(map.get(num1) == 1){
-                    continue;
-                }
-            }
-            if(map.containsKey(complement)){
-                if(map.get(num1) > 0 && map.get(complement) > 0){
-                 map.put(num1, map.get(num1) - 1);
-                 map.put(complement, map.get(complement) - 1);
-                 op ++;
-                } 
-            } 
-        }
         return op;
+
+// beats 6.93%
+        // int op = 0;
+        // HashMap <Integer, Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //    map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+        // for(int num1 : nums){
+        //     // if (num1 >= k){
+        //     //     continue;
+        //     // }
+        //     int complement = k - num1;
+        //     if(complement == num1){
+        //         if(map.get(num1) == 1){
+        //             continue;
+        //         }
+        //     }
+        //     if(map.containsKey(complement)){
+        //         if(map.get(num1) > 0 && map.get(complement) > 0){
+        //          map.put(num1, map.get(num1) - 1);
+        //          map.put(complement, map.get(complement) - 1);
+        //          op ++;
+        //         } 
+        //     } 
+        // }
+        // return op;
     }
 }
