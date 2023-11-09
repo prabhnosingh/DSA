02513bcfd18b6d66536f6c1f69ca879dab6d000c
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-11-09 15:59:00 -0500

    Time: 1 ms (99.07%) | Memory: 44.3 MB (30.34%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,23 +1,48 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int pi = -1;
-        for(int idx = 0; idx < nums.length; idx ++){
-            int varL = idx - 1;
-            int varR = idx + 1;
-            int totalLeft = 0;
-            int totalRight = 0;
-            while(varL >= 0){
-                totalLeft += nums[varL];
-                varL --;
-            }
-            while(varR < nums.length){
-                totalRight += nums[varR];
-                varR ++;
-            }
-            if(totalRight == totalLeft){
-                return idx;
-            }
+       
+       int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
         }
-        return pi;
+        
+        int sumL = 0;
+
+        for(int i = 0; i < nums.length; i ++){
+            if(sumL * 2 == totalSum - nums[i]){
+                return i;
+            }
+            sumL += nums[i];
+        }
+        return -1;
+
+
+
+
+
+
+
+
+
+       // beats 9.23 %
+        // int pi = -1;
+        // for(int idx = 0; idx < nums.length; idx ++){
+        //     int varL = idx - 1;
+        //     int varR = idx + 1;
+        //     int totalLeft = 0;
+        //     int totalRight = 0;
+        //     while(varL >= 0){
+        //         totalLeft += nums[varL];
+        //         varL --;
+        //     }
+        //     while(varR < nums.length){
+        //         totalRight += nums[varR];
+        //         varR ++;
+        //     }
+        //     if(totalRight == totalLeft){
+        //         return idx;
+        //     }
+        // }
+        // return pi;
     }
 }
