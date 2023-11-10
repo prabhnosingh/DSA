f351e51b5bbd1b85aee69a925b28843772628639
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-11-10 00:34:44 -0500

    Time: 1 ms (99.74%) | Memory: 45.9 MB (95.86%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,27 +1,42 @@
 class Solution {
 
-public int countingOnes(int m){
-    int ones = 0;
+    public int[] countBits(int n) {
 
-    while(m != 0){
-        if(m % 2 > 0){
-            ones ++;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int offset = 1;
+        for(int i = 1; i < n + 1; i ++){
+            if(offset * 2 == i){
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];    
         }
-        m = m / 2;
+        return dp;
+
+/////////////////////////////////////////////
+// beats 12.16 %
+// public int countingOnes(int m){
+//     int ones = 0;
+
+//     while(m != 0){
+//         if(m % 2 > 0){
+//             ones ++;
+//         }
+//         m = m / 2;
         
-    }
-    return ones;
-}
+//     }
+//     return ones;
+// }
 
-public int[] countBits(int n) {
+// public int[] countBits(int n) {
 
-int[] ans = new int[n + 1];
+// int[] ans = new int[n + 1];
 
       
-        for(int i = 0; i < n + 1; i ++){
-            ans[i] = countingOnes(i);
-        }
-        return ans;
+//         for(int i = 0; i < n + 1; i ++){
+//             ans[i] = countingOnes(i);
+//         }
+//         return ans;
 
 
 //////////////////////////////////////////////////////
