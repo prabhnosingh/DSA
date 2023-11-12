d908f4c0f3bba2e4cfd1c83bb56662c720c80d5f
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-11-12 12:43:51 -0500

    Time: 2 ms (99.95%) | Memory: 44.7 MB (28.64%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,32 +1,62 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        
-       List<List<Integer>> answer = new ArrayList<>();
-       int[] counts1 = new int[2001];
-       int[] counts2 = new int[2001];
-       
-        for(int num : nums1){
-            counts1[num + 1000] ++;
-        }
-        for(int num : nums2){
-            counts2[num + 1000] ++;
-        }
 
+        List<List<Integer>> answer = new ArrayList<>();
         List<Integer> ans1 = new ArrayList<>();
         List<Integer> ans2 = new ArrayList<>();
-        for(int i = 0; i < counts1.length; i ++){
-            if(counts1[i] != 0 && counts2[i] == 0){
-                ans1.add(i - 1000);
-            }
-            if(counts2[i] != 0 && counts1[i] == 0){
-                ans2.add(i - 1000);
-            }
 
+        boolean[] counts1 = new boolean[2001];
+        boolean[] counts2 = new boolean[2001];
+
+        for(int num : nums1){
+            counts1[num + 1000] = true;
+        }
+        for(int num : nums2){
+            counts2[num + 1000] = true;
+            if(!counts1[num + 1000]){
+                ans2.add(num);
+                counts1[num + 1000] = true;
+            }
+        }
+
+        for(int num : nums1){
+            if(!counts2[num + 1000]){
+                ans1.add(num);
+                counts2[num + 1000] = true;
+            }
         }
 
         answer.add(ans1);
         answer.add(ans2);
         return answer;
+///////////////////////////////////////////////////////////////
+     // beats 99 %   
+    //    List<List<Integer>> answer = new ArrayList<>();
+    //    int[] counts1 = new int[2001];
+    //    int[] counts2 = new int[2001];
+       
+    //     for(int num : nums1){
+    //         counts1[num + 1000] ++;
+    //     }
+    //     for(int num : nums2){
+    //         counts2[num + 1000] ++;
+    //     }
+
+    //     List<Integer> ans1 = new ArrayList<>();
+    //     List<Integer> ans2 = new ArrayList<>();
+    //     for(int i = 0; i < counts1.length; i ++){
+    //         if(counts1[i] != 0 && counts2[i] == 0){
+    //             ans1.add(i - 1000);
+    //         }
+    //         if(counts2[i] != 0 && counts1[i] == 0){
+    //             ans2.add(i - 1000);
+    //         }
+
+    //     }
+
+    //     answer.add(ans1);
+    //     answer.add(ans2);
+    //     return answer;
 
 ///////////////////////////////////////////////////////////       
        // beats 37.50%
