489a0545e76653d6227f8f82dcff18161a8ced87
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-11-19 11:56:53 -0500

    Time: 1 ms (99.93%) | Memory: 56 MB (12.63%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,27 +1,72 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-       
-        int left = 0;
-        
-        int zeros = 0;
 
-        int ans = 0;
-        for(int right = 0; right < nums.length; right ++){
+        int prevCount = 0;
+        int currCount = 0;
+        int maxCount = 0;
 
-            if(nums[right] == 0){
-                zeros ++;
+        for(int i = 0; i < nums.length; i ++){
+            
+            if(nums[i] == 1){
+                currCount ++;
             }
 
-            while(zeros > 1){
-                if(nums[left] == 0){
-                    zeros --;
-                }
-                left ++;
+            else{
+                maxCount = Math.max(maxCount, prevCount + currCount);
+                prevCount = currCount;
+                currCount = 0;
+                
             }
 
-            ans = Math.max(ans, right - left + 1 - 1);
         }
+        maxCount = Math.max(maxCount, prevCount + currCount);
 
-        return ans == nums.length ? ans - 1 : ans;
+        return maxCount == nums.length ? maxCount - 1 : maxCount;
+        // return maxCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+   //beats 94%     
+        // int left = 0;
+        
+        // int zeros = 0;
+
+        // int ans = 0;
+        // for(int right = 0; right < nums.length; right ++){
+
+        //     if(nums[right] == 0){
+        //         zeros ++;
+        //     }
+
+        //     while(zeros > 1){
+        //         if(nums[left] == 0){
+        //             zeros --;
+        //         }
+        //         left ++;
+        //     }
+
+        //     ans = Math.max(ans, right - left + 1 - 1);
+        // }
+
+        // return ans == nums.length ? ans - 1 : ans;
     }
 }
