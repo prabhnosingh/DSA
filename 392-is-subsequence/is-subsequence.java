e3a2ee1c997334e6ebb65b9b9b61be8248d2bbde
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-09 00:14:29 -0500

    Time: 0 ms (100.00%) | Memory: 40.5 MB (52.75%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,19 +1,64 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
 
-        if(s.isEmpty()) return true;
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-        
-        int idx = 0;
-        for(char c : tChar){
-            if(sChar[idx] == c){
-                idx ++;
-            }
+        char[] chs = s.toCharArray();
+        char[] cht = t.toCharArray();
 
-            if(idx == sChar.length) return true;
+        if(chs.length == 0){
+            return true;
+        }
+        int idx = 0;
+        
+        for(char c : cht){
+            if(c == chs[idx]){
+                idx ++;
+                if(idx == chs.length){
+                    return true;
+                }
+            }
         }
         return false;
+
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+//************************************************************************************* */      
+        // if(s.isEmpty()) return true;
+        // char[] sChar = s.toCharArray();
+        // char[] tChar = t.toCharArray();
+        
+        // int idx = 0;
+        // for(char c : tChar){
+        //     if(sChar[idx] == c){
+        //         idx ++;
+        //     }
+
+        //     if(idx == sChar.length) return true;
+        // }
+        // return false;
 //************************************************************** */
       //beats 90%  
         // if(s.length() == 0) return true;
