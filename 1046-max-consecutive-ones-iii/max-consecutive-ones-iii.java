fe1da1593da67abd3ba232cc5c8bd1deeb105dd3
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-12 11:36:02 -0500

    Time: 2 ms (100.00%) | Memory: 47.1 MB (5.17%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,33 +1,29 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
 
-        // int left = 0;
-        // int right = 0;
-        // int maxLen = 0;
-        // int n = k;
-        // while(right < nums.length){
-        //     int len = 0;
-        //     if(nums[right] == 1){
-        //         right ++;
-        //     }
-        //     else{
+        int left = 0;
+        int right = 0;
+        int zeros = 0;
+        while(right < nums.length){
+        
+            if(nums[right] == 0){
+                zeros ++;
+            }
 
-        //         if(k == 0){
-        //             left = right;
-        //             k = n - 1;
-        //         }
-        //         else{
-        //             k --;
-        //             right ++;
-        //             len = right - left + 1;
-        //             maxLen = Math.max(maxLen, len);
-        //         }
-                
+            right ++;
+
+            if(zeros > k){
+                if(nums[left] == 0){
+                    zeros --;
+                }
+                left ++;
+            }
             
 
-        //     }
-        // }
-        // return maxLen;
+            }
+            return right - left;
+            
+    
         
 
 
@@ -62,29 +58,30 @@ class Solution {
 
 
 ///////////////////////////////////////////////////////////////
-        int left = 0;
-        int right = 0;
-        int f = k;
-        int count = 0;
-        int maxCount = 0;
+    // beats 5.01%
+        // int left = 0;
+        // int right = 0;
+        // int f = k;
+        // int count = 0;
+        // int maxCount = 0;
 
-        while(right < nums.length){
-            if(nums[right] == 1){
-                count ++;
-            }
-            else if(nums[right] == 0 && f > 0){
-                f --;
-                count ++;
-            }
-            else if(nums[right] == 0 && f == 0){
-                left ++;
-                right = left - 1;
-                count = 0;
-                f = k;
-            }
-            maxCount = Math.max(count, maxCount);
-            right ++;
-        }
-        return maxCount;
+        // while(right < nums.length){
+        //     if(nums[right] == 1){
+        //         count ++;
+        //     }
+        //     else if(nums[right] == 0 && f > 0){
+        //         f --;
+        //         count ++;
+        //     }
+        //     else if(nums[right] == 0 && f == 0){
+        //         left ++;
+        //         right = left - 1;
+        //         count = 0;
+        //         f = k;
+        //     }
+        //     maxCount = Math.max(count, maxCount);
+        //     right ++;
+        // }
+        // return maxCount;
     }
 }
