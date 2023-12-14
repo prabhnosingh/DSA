19bf831c50f1d9bfd1e93d91277a5027951bcf6b
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-14 14:52:11 -0500

    Time: 2 ms (81.41%) | Memory: 43.3 MB (89.84%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,103 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map1= new HashMap<>();
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
 
-       int[] result = new int[2];
-    //    int s=0;
-    //    int e=0;
-
-        for(int i=0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i ++){
             int complement = target - nums[i];
-            if(!map1.containsKey(complement)){
-                map1.put(nums[i],i);
+
+            if(map.containsKey(nums[i])){
+                // return new int[]
+                ans[0] = i;
+                ans[1] = map.get(nums[i]);
+                break;
             }
-            else{
-                result[0] = map1.get(complement);
-                result[1] = i;
-            //    s = map1.get(complement);
-            // e = i;
+            map.put(complement, i);
+        }
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int start = 0;
+        // int end = 1;
+        // int window = 1;
+
+        // while(true){
+        //     if(nums[start] + nums[end] == target){
+        //         break;
+        //     }
+
+        //     else if(end == nums.length - 1){
+        //         window ++;
+        //         start = 0;
+        //         end = window;
+        //     }
+            
+        //     else{
+        //         start ++;
+        //         end ++;
+        //     }
+        // }
+
+        // return new int[]{start, end};
+        
+//************************************************************************* */   
+   //  beats 60 %
+    // Map<Integer, Integer> map1= new HashMap<>();
+
+    //    int[] result = new int[2];
+    // //    int s=0;
+    // //    int e=0;
+
+    //     for(int i=0; i < nums.length; i++){
+    //         int complement = target - nums[i];
+    //         if(!map1.containsKey(complement)){
+    //             map1.put(nums[i],i);
+    //         }
+    //         else{
+    //             result[0] = map1.get(complement);
+    //             result[1] = i;
+    //         //    s = map1.get(complement);
+    //         // e = i;
                 
 
                 
-            }
-        }
-        // return new int[]{s,e};
-        return result;
+    //         }
+    //     }
+    //     // return new int[]{s,e};
+    //     return result;
         
        
     }
