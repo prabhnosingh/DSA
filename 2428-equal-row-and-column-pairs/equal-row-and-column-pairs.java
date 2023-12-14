083076b39bc371f7eae44ff273d09c480e13803c
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-14 14:30:10 -0500

    Time: 3 ms (99.20%) | Memory: 47.8 MB (84.35%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,39 +1,85 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        
-        int n = grid[0].length;
-        int count = 0;
-       
-        HashMap<ArrayList<Integer>, Integer> map  = new HashMap<>();
+
+        int n = grid.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
 
         for(int i = 0; i < n; i ++){
-            ArrayList<Integer> arr = new ArrayList<>();
-         
+            int hash = 0;
 
             for(int j = 0; j < n; j ++){
-                arr.add(grid[i][j]);
-              
+                hash = grid[i][j] + (hash * 13);
+
+            }
+            map.put(hash, map.getOrDefault(hash, 0) + 1);
+            
+        }
+        int count = 0;
+
+        for(int i = 0; i < n; i ++){
+            int hash = 0;
+
+            for(int j = 0; j < n; j ++){
+                hash = grid[j][i] + (hash * 13);
+
             }
 
-            map.put(arr, map.getOrDefault(arr, 0) + 1);
+            count += map.getOrDefault(hash, 0);
+
+        }
+
+        return count;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////
+      // beats 90 %
+        // int n = grid[0].length;
+        // int count = 0;
+       
+        // HashMap<ArrayList<Integer>, Integer> map  = new HashMap<>();
+
+        // for(int i = 0; i < n; i ++){
+        //     ArrayList<Integer> arr = new ArrayList<>();
+         
+
+        //     for(int j = 0; j < n; j ++){
+        //         arr.add(grid[i][j]);
+              
+        //     }
+
+        //     map.put(arr, map.getOrDefault(arr, 0) + 1);
                  
             
           
-        }
+        // }
 
-        for(int i = 0; i < n; i ++){
-            ArrayList<Integer> arr2 = new ArrayList<>();
+        // for(int i = 0; i < n; i ++){
+        //     ArrayList<Integer> arr2 = new ArrayList<>();
 
-             for(int j = 0; j < n; j ++){
-                arr2.add(grid[j][i]);
+        //      for(int j = 0; j < n; j ++){
+        //         arr2.add(grid[j][i]);
               
-            }
+        //     }
 
-            if(map.containsKey(arr2)){
-                count += map.get(arr2);
-            }
-        }
-        // return equal == n ? count + equal - 1 : count;
-        return count;
+        //     if(map.containsKey(arr2)){
+        //         count += map.get(arr2);
+        //     }
+        // }
+        // // return equal == n ? count + equal - 1 : count;
+        // return count;
     }
 }
