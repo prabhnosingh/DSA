ade72db030dc544efea61b955207312878658a42
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-21 20:26:31 -0500

    Time: 29 ms (80.78%) | Memory: 51.3 MB (31.81%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,7 +5,7 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         
 
-        for(int i = 0; i < nums.length -2; i ++){
+        for(int i = 0; i < nums.length - 2; i ++){
 
             if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
