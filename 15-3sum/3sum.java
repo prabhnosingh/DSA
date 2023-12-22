be0e4d2a95e06996f6773b57e34c97047b167614
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2023-12-21 20:26:20 -0500

    Time: 28 ms (84.28%) | Memory: 50.8 MB (51.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,163 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        int target = 0;
         Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> output = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        
 
-        for(int i = 0; i < nums.length; i ++){
-            int j = i + 1;
-            int k = nums.length - 1;
+        for(int i = 0; i < nums.length -2; i ++){
 
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if(sum == target){
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j ++;
-                    k --;
-                }
-                else if(sum < target){
-                    j ++;
-                }
-                else{
-                    k --;
-                }
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
             }
 
+            int first = nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            int sum = 0;  
+            while(left < right){
+
+                int second = nums[left];
+                int third = nums[right];
+                sum = first + second + third;
+
+                if(sum == 0){
+                    ans.add(Arrays.asList(first, second, third));
+                    left ++;
+                    while(nums[left] == nums[left - 1] && left < right){
+                        left ++;
+                    }
+                    
+                }   
+
+                else if(sum > 0){
+                   right --;
+                }
+                else{
+                    
+                    left ++;
+                }
+            }
         }
-        output.addAll(s);
-        return output;
+    return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+    // int[] twoSum(int[] nums, int idx, int target){
+
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+
+    //     int complement = 0; 
+    //     for(int i = 0; i < nums.length; i ++){
+    //         if(i == idx){
+    //             continue;
+    //         }
+    //         complement = target - nums[i];
+
+    //         if(map.containsKey(complement)){
+    //             return new int[]{i, map.get(complement)};
+    //         }
+    //         map.put(nums[i], i);
+    //     }   
+    //     return new int[]{-1,-1};
+    // }
+    // public List<List<Integer>> threeSum(int[] nums) {
+    //     HashSet<List<Integer>> set = new HashSet<>();
+    //     List<List<Integer>> ans = new ArrayList<>();
+     
+    //   for(int i = 0; i < nums.length; i ++){
+
+        
+    //       int[] twoS = twoSum(nums, i, -nums[i]);
+    //       if(twoS[0] != -1 && twoS[1] != -1){
+    //         int[] temp = new int[3];
+    //         temp[0] = nums[i];
+    //         temp[1] = nums[twoS[0]]; 
+    //         temp[2] = nums[twoS[1]];
+    //         Arrays.sort(temp);
+    //         set.add(Arrays.asList(temp[0], temp[1], temp[2]));
+    //       }
+        
+         
+
+    //   }
+    //          ans.addAll(set);
+    //   return ans;      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*********************************************************************** */
+        // beats 11.53%
+        // int target = 0;
+        // Arrays.sort(nums);
+        // Set<List<Integer>> s = new HashSet<>();
+        // List<List<Integer>> output = new ArrayList<>();
+
+        // for(int i = 0; i < nums.length; i ++){
+        //     int j = i + 1;
+        //     int k = nums.length - 1;
+
+        //     while(j < k){
+        //         int sum = nums[i] + nums[j] + nums[k];
+
+        //         if(sum == target){
+        //             s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+        //             j ++;
+        //             k --;
+        //         }
+        //         else if(sum < target){
+        //             j ++;
+        //         }
+        //         else{
+        //             k --;
+        //         }
+        //     }
+
+        // }
+        // output.addAll(s);
+        // return output;
     
 
     }
