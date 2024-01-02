f7f071929cc7915226cf27508b136b48b681bd49
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-01-02 00:55:10 -0500

    Time: 1 ms (12.28%) | Memory: 43.3 MB (6.05%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -17,15 +17,46 @@ class Solution {
     int depth = 1;
     public int maxDepth(TreeNode root) {
         
+        //recursive DFS
+        // if(root == null){
+        //     return 0;
+        // }
+
+        
+
+        // int l = maxDepth(root.left);
+        // int r = maxDepth(root.right);
+        // return 1 + Math.max(l, r);
+//***************************************************** */
+
+        //Iterative BFS using a Queue
 
         if(root == null){
             return 0;
         }
+        int height = 0;
+        Queue<TreeNode> q = new LinkedList<>();
 
-        
+        q.add(root);
 
-        int l = maxDepth(root.left);
-        int r = maxDepth(root.right);
-        return 1 + Math.max(l, r);
+        while(!q.isEmpty()){
+
+            height ++;
+            int size = q.size();
+
+            while(size-- > 0){
+                TreeNode temp = q.poll();
+
+                if(temp.left != null){
+                    q.add(temp.left);
+                }
+
+                if(temp.right != null){
+                    q.add(temp.right);
+                }
+            }
+
+        }
+        return height;
     }
 }
