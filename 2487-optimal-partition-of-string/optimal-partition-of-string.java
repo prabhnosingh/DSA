3df1f7acddc392fbdad2dd08b214aeb3ada4e28b
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-01-05 00:34:43 -0500

    Time: 4 ms (99.96%) | Memory: 45.3 MB (20.60%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -1,24 +1,43 @@
 class Solution {
     public int partitionString(String s) {
-        
-        // HashMap<Character, String>
-        HashSet<Character> set = new HashSet<>();
 
-        // int[] chars = new int[26];
-        int count = 1;
-        // int max = 0;
-        
+        int xr = 0;
+        int ans = 1;
+
         for(char c : s.toCharArray()){
-                if(!set.contains(c)){
-                    set.add(c);
-                }
-                else{
-                    set.clear();
-                    set.add(c);
-                    count ++;
-                }
-             
+            if((xr & (1 << c)) != 0){
+                
+                xr = 0;
+                ans ++;
+            }
+
+            xr ^= (1 << c);
         }
-        return count;
+        return ans;
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////       
+        //beats 53.77 %
+        // // HashMap<Character, String>
+        // HashSet<Character> set = new HashSet<>();
+
+        // // int[] chars = new int[26];
+        // int count = 1;
+        // // int max = 0;
+        
+        // for(char c : s.toCharArray()){
+        //         if(!set.contains(c)){
+        //             set.add(c);
+        //         }
+        //         else{
+        //             set.clear();
+        //             set.add(c);
+        //             count ++;
+        //         }
+             
+        // }
+        // return count;
     }
 }
