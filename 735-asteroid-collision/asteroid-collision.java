3d7287c5752a4c5e1b9f551950eaea4173f3e595
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-02-24 12:36:21 -0500

    Time: 1 ms (100.00%) | Memory: 45.2 MB (73.96%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,52 +1,102 @@
 class Solution {
-        
-        public boolean isSameSign(int x, int y){
-            if(x * y > 0){
-                return true;
+
+     public int[] asteroidCollision(int[] asteroids) {
+
+        int index = -1;
+
+        for(int currentAsteroid: asteroids){
+
+            boolean current_A_Undestroyed = true;
+
+            while(current_A_Undestroyed && index >= 0 && currentAsteroid < 0 && asteroids[index] > 0){
+                current_A_Undestroyed = currentAsteroid + asteroids[index] < 0;
+                if(currentAsteroid + asteroids[index] <= 0){
+                    -- index;
+                }
+
             }
-            else{
-                return false;
+
+            if(current_A_Undestroyed){
+                asteroids[++ index] = currentAsteroid;
             }
+
         }
-  public int[] asteroidCollision(int[] a) {
 
-        Stack<Integer> s1 = new Stack<>();
+        return Arrays.copyOfRange(asteroids, 0, index + 1);
+
+
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
+   // beats 12%
+
+//         public boolean isSameSign(int x, int y){
+//             if(x * y > 0){
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//   public int[] asteroidCollision(int[] a) {
+
+//         Stack<Integer> s1 = new Stack<>();
     
-        for(int i = 0; i < a.length; i ++){
+//         for(int i = 0; i < a.length; i ++){
 
-          if(s1.size() == 0 || (s1.peek() < 0 && a[i] > 0) || isSameSign(s1.peek(), a[i])){
-              s1.push(a[i]);
-          }
+//           if(s1.size() == 0 || (s1.peek() < 0 && a[i] > 0) || isSameSign(s1.peek(), a[i])){
+//               s1.push(a[i]);
+//           }
           
-          else{
-              while(s1.size() > 0 && s1.peek() <= Math.abs(a[i]) && !isSameSign(s1.peek(), a[i])){
+//           else{
+//               while(s1.size() > 0 && s1.peek() <= Math.abs(a[i]) && !isSameSign(s1.peek(), a[i])){
 
-              if(s1.size() > 0 && s1.peek() == Math.abs(a[i])){
-                  s1.pop();
+//               if(s1.size() > 0 && s1.peek() == Math.abs(a[i])){
+//                   s1.pop();
                
-                  break;
-              }
-              if(s1.size() > 0) s1.pop();
+//                   break;
+//               }
+//               if(s1.size() > 0) s1.pop();
              
-              if(s1.size() == 0 || s1.size() > 0 && isSameSign(s1.peek(), a[i])){
-                  s1.push(a[i]);
-              }
-              }
+//               if(s1.size() == 0 || s1.size() > 0 && isSameSign(s1.peek(), a[i])){
+//                   s1.push(a[i]);
+//               }
+//               }
 
-          }
+//           }
 
 
 
-      }
-    int n = s1.size();
-    int[] ans = new int[n];
-    n--;
-    while(s1.size() != 0){
-          ans[n --] = s1.pop(); 
-      }
+//       }
+//     int n = s1.size();
+//     int[] ans = new int[n];
+//     n--;
+//     while(s1.size() != 0){
+//           ans[n --] = s1.pop(); 
+//       }
 
-      return ans;
-  }
+//       return ans;
+//   }
 
 
 
@@ -125,4 +175,4 @@ class Solution {
     //     return ans;
 // } 
 
-}
+// }
