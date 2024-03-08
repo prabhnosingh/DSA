d5580b96a64f6d11032194eb24eb5a1d71d57845
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-03-07 23:36:01 -0500

    Time: 1 ms (16.71%) | Memory: 44.4 MB (5.11%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -18,23 +18,60 @@ class Solution {
     int ans = 0;
     public int diameterOfBinaryTree(TreeNode root) {
 
-        if(root == null) return 0;
+        if(root == null){
+            return 0;
+        }
         height(root);
         return ans;
 
     }
 
     public int height(TreeNode root){
-        if(root == null) return -1;
 
-        int L = height(root.left);
-        int R = height(root.right);
+        if(root == null){
+            return -1;
+        }
 
-        ans = Math.max(ans, L + R + 2); // ans signifies diameter
+        int leftChild = height(root.left);
+        int rightChild = height(root.right);
 
-        return 1 + Math.max(L, R);
+        ans = Math.max(ans, leftChild + rightChild + 2);
+
+        return 1 + Math.max(leftChild, rightChild);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    //     if(root == null) return 0;
+    //     height(root);
+    //     return ans;
+
+    // }
+
+    // public int height(TreeNode root){
+    //     if(root == null) return -1;
+
+    //     int L = height(root.left);
+    //     int R = height(root.right);
+
+    //     ans = Math.max(ans, L + R + 2); // ans signifies diameter
+
+    //     return 1 + Math.max(L, R);
+
+    // }
 //****************************************************************************************** */  
         //recursive solution(beats 6%)
     //     if(root == null) return 0;
