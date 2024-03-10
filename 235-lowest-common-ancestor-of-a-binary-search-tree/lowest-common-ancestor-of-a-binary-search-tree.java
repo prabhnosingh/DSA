c2494e5ecb569ac3d6348aaf3efa0de18a64130f
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-03-10 15:42:24 -0400

    Time: 5 ms (100.00%) | Memory: 44.8 MB (67.84%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -27,16 +27,34 @@ class Solution {
         // }
         // return null;
               
+        // Recursive solution
+        // if(p.val < root.val && q.val < root.val){
+        //     return lowestCommonAncestor(root.left, p, q);
+        // }
+        // else if(p.val > root.val && q.val > root.val){
+        //     return lowestCommonAncestor(root.right, p, q);
+        // }
+        // else{
+        //     return root;
+        // }
 
-        if(p.val < root.val && q.val < root.val){
-            return lowestCommonAncestor(root.left, p, q);
+        // Iterative solution
+
+        TreeNode cur = root;
+        while(cur != null){
+
+            if(p.val > cur.val && q.val > cur.val){
+                cur = cur.right;
+            }
+            else if(p.val < cur.val && q.val < cur.val){
+                cur = cur.left;
+            }
+            else{
+                return cur;
+            }
+            
         }
-        else if(p.val > root.val && q.val > root.val){
-            return lowestCommonAncestor(root.right, p, q);
-        }
-        else{
-            return root;
-        }
+        return null;
 
     }
 }
