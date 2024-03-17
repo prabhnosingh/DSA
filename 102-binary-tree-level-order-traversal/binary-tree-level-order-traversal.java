d18304165475bb5b4b3b99758f932d7017e43d4b
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-03-17 18:57:57 -0400

    Time: 2 ms (17.31%) | Memory: 45.1 MB (23.02%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,52 +15,103 @@
  */
 class Solution {
 
-    Queue<TreeNode> q = new LinkedList<>();
-    int iterations = q.size();
-
-    public List<Integer> makeList(int iterations){    
-        List<Integer> l = new ArrayList<>();
-        int i = 0;
-        for(int j = 0; j < iterations; j ++){
-            if(q.peek() == null) break;
-            if(q.peek().left == null && q.peek().right == null){
-                i = q.remove().val; 
-            }
-            else if(q.peek().left != null && q.peek().right == null){
-                q.add(q.peek().left);
-                i = q.remove().val;
-            }
-            else if(q.peek().left == null && q.peek().right != null){
-                q.add(q.peek().right);
-                i = q.remove().val;
-
-            }   
-            else{
-                q.add(q.peek().left);
-                q.add(q.peek().right);
-                i = q.remove().val;
-            }
-            l.add(i);
-        }
-        // iterations = q.size();
-        return l;
-
-    }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        if(root == null) return ans;
-        q.add(root); 
-        iterations ++;   
-        while(iterations != 0){
-            ans.add(makeList(iterations));
-            iterations = q.size();
-        }    
         
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) return ans;
+
+        q.add(root);
+
+        while(q.size() != 0){
+            int iterations = q.size();
+            List<Integer> l = new ArrayList<>();
+
+            for(int i = 0; i < iterations; i ++){
+
+                if(q.peek() != null){
+                    q.add(q.peek().left);
+                    q.add(q.peek().right);
+                    l.add(q.remove().val);
+                }
+                else{
+                    q.remove();
+                }
+
+                // TreeNode node = q.remove();
+                // if(node != null){
+                //     q.add(node.left);
+                //     q.add(node.right);
+                //     l.add(node.val);
+                // }
+                
+                
+            }
+            if(l.size() != 0){
+                ans.add(l);
+            }
+
+        }
+
 
 
         return ans;
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////
+    // beats 88.34% 
+    // Queue<TreeNode> q = new LinkedList<>();
+    // int iterations = q.size();
+
+    // public List<Integer> makeList(int iterations){    
+    //     List<Integer> l = new ArrayList<>();
+    //     int i = 0;
+    //     for(int j = 0; j < iterations; j ++){
+    //         if(q.peek() == null) break;
+    //         if(q.peek().left == null && q.peek().right == null){
+    //             i = q.remove().val; 
+    //         }
+    //         else if(q.peek().left != null && q.peek().right == null){
+    //             q.add(q.peek().left);
+    //             i = q.remove().val;
+    //         }
+    //         else if(q.peek().left == null && q.peek().right != null){
+    //             q.add(q.peek().right);
+    //             i = q.remove().val;
+
+    //         }   
+    //         else{
+    //             q.add(q.peek().left);
+    //             q.add(q.peek().right);
+    //             i = q.remove().val;
+    //         }
+    //         l.add(i);
+    //     }
+    //     // iterations = q.size();
+    //     return l;
+
+    // }
+
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> ans = new ArrayList<>();
+
+    //     if(root == null) return ans;
+    //     q.add(root); 
+    //     iterations ++;   
+    //     while(iterations != 0){
+    //         ans.add(makeList(iterations));
+    //         iterations = q.size();
+    //     }    
+        
+
+
+    //     return ans;
         
     }
 }
