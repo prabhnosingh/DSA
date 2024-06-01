83c2e149921607207ed27361deabc6e382d27a7c
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-06-01 02:32:02 -0400

    Time: 6 ms (73.02%) | Memory: 53.1 MB (91.10%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,15 +1,68 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if(map.get(num) == 2){
-                ans.add(num);
+        int count = 0;
+        for(int i = 0; i < nums.length; i ++){
+            
+            int idx = Math.abs(nums[i]);
+            
+            if(nums[idx - 1] < 0){
+                ans.add(Math.abs(idx));
             }
+            nums[idx - 1] = -1 * nums[idx - 1];
+            
         }
         return ans;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// not correct, uses non-constant space and beats 5.84 %
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // List<Integer> ans = new ArrayList<>();
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        //     if(map.get(num) == 2){
+        //         ans.add(num);
+        //     }
+        // }
+        // return ans;
 
 
         
