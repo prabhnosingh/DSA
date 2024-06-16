07bbf1890dc5b8ab92e143e248357fa494b00321
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-06-16 08:28:32 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (48.88%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,15 +10,15 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
+
         if(list1 == null) return list2;
-        else if(list2 == null) return list1;
+        if(list2 == null) return list1;
 
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
 
         while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
+            if(list1.val < list2.val){
                 curr.next = list1;
                 list1 = list1.next;
             }
@@ -29,7 +29,61 @@ class Solution {
             curr = curr.next;
         }
 
-        curr.next = list2 == null ? list1 : list2;
+        curr.next = list1 == null ? list2 : list1;
+
         return dummy.next;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////
+        // if(list1 == null) return list2;
+        // else if(list2 == null) return list1;
+
+        // ListNode dummy = new ListNode(0);
+        // ListNode curr = dummy;
+
+        // while(list1 != null && list2 != null){
+        //     if(list1.val <= list2.val){
+        //         curr.next = list1;
+        //         list1 = list1.next;
+        //     }
+        //     else{
+        //         curr.next = list2;
+        //         list2 = list2.next;
+        //     }
+        //     curr = curr.next;
+        // }
+
+        // curr.next = list2 == null ? list1 : list2;
+        // return dummy.next;
     }
 }
