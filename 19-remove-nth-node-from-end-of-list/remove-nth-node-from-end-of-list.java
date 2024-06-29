3e8b0c765192f3fd2e85cc84bda592977aeb42f3
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-06-29 09:16:54 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (46.38%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,52 +10,67 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+
+
+
+        ListNode dummy = new ListNode(0, head);
+        
+        dummy.next = head;
+
+        ListNode p1 = dummy;
+        ListNode p2 = dummy;
+        int i = 0;
+        while(i != n + 1 && p2 != null){
+            p2 = p2.next;
+            i ++;
+        }
+
+        while(p2 != null){
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+
+        p1.next = p1.next.next;
+
+        return dummy.next;
+
+       
+    // **********************************************************************
+    //    ListNode pointer = head;
+    //    int len = 0;
+
+
+    //    while(pointer != null){
+    //        pointer = pointer.next;
+    //        len ++;
+    //    }
+    //    if(len == 1){
+    //        return null;
+    //    }
+       
+    //    int loc = len - n;
+    //     ListNode i = head;
+    //     int count = 0;
+    //    while(i != null && i.next != null){
+
+    //        if( loc - 1 < 0||count == loc - 1){
+
+    //            if(loc - 1 < 0) {
+    //               return head = i.next;
+    //            }
+               
+    //                 i.next = i.next.next;
+               
+               
+    //        }
+    //        i = i.next;
+    //        count ++;
+    //    }
+    //     return head;
+       
        
     
-       ListNode pointer = head;
-       int len = 0;
-
-
-       while(pointer != null){
-           pointer = pointer.next;
-           len ++;
-       }
-       if(len == 1){
-           return null;
-       }
-       
-       int loc = len - n;
-        ListNode i = head;
-        int count = 0;
-       while(i != null && i.next != null){
-
-           if( loc - 1 < 0||count == loc - 1){
-
-               if(loc - 1 < 0) {
-                  return head = i.next;
-               }
-               
-                    i.next = i.next.next;
-               
-               
-           }
-           i = i.next;
-           count ++;
-       }
-        return head;
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
        
        
        
