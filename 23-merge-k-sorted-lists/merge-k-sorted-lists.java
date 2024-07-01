dd8fb206752b8dadab3070646dc6d1e90a9f804a
Prabhnoor Singh <71306744+prabhnoor23@users.noreply.github.com>
2024-07-01 10:16:24 -0400

    Time: 83 ms (15.79%) | Memory: 44.1 MB (84.19%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        if(lists.length == 0) return null;
+        
+        ListNode ans = lists[0];
+        // System.out.println(ans.next.val);
+        // System.out.println(ans.next.next.val);
+        ListNode ansCurr = ans;
+        for(int i = 1; i < lists.length; i ++){
+
+                ans = mergeTwoLists(ans, lists[i]);
+        
+            }
+            return ans;
+
+        }
+
+
+        public ListNode mergeTwoLists(ListNode head1, ListNode head2){
+
+            if(head1 == null) return head2;
+            if(head2 == null) return head1;
+
+            ListNode dummy = new ListNode(0);
+            ListNode curr = dummy;
+            ListNode curr1 = head1;
+            ListNode curr2 = head2;
+
+            while(head1 != null && head2 != null){
+
+                if(head1.val < head2.val){
+                    curr.next = head1;
+                    head1 = head1.next;
+                }
+                else{
+                    curr.next = head2;
+                    head2 = head2.next;
+                }
+
+                curr = curr.next;
+
+            }
+
+            curr.next = head2 == null ? head1 : head2;
+            return dummy.next;
+        }
+        
+    }
