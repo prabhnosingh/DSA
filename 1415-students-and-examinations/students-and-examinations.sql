9a03458c7deb51f23648e757e79e20dfa249f3b4
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2024-10-18 00:27:58 -0400

    Time: 1212 ms (9.32%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -6,5 +6,5 @@ LEFT JOIN Examinations e
 ON stu.student_id = e.student_id
 AND sub.subject_name = e.subject_name
 GROUP BY stu.student_id, sub.subject_name
-ORDER BY student_id ASC
+ORDER BY student_id ASC, subject_name ASC
 
