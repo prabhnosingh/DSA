ff8715906b2545d632e9be5c76788e79af7102f9
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2024-11-26 22:51:00 -0500

    Time: 12 ms (98.89%) | Memory: 47 MB (9.95%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -3,9 +3,9 @@ class Solution {
         
         Arrays.sort(nums);
         int maxSum = 0;
-        for(int i = 0; i < nums.length; i ++){
+        for(int i = 0; i < nums.length; i +=2){
             maxSum += nums[i];
-            i ++;
+            // i ++;
 
         }
 
