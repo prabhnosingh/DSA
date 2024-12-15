dbf35544c9ac1fba138fc1ff787a69a0de5871d9
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2024-12-15 17:33:10 -0500

    Time: 1284 ms (84.87%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,15 +1,42 @@
 # Write your MySQL query statement below
-SELECT customer_id, COUNT(visit_id) AS count_no_trans
-FROM Visits
-LEFT JOIN Transactions 
-USING(visit_id)
-WHERE transaction_id IS NULL 
-Group BY customer_id
+
+
+ select visits.customer_id, count(visits.customer_id) as count_no_trans
+ from visits left join
+ transactions on visits.visit_id = transactions.visit_id
+ where transactions.visit_id is null 
+--  where transactions.amount is null -> This cannot be used as there is a case where a user has entry 
+-- in transactions table with null amount, but it is still counted as a transaction 
+ group by visits.customer_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT customer_id, COUNT(visit_id) AS count_no_trans
+-- FROM Visits
+-- LEFT JOIN Transactions 
+-- USING(visit_id)
+-- WHERE transaction_id IS NULL 
+-- Group BY customer_id
 
 -- SELECT customer_id, COUNT(v.visit_id) AS count_no_trans
 -- FROM Visits v
 -- LEFT JOIN Transactions t
 -- ON v.visit_id = t.visit_id 
--- -- USING(visit_id)
 -- WHERE t.transaction_id IS NULL 
 -- Group BY v.customer_id
