8b4f5fe919d5a0f5d7e49fa3bdb326acc0cbbe20
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-01-07 13:03:40 -0500

    Time: 9 ms (2.18%) | Memory: 43.5 MB (8.46%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -5,18 +5,31 @@ class Solution {
         dfs(nums, 0, subset, res);
         return res;
     }
+    // subset will be the temporary set that will be added into res(result -> power set)
 
     private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res){
-        if(i >= nums.length){
+        if(i == nums.length){
             res.add(new ArrayList<>(subset));
+            System.out.println("subset that is added to res is: " + subset); 
+            // subset that is added to res is: [1, 2, 3]
+            // subset that is added to res is: [1, 2]
+            // subset that is added to res is: [1, 3]
+            // subset that is added to res is: [1]
+            // subset that is added to res is: [2, 3]
+            // subset that is added to res is: [2]
+            // subset that is added to res is: [3]
+            // subset that is added to res is: []
             return;
         }
 
+        // decision to include the nums[i] in the subset
         subset.add(nums[i]);
         dfs(nums, i + 1, subset, res);
-        // System.out.println("Subset before removing: " + subset);
+        
+        // decision to not include the nums[i] in the subset
+        // System.out.println("Subset before removing: " + subset); // Subset before removing: [1, 2, 3] 
         subset.remove(subset.size() - 1);
-        // System.out.println("Subset after removing: " + subset);
+        // System.out.println("Subset after removing: " + subset); // Subset after removing: [1, 2] -> subset.remove(2) -> here 2 is the index that is in this case number '3'.
         dfs(nums, i + 1, subset, res);
     }
 }
