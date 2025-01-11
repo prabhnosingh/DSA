4011d296c7cbd21c18e2030b665e3469c58a3a34
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-01-10 22:30:11 -0500

    Time: 7 ms (10.87%) | Memory: 43.6 MB (39.97%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -9,7 +9,7 @@ class Solution {
 
     private void dfs(List<List<Integer>> res, List<Integer> currSubset, int[] nums, int start){
         if(start == nums.length){
-            if(!res.contains(currSubset)){
+            if(!res.contains(currSubset)){ // this works as we have sorted the array in the first place
                 res.add(new ArrayList<>(currSubset));
             }
             return;
@@ -18,6 +18,9 @@ class Solution {
         currSubset.add(nums[start]);
         dfs(res, currSubset, nums, start + 1);
 
+        while(start + 1 < nums.length && nums[start] == nums[start + 1]){
+            start ++;
+        }
         currSubset.remove(currSubset.size() - 1);
         dfs(res, currSubset, nums, start + 1);
     }
