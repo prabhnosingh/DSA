c23bfccf82a5ce21a1ae9f30c1a1562d0f2014f8
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-08 19:22:21 -0500

    Time: 459 ms (31.85%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,11 +1,59 @@
 # Write your MySQL query statement below
-SELECT a1.machine_id, 
--- a1.timestamp, 
--- a2.timestamp
-ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
-FROM Activity a1
-JOIN Activity a2
-ON a1.process_id = a2.process_id
-AND a1.machine_id = a2.machine_id
-AND a1.timestamp < a2.timestamp
-GROUP BY a1.machine_id
+
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
+-- a1.timestamp, a2.timestamp
+from activity a1
+join activity a2
+on a1.process_id = a2.process_id
+where a1.machine_id = a2.machine_id
+and a2.timestamp > a1.timestamp
+group by a1.machine_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT a1.machine_id, 
+-- -- a1.timestamp, 
+-- -- a2.timestamp
+-- ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+-- FROM Activity a1
+-- JOIN Activity a2
+-- ON a1.process_id = a2.process_id
+-- AND a1.machine_id = a2.machine_id
+-- AND a1.timestamp < a2.timestamp
+-- GROUP BY a1.machine_id
