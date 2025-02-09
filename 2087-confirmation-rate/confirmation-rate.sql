83c9075ecf7b21ea6c1a062f0c852dc0717e6630
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-09 09:51:22 -0500

    Time: 831 ms (85.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+select s.user_id, 
+case
+when round((select count(c2.action) from confirmations c2 where c2.action = 'confirmed' and c2.user_id = c.user_id group by c2.user_id)/count(s.user_id), 2) is null then 0
+else round((select count(c2.action) from confirmations c2 where c2.action = 'confirmed' and c2.user_id = c.user_id group by c2.user_id)/count(s.user_id), 2)
+
+end as confirmation_rate
+from signups s
+left join confirmations c on s.user_id = c.user_id
+-- where c.action = 'confirmed'
+group by s.user_id
