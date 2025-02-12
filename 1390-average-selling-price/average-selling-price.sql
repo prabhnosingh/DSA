f7b93e731d897e7b2b0cb4ccaae4b6ef06b9cbe1
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-11 21:19:14 -0500

    Time: 1871 ms (16.87%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -5,5 +5,6 @@ select p.product_id, ifnull(round(sum(u.units * p.price) / sum(u.units), 2), 0) 
 from prices p
 left join unitssold u on p.product_id = u.product_id
 and u.purchase_date between p.start_date and p.end_date
--- where p.start_date <= u.purchase_date <= p.end_date 
+-- where u.purchase_date between p.start_date and p.end_date -> This does not work because in case when we have empty unitsSold table, 'where' conditions ...
+-- ...generates empty table after join. But 'and' leads to a table with null values which forms the basis of "If a product does not have any sold units, its average selling price is assumed to be 0."
 group by p.product_id
