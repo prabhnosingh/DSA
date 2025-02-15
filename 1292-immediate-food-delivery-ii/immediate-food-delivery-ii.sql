25e33891a8dc92f15883a2cfb95cb5818cb12561
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-15 13:52:59 -0500

    Time: 621 ms (95.94%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
 
-select  round(sum(customer_pref_delivery_date = (select min(d2.order_date) from delivery d2 where d1.customer_id = d2.customer_id)) * 100
- / count(distinct customer_id), 2) 
- as immediate_percentage 
--- select delivery_id, customer_id, sum(case when customer_pref_delivery_date = min(order_date) then 1 else 0 end) as immediate_percentage 
-from delivery d1
--- group by customer_id
+-- select  round(sum(customer_pref_delivery_date = (select min(d2.order_date) from delivery d2 where d1.customer_id = d2.customer_id)) * 100
+--  / count(distinct customer_id), 2) 
+--  as immediate_percentage 
+-- from delivery d1
+
+select round(avg(customer_pref_delivery_date = order_date) * 100, 2) as immediate_percentage 
+from delivery
+where (customer_id, order_date) in
+(select customer_id, min(order_date) from delivery group by customer_id)
