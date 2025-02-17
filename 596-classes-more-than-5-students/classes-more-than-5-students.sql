80892fcd757edab0d0a45d2e2847cb305f7689f6
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-17 16:13:43 -0500

    Time: 489 ms (75.22%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 
-select class
-from courses c1
-where (select count(student) from courses c2 where c2.class = c1.class  group by class) > 4
+-- select class
+-- from courses c1
+-- where (select count(student) from courses c2 where c2.class = c1.class  group by class) > 4
+-- group by class
+
+select class 
+from courses
 group by class
+having count(student) > 4
