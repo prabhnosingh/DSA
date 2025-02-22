ae7d47bc21462d6223572c083948756c89119015
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-02-22 10:01:33 -0500

    Time: 633 ms (46.99%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,19 @@
 # Write your MySQL query statement below
 
-select id,
+-- select id,
+-- case
+-- when id % 2 = 0 then lag(student) over(order by id) -- in case id is even, fetch the student from previous row
+-- else coalesce(lead(student) over(order by id), student) -- in case id is odd, fetch the student from next row 
+-- -- coaleasce returns first non null value in a list
+-- end as student
+-- from seat
+
+select 
 case
-when id % 2 = 0 then lag(student) over(order by id) -- in case id is even, fetch the student from previous row
-else coalesce(lead(student) over(order by id), student) -- in case id is odd, fetch the student from next row 
--- coaleasce returns first non null value in a list
-end as student
+when id % 2 = 0 then id - 1 -- in case id is even
+when id = (select max(id) from seat order by id) then id -- in case id is the last in table
+else id + 1 -- in case id is odd
+end
+as id, student
 from seat
+order by id
