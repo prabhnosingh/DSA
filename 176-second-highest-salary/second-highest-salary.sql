2b6dd3e0c26d43e180d2a094f1399eaf00a014c4
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-03-01 08:50:31 -0500

    Time: 1080 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -2,4 +2,5 @@
 
 select max(salary) as SecondHighestSalary -- using max() here only to satisfy null condition. max() tends to return null if the value is empty
 from employee e1
-where (select count(distinct(e2.salary)) from employee e2 where e2.salary > e1.salary) = 1
+-- where (select count(distinct(e2.salary)) from employee e2 where e2.salary > e1.salary) = 1
+where salary < (select max(salary) from employee)
