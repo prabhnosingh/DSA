ea20a66f177ab4c54f190cad81616dab2657f729
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-27 08:44:02 -0400

    Time: 1 ms (29.32%) | Memory: 43.7 MB (99.87%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,89 +14,23 @@
  * }
  */
 class Solution {
-    
-
+    //intuition 1: traverse through the whole tree and add elements in a list, then sort and return the kth smallest
+    //intuition 2: Use BST's properties (left < root < right) to optimize
+    List<Integer> list = new ArrayList<>();
     public int kthSmallest(TreeNode root, int k) {
-        
-        Stack<TreeNode> s = new Stack<>();
-        
-        TreeNode curr = root;
-        // stack.push(curr);
-        while(curr != null || !s.isEmpty()){
 
-            while(curr != null){
-                s.push(curr);
-                curr = curr.left;
-            }
+        dfs(root);
 
-            curr = s.pop();
-
-            k --;
-            if(k == 0) return curr.val;
-            curr = curr.right;
-        }
-        return 0;
+        // Arrays.sort 
+        return list.get(k - 1);
 
     }
-
-
-///////////////////////////////////////////////////////////////    
-    // beats 100%
-    // ArrayList<Integer> ans = new ArrayList<>();
-    // public int kthSmallest(TreeNode root, int k) {
-
-    //     traverse(root);
-
-    //     return ans.get(k - 1);
-
-    // }
-
-    // public void traverse(TreeNode root){
-    //     if(root == null){
-    //         return; 
-    //     }
-
-    //     traverse(root.left);
-    //     ans.add(root.val);
-    //     traverse(root.right);
-
-    // }
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////    
-    // beats 8.18 %
-    // ArrayList<Integer> list = new ArrayList<>();
-    // public int kthSmallest(TreeNode root, int k) {
-        
-    //     traverse(root);
-    //     int n = list.size();
-    //     int[] ans = new int[n];
-        
-    //     for(int i = 0; i < n; i ++) ans[i] = list.get(i);
-    //     Arrays.sort(ans);
-
-    //     return ans[k - 1];
-
-    // }
-
-    // public int traverse(TreeNode root){
-    //     if(root == null){
-    //         return -1;
-    //     }
-
-    //     list.add(root.val);
-
-    //     traverse(root.left);
-    //     traverse(root.right);
-        
-    //     return 1;
-        
+    public void dfs(TreeNode root){
+        if(root == null){
+            return;
+        }
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
 }
