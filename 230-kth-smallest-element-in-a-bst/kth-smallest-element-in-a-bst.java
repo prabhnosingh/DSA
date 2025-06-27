08bdfb923cb1aad5afb5033485a2ef7ee067842a
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-27 19:08:42 -0400

    Time: 0 ms (100.00%) | Memory: 44.5 MB (47.41%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,22 +15,51 @@
  */
 class Solution {
     //intuition 1: traverse through the whole tree and add elements in a list, then sort and return the kth smallest
-    //intuition 2: Use BST's properties (left < root < right) to optimize
-    List<Integer> list = new ArrayList<>();
+    // List<Integer> list = new ArrayList<>();
+    // public int kthSmallest(TreeNode root, int k) {
+
+    //     dfs(root);
+
+    //     // Collections.sort(list); // no need of sorting the list as in dfs inorder traversal is being used and in BST
+                                      // this leads to already sorted list
+    //     return list.get(k - 1);
+
+    // }
+    // public void dfs(TreeNode root){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     dfs(root.left);
+    //     list.add(root.val);
+    //     dfs(root.right);
+    // }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //intuition 2: Use BST's properties (left < root < right) to optimize. Traverse to the left most 
+    //node(having left child as null), then start backtracking and decreasing k by 1 with each traversal
+    //and return the node where k == 0
+    int count = 0;
+    int ans = 0;
     public int kthSmallest(TreeNode root, int k) {
-
-        dfs(root);
-
-        // Arrays.sort 
-        return list.get(k - 1);
-
+        // globalK = k;
+        traverse(root, k);
+        return ans; 
+        
     }
-    public void dfs(TreeNode root){
+
+    public void traverse(TreeNode root, int k){
         if(root == null){
             return;
+        }         
+        traverse(root.left, k);
+        count ++;
+        if(k == count){
+            ans = root.val;
+            return;
         }
-        dfs(root.left);
-        list.add(root.val);
-        dfs(root.right);
+        traverse(root.right, k);
+
     }
 }
+
+
+
