94d0303ea196cb061296d4c38cfdd59f45f59200
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-27 22:17:07 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (95.98%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -16,23 +16,58 @@
 // intuition 1: Use dfs to find leaf nodes, keep adidng root.val along the way in a string "path" and when a leaf
 //node is encountered, add the "path" to "ans"
 class Solution {
+    // List<String> ans = new ArrayList<>();
+    // public List<String> binaryTreePaths(TreeNode root) {
+    //     // dfs(root, new StringBuilder());
+    //     dfs(root, "");
+    //     return ans;
+    // }
+    // public void dfs(TreeNode root, String path){ // try with string builder
+    //     if(root == null){
+    //         return;
+    //     }
+    //     path += root.val;
+
+    //     if(root.left == null && root.right == null){ // leafNode encountered
+    //         ans.add(path);
+    //         return;
+    //     }
+    //     path += "->";
+    //     dfs(root.left, path);
+    //     dfs(root.right, path);
+    // }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // intuition 2 (stringbuilder): Use dfs to find leaf nodes, keep adidng root.val along the way in a stringBuilder 
+    // object "path" and when a leaf node is encountered, add the "path" to "ans". Reset the path using setLength to 
+    // previous length to 
+
     List<String> ans = new ArrayList<>();
     public List<String> binaryTreePaths(TreeNode root) {
-        dfs(root, "");
+        dfs(root, new StringBuilder());
         return ans;
     }
-    public void dfs(TreeNode root, String path){ // try with string builder
+
+    public void dfs(TreeNode root, StringBuilder path){ // try with string builder -> this does not work as 
+    // string builder object keeps adding the strings to one place only, resulting in output like this ->
+    // ["1->2->5","1->2->53"]. There is a need of new string object every recursive call rather than a single object.
+
         if(root == null){
             return;
         }
-        path += root.val;
+        int len = path.length();
+        path.append(root.val);
 
         if(root.left == null && root.right == null){ // leafNode encountered
-            ans.add(path);
-            return;
+            ans.add(path.toString());
+          
         }
-        path += "->";
-        dfs(root.left, path);
-        dfs(root.right, path);
+        else{
+            path.append("->");
+            dfs(root.left, path);
+            dfs(root.right, path);
+        }
+        path.setLength(len);
+        
     }
 }
