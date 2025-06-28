d41e7687b64001e8a414965f772dfa5c7a27ee6f
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-28 08:16:20 -0400

    Time: 7 ms (10.03%) | Memory: 84.1 MB (5.18%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,35 +14,38 @@
  * }
  */
 class Solution {
+    // preorder -> root -> left -> right
+    // inorder -> left -> root -> right
+    // intuition 1: Start with preorder[0](master root). Find the index of master root in inorder.
+    // Divide the array in two subarrays, now the left subArray will be left child and right subArray
+    // will be right child. The length of these left and right subArrays (in inorder) will determine how we split 
+    // preoder array
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-
         if(preorder.length == 0 || inorder.length == 0){
             return null;
         }
-        
-        TreeNode root = new TreeNode(preorder[0]);
-        // Integer rootValue = preorder[0]; // Ensure this matches the type in inorder
-        // int mid = Arrays.asList(inorder).indexOf(rootValue);
-        int rootValue = preorder[0];
 
-        // Find the index of root value in the inorder array
+        TreeNode root = new TreeNode(preorder[0]);
+
+        //finding index of root in inorder array
         int mid = -1;
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == rootValue) {
+        for(int i = 0; i < inorder.length; i ++){
+            if(inorder[i] == root.val){
                 mid = i;
                 break;
             }
         }
-        // if(mid == -1){
-        //     root.left = buildTree(Arrays.copyOfRange(preorder, 0, 0), Arrays.copyOfRange(inorder, 0, 0));
-        // }
-        // else{
-        //     root.left = buildTree(Arrays.copyOfRange(preorder, 1, mid + 1), Arrays.copyOfRange(inorder, 0, mid));
-        // }
-        //     root.right = buildTree(Arrays.copyOfRange(preorder, mid + 1, preorder.length), Arrays.copyOfRange(inorder, mid + 1, inorder.length));
-            root.left = buildTree(Arrays.copyOfRange(preorder, 1, mid + 1), Arrays.copyOfRange(inorder, 0, mid));
-            root.right = buildTree(Arrays.copyOfRange(preorder, mid + 1, preorder.length), Arrays.copyOfRange(inorder, mid + 1, inorder.length));
+
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid); // 0 to mid - 1(inclusive) is the left subtree
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1); // starting from 1 because there is 
+        // root at '0' index
+        root.left = buildTree(leftPreorder, leftInorder);
+
+        int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, inorder.length);
+        int[] rightPreorder = Arrays.copyOfRange(preorder, mid + 1, preorder.length);
+
+        root.right = buildTree(rightPreorder, rightInorder);
+
         return root;
     }
-
 }
