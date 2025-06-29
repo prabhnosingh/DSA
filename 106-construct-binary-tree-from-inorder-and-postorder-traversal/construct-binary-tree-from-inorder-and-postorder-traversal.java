2c2488cd5f936b0a351032102e72a63a52e7778f
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-29 09:40:32 -0400

    Time: 1 ms (97.01%) | Memory: 44.8 MB (14.70%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // intuition 1: The last element of postorder will be the master root. The left of root in inorder will be left subtree
+    // the right of root in inorder will be right subtree. Use HashMap to store the indices if inorder elements
+    private HashMap<Integer, Integer> map;
+    private int postOrderIdx;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        postOrderIdx = postorder.length - 1;
+        for(int i = 0; i < inorder.length; i ++){
+            map.put(inorder[i], i);
+        }
+
+        return build(postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode build(int[] postorder, int start, int end){
+        if(start > end){
+            return null;
+        }
+
+        int rootVal = postorder[postOrderIdx --]; // 3
+        TreeNode root = new TreeNode(rootVal);
+        int mid = map.get(rootVal); // 1
+
+        root.right = build(postorder, mid + 1, end); // (4 - 1, 1)
+        root.left = build(postorder, start, mid - 1);
+        return root;
+    }
+}
