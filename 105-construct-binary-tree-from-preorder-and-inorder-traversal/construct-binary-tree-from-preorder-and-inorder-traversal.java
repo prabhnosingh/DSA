9b014a2685375a0dd558805b3d8e52c486aa3639
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-06-29 07:52:08 -0400

    Time: 7 ms (10.05%) | Memory: 84.3 MB (5.21%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,33 +19,82 @@ class Solution {
     // intuition 1: Start with preorder[0](master root). Find the index of master root in inorder.
     // Divide the array in two subarrays, now the left subArray will be left child and right subArray
     // will be right child. The length of these left and right subArrays (in inorder) will determine how we split 
-    // preoder array
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0 || inorder.length == 0){
+    // preoder array 
+
+    //************Optimize this*********************
+    // public TreeNode buildTree(int[] preorder, int[] inorder) {
+    //     if(preorder.length == 0 || inorder.length == 0){
+    //         return null;
+    //     }
+
+    //     TreeNode root = new TreeNode(preorder[0]);
+
+    //     //finding index of root in inorder array
+    //     int mid = -1;
+    //     for(int i = 0; i < inorder.length; i ++){
+    //         if(inorder[i] == root.val){
+    //             mid = i;
+    //             break;
+    //         }
+    //     }
+
+    //     int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid); // 0 to mid - 1(inclusive) is the left subtree
+    //     int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1); // starting from 1 because there is 
+    //     // root at '0' index
+    //     root.left = buildTree(leftPreorder, leftInorder);
+
+    //     int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, inorder.length);
+    //     int[] rightPreorder = Arrays.copyOfRange(preorder, mid + 1, preorder.length);
+
+    //     root.right = buildTree(rightPreorder, rightInorder);
+
+    //     return root;
+    // }
+
+
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // inuition 2: use recursive approach to build left and right sub trees by dividing the arrays
+    public TreeNode buildTree(int[] preorder, int[] inorder){
+        if(preorder.length == 0){
             return null;
         }
-
         TreeNode root = new TreeNode(preorder[0]);
 
-        //finding index of root in inorder array
-        int mid = -1;
+        int rootIdxInInorder = 0;
         for(int i = 0; i < inorder.length; i ++){
-            if(inorder[i] == root.val){
-                mid = i;
+            if(preorder[0] == inorder[i]){
+                rootIdxInInorder = i;
                 break;
             }
         }
 
-        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid); // 0 to mid - 1(inclusive) is the left subtree
-        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1); // starting from 1 because there is 
-        // root at '0' index
+        int mid = rootIdxInInorder; //in inorder array, left of this index is the left subtree and right is right subtree
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1);
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid + 1);
         root.left = buildTree(leftPreorder, leftInorder);
 
-        int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, inorder.length);
         int[] rightPreorder = Arrays.copyOfRange(preorder, mid + 1, preorder.length);
-
+        int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, preorder.length);
         root.right = buildTree(rightPreorder, rightInorder);
 
         return root;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
