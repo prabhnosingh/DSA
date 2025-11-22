d95e182805d017cb524ebf878ee1d675a7bcf584
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-11-22 10:11:37 -0500

    Time: 22 ms (5.51%) | Memory: 45.9 MB (8.53%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.java
@@ -2,7 +2,9 @@ class Solution {
 
     //Solving on 22 Nov 2025
 
-    //Intuition 1: To maximize the sum:
+    //intuition 2: using arrays.sort after each negation
+    
+    //To maximize the sum:
         //if there are negative numbers, the largest negative element should be converted to a positive element.
         //if there are positive numbers only, the smallest element should be convereted to a negative element.
 
@@ -11,33 +13,69 @@ class Solution {
                 //are no more negative numbers
             //After negating the element, offer back to the heap and for remaining k do the negation for smallest elements
 
-    //TC: O(nlogn + nlogn) (insert + remove) 
-    //SC: O(n)
+    //TC: O(klogn) (negations) 
+    //SC: O(1)
     public int largestSumAfterKNegations(int[] nums, int k) {
         
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> (a - b));
-
-        for(int num : nums){
-            minHeap.offer(num);
-        }
 
         while(k != 0){
-            int currNum = minHeap.remove();
-
-            if(currNum == 0){
+            Arrays.sort(nums);
+            if(nums[0] == 0){
                 break;
             }
-            currNum = currNum * -1;
-            minHeap.offer(currNum);
+            nums[0] = nums[0] * -1;
+            
             k --;
         }
-
+        
         int sum = 0;
-        while(!minHeap.isEmpty()){
-            sum += minHeap.remove();
+        for(int num : nums){
+            sum += num;
         }
-
         return sum;
 
     }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 22 Nov 2025
+
+    // //intuition 1: To maximize the sum:
+    //     //if there are negative numbers, the largest negative element should be converted to a positive element.
+    //     //if there are positive numbers only, the smallest element should be convereted to a negative element.
+
+    //     //Use minheap to store the elements. Remove the top most element:
+    //         //if a 0 is found, then apply all the negations to it. This makes sense as now we know that there
+    //             //are no more negative numbers
+    //         //After negating the element, offer back to the heap and for remaining k do the negation for smallest elements
+
+    // //TC: O(nlogn + nlogn + klogn) (insert + negations + remove) 
+    // //SC: O(n)
+    // public int largestSumAfterKNegations(int[] nums, int k) {
+        
+    //     PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> (a - b));
+
+    //     for(int num : nums){
+    //         minHeap.offer(num);
+    //     }
+
+    //     while(k != 0){
+    //         int currNum = minHeap.remove();
+
+    //         if(currNum == 0){
+    //             break;
+    //         }
+    //         currNum = currNum * -1;
+    //         minHeap.offer(currNum);
+    //         k --;
+    //     }
+
+    //     int sum = 0;
+    //     while(!minHeap.isEmpty()){
+    //         sum += minHeap.remove();
+    //     }
+
+    //     return sum;
+
+    // }
 }
