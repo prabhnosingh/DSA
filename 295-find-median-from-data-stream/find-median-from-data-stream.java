bef8363fa0d58d549c4b2225a45234bf952aa37e
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-11-23 12:22:57 -0500

    Time: 170 ms (17.30%) | Memory: 112.1 MB (5.15%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,91 +1,94 @@
+
+//Re-solving on 23 Nov 2025
+
+//intuition 2 (TLE): Maintain two heaps, one min Heap and one max Heap. 
+    //Every time addNum is called add to max heap
+    //The remove the top elemnt from maxHeap and add it to the minHeap
+    //If minHeap.size() > maxHeap.size(). Remove the top from min and add it to max to
+        //maintain the median at top of maxHeap 
+    //If findMedian is called:
+        //if both heaps are of equal size, then return average of tops of both heaps
+        //if the size of maxHeap is greater than minHeap, return top to maxHeap
+
 class MedianFinder {
 
-    PriorityQueue<Integer> smallHeap;
-    PriorityQueue<Integer> bigHeap;
+    PriorityQueue<Integer> first_half;
+    PriorityQueue<Integer> second_half;
 
     public MedianFinder() {
-        smallHeap = new PriorityQueue<>(Collections.reverseOrder()); // max heap
-        bigHeap = new PriorityQueue<>(); // min heap
+        first_half = new PriorityQueue<>((a,b) -> (b-a)); //max Heap
+        second_half = new PriorityQueue<>((a,b) -> (a-b)); //min Heap
     }
-
+    
     public void addNum(int num) {
+        first_half.offer(num);
+        second_half.offer(first_half.remove());
 
-        if(smallHeap.size() != 0 && bigHeap.size() != 0){
-            if(num >= bigHeap.peek()){
-                bigHeap.offer(num);
-            }
-    
-            else{
-                smallHeap.offer(num);
-            }
+        if(second_half.size() > first_half.size()){
+            first_half.offer(second_half.remove());
         }
-        else{
-            smallHeap.offer(num);
-        }
-        
-        int smallHeapLen = smallHeap.size();
-        int bigHeapLen = bigHeap.size();
-
-        if(Math.abs(smallHeapLen - bigHeapLen) > 1){
-            if(smallHeapLen > bigHeapLen){
-                bigHeap.offer(smallHeap.poll());
-            }
-            else{
-                smallHeap.offer(bigHeap.poll());
-            }
-        }
-
     }
-
+    
     public double findMedian() {
-        int smallHeapLen = smallHeap.size();
-        int bigHeapLen = bigHeap.size();
-
-        double med = 0.0;
-
-        int lenDiff = smallHeapLen - bigHeapLen;
-
-        if(lenDiff == 0){
-            med = (smallHeap.peek() + bigHeap.peek()) / 2.0;
-        }
-        else{
-            if(lenDiff < 0){
-                med = bigHeap.peek();
-            }
-            else{
-                med = smallHeap.peek();
-            }
-        }
-
-        return med;
-
+       if(first_half.size() == second_half.size()){ //even numbers
+            return (double)(first_half.peek() + second_half.peek()) / 2;
+       }
+       else{
+        return (double) first_half.peek();
+       }
     }
 
-// TLE 
-    // ArrayList<Integer> numList = new ArrayList<>();;
-    // public MedianFinder() {
-    //     // numList = new ArrayList<>();
-    // }
-    
-    // public void addNum(int num) {
-    //     numList.add(num);
-    //     Collections.sort(numList);
-    // }
-    
-    // public double findMedian() {
-    //     double med = 0.0;
-    //     int len  = numList.size();
-    //     if(len % 2 != 0){
-    //         med = numList.get(len / 2);
-    //     }
-    //     else{
-    //         med = (numList.get(len / 2) + numList.get((len /2) - 1)) / 2.0;
-    //         // System.out.println(numList.get(len / 2) + numList.get((len /2) - 1));
-    //     }
-
-    //     return med;
-    // }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// //Re-solving on 23 Nov 2025
+
+// //intuition 1 (TLE): Maintain two heaps, one min Heap and one max Heap. 
+// //Every time addNum is called add to max heap
+// //If the findMeian is called, remove the elements from maxHeap and add it to the minHeap, until 
+// //the size of both the heaps become equal or size of minHeap exeeds.
+//     //If the size of both the heaps become equal, take peak from both the heaps and return the mean
+//     //If the size of minHeap is greater, take a peek from minHeap and return that
+// //top of both and 
+
+// class MedianFinder {
+
+//     PriorityQueue<Integer> minHeap;
+//     PriorityQueue<Integer> maxHeap;
+
+//     public MedianFinder() {
+//         minHeap = new PriorityQueue<>((a,b) -> (a-b));
+//         maxHeap = new PriorityQueue<>((a,b) -> (b-a));
+//     }
+    
+//     public void addNum(int num) {
+//         maxHeap.offer(num);
+//     }
+    
+//     public double findMedian() {
+//         while(minHeap.size() < maxHeap.size()){
+//             minHeap.offer(maxHeap.remove());
+//         }
+//         double median = 0.0; 
+//         if(minHeap.size() == maxHeap.size()){
+//             median = (double) (minHeap.peek() + maxHeap.peek()) / 2;
+//         }
+
+//         else{ //minHeap.size() > maxHeap.size();
+//             median = (double) minHeap.peek();
+//         }
+        
+//         //adding back the elements from minHeap to maxHeap
+//         while(!minHeap.isEmpty()){
+//             maxHeap.offer(minHeap.remove());
+//         }
+
+//         return median;
+//     }
+
+// }
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
