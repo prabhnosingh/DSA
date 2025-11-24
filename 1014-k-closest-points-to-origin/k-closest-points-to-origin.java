b7bb5497e4b7640c643a3f78b77530e588ffdd84
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-11-24 07:58:56 -0500

    Time: 134 ms (5.00%) | Memory: 61.8 MB (5.28%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,58 +1,245 @@
 class Solution {
+    
+    //Re-solving on 24 Nov 2025:
+
+    //intuition 1: Have a max heap of size k that takes arraylist of integers and compares a.get(0)^2 + a.get(0)^2
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<List<Integer>> maxHeap = new PriorityQueue<>((b,a) -> (
+            (a.get(0)*a.get(0) + a.get(1)*a.get(1)) 
+            - (b.get(0)*b.get(0) + b.get(1)*b.get(1))));
+        int[][] kClosest = new int[k][2];
+        int idx = 0;
         
-
-        // Comparator<int[]> comp = new Comparator<int[]>(){
-        //     @Override
-        //     public int compare(int[] first, int[] second){
-        //         int firstDist = first[0] * first[0] + first[1] * first[1]; 
-        //         int secondDist = second[0] * second[0] + second[1] * second[1]; 
-        //         return Integer.compare(firstDist, secondDist);
-        //     }
-        // };
-        // Beats 74.36 %
-        // PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>(comp);
-        
-        // Beats 89.59 %
-        PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((int[] p1, int[] p2) -> (p1[0] * p1[0] + p1[1] * p1[1]) -(p2[0] * p2[0] + p2[1] * p2[1]));
         for(int[] point : points){
-            minHeap.offer(point);
+            maxHeap.offer(new ArrayList(Arrays.asList(point[0], point[1])));
+            
+            if(maxHeap.size() > k) maxHeap.remove(); //to keept the size upto k only
+        }
+        
+
+        while(!maxHeap.isEmpty()){
+            List<Integer> currPoint = maxHeap.remove();
+
+            int[] point = {currPoint.get(0), currPoint.get(1)};
+            kClosest[idx ++] = point;
         }
 
-        int[][] ans = new int[k][2];
-        k --;
-        while(k >= 0 && minHeap.size() != 0){
-            // System.out.println(minHeap.size());
-            ans[k --] = minHeap.poll();
-        }
-
-        return ans;
-
-//***************************** Beats 19.79 %*************************************************************** */
-
-//     PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparing(a -> a[0])); 
-//     // a -> a[0] means that a (which is an array of integers) will be compared based on the first element of the array(which is a[0])
-
-//     for(int[] point : points){
-//         int dist = point[0] * point[0] + point[1] * point[1];
-
-//         minHeap.offer(new int[]{dist, point[0], point[1]});
-//     }
-//     int[][] res = new int[k][2];
-//     k --;
-//     while(k >= 0 && minHeap.size() != 0){
-//         int[] temp = minHeap.poll();
-//         // res[k][0] = temp[1];
-//         // res[k --][1] = temp[2];
-
-//         res[k --] = new int[]{temp[1], temp[2]};
-//     }
-
-// return res;
-
-
-
-
+        return kClosest;
 
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // intuition 1: use minHeap to store coordinates and distance (dist, x, y) and sort based on dist for all points 
+    // then poll first k and return
+    // public int[][] kClosest(int[][] points, int k) {
+    //     int[][] ans = new int[k][2];
+    //     PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0])); // a[0] denotes dist for point 'a' 
+    //     // from origin 
+        
+    //     for(int[] point : points){
+    //         int dist = (int) (Math.pow(point[0], 2) + Math.pow(point[1], 2)); // we do not need square root as we are only
+    //         // comparing and do not care about exact values
+    //         // using (int) as Math.pow's return value is double and in any given case we cannot have two decimal 
+    //         // coordinates(because points is of type int[][]), therefore we do not loose any decimal values by 
+    //         // doing '(int)' and it safely assigns the value to variable 'dist'
+
+    //         minHeap.offer(new int[]{dist, point[0], point[1]}); 
+    //     }
+
+    //     for(int i = 0; i < k; i ++){
+    //         int[] temp = minHeap.poll();
+    //         ans[i] = new int[]{temp[1], temp[2]}; // x, y
+    //     }
+    //     return ans;
+    // }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//     //intuition 2: use custom comparator
+//     public int[][] kClosest(int[][] points, int k) {
+//         int[][] ans = new int[k][2];
+//         Comparator<int[]> comp = new Comparator<int[]>(){
+//             @Override
+//             public int compare(int[] first, int[] second){
+//                 int firstDist = first[0] * first[0] + first[1] * first[1];
+//                 int secondDist = second[0] * second[0] + second[1] * second[1];
+//                 return Integer.compare(firstDist, secondDist);
+//              }
+//         };
+
+//         PriorityQueue<int[]> minHeap = new PriorityQueue<>(comp);
+
+//         for(int[] point : points){
+//             minHeap.offer(point);
+//         }
+
+//         int i = 0;
+//         while (i < k){
+//             ans[i ++] = minHeap.poll();
+//         }
+//         return ans;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+    //Re-solving: intuition 1: We can solve this problem by using minHeap (priority Queue) that
+    //takes x and y coordinates, and sorts different points by using a custom comparator
+    //and comparing distance of each point from origin. At the last, output k points by polling
+    //k elements from the min heap
+
+    //first solve it with min heap and then other approaches (divide and conquer, QuickSelect, 
+    //Sorting etc.)
+
+    //TC: O(nlogn (insertion) + nlogk (polling)) = O(nlogn) -> where n is the number of points
+    //SC: O(n) -> max size of heap 
+    // public int[][] kClosest(int[][] points, int k) {
+    //     int[][] ans = new int[k][2];
+    //     PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> ((a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])));
+
+
+    //     for(int[] point : points){
+    //         minHeap.offer(point);
+    //     }
+
+    //     int idx = 0;
+    //     while(k > 0){
+    //         ans[idx] = minHeap.poll();
+    //         // System.out.println(ans[idx]);
+    //         k --; 
+    //         idx ++;
+    //     }
+    //     return ans;
+    // }
+
+
+    // //doing with maxheap to reduce TC
+
+    // //TC: O(nlog)
+    // //SC:
+    // public int[][] kClosest(int[][] points, int k) {
+    //     int[][] ans = new int[k][2];
+    //     PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> ((b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])));
+
+
+    //     for(int[] point : points){
+    //         maxHeap.offer(point);
+    //         if(maxHeap.size() == k + 1){
+    //             maxHeap.poll();
+    //         }
+    //     }
+
+    //     int idx = 0;
+    //     while(k > 0){
+    //         ans[idx] = maxHeap.poll();
+    //         // System.out.println(ans[idx]);
+    //         k --; 
+    //         idx ++;
+    //     }
+    //     return ans;
+    // }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
