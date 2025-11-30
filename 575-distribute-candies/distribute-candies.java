9bbb51f48e97ae24e53c8ae82e9eb2853dc11011
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-11-30 12:53:07 -0500

    Time: 11 ms (97.53%) | Memory: 48.5 MB (28.83%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,40 +1,112 @@
 class Solution {
-    //Solving on 29 Nov 2025:
+    //Solving on 30 Nov 2025:
 
-    //intuition 2: Have a freqArray count the different types of candies. Then find maxCandiesAllowed = candyType / 2.
-    //Then find maxCandyTypesPossible = Math.min(uniqueCandyTypes, maxCandiesAllowed)
+    //intuition 1: As Alice has to eat n/2 candies, the max number of different types of candies    
+        //will be Math.min(typeOfCandies, n/2)
+        //To find the typeOfCandies, you can use a hashSet or frequency array.
+            //HashSet will be too easy and computationaly heavy, go with freq array
+            //Count the entries in freq array that have values greater than 0 to get the 
+                //number of different types of candies
     public int distributeCandies(int[] candyType) {
-        int[] candyFreqArray = new int[candyType.length];
+        
+        int maxCandyType = Integer.MIN_VALUE;
+        int minCandyType = Integer.MAX_VALUE;
 
-        int maxCandy = Integer.MIN_VALUE;
-        int minCandy = Integer.MAX_VALUE;
-
-        for(int candy : candyType){ 
-            maxCandy = Math.max(maxCandy, candy);
-            minCandy = Math.min(minCandy, candy);
+        for(int candy : candyType){
+            minCandyType = Math.min(minCandyType, candy);
+            maxCandyType = Math.max(maxCandyType, candy);
         }
 
-        int[] freqArr = new int[maxCandy - minCandy + 1];
-        for(int candy : candyType){ 
-            freqArr[candy - minCandy] ++;
+        int[] freqArray = new int[maxCandyType - minCandyType + 1];
+        
+        for(int candy : candyType){
+            freqArray[candy - minCandyType] += 1;
         }
 
-        int uniqueCandyTypes = 0;
-        for(int freq : freqArr){
+
+        int differentCandyTypes = 0;
+        for(int freq : freqArray){
             if(freq > 0){
-                uniqueCandyTypes += 1;
+                differentCandyTypes += 1;
             }
         }
 
-
-
-        return Math.min(uniqueCandyTypes, candyType.length / 2);
+        return Math.min(candyType.length / 2 , differentCandyTypes);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 29 Nov 2025:
+
+    // //intuition 2 (98.67%): Have a freqArray count the different types of candies. Then find maxCandiesAllowed = candyType / 2.
+    // //Then find maxCandyTypesPossible = Math.min(uniqueCandyTypes, maxCandiesAllowed)
+    // public int distributeCandies(int[] candyType) {
+    //     int[] candyFreqArray = new int[candyType.length];
+
+    //     int maxCandy = Integer.MIN_VALUE;
+    //     int minCandy = Integer.MAX_VALUE;
+
+    //     for(int candy : candyType){ 
+    //         maxCandy = Math.max(maxCandy, candy);
+    //         minCandy = Math.min(minCandy, candy);
+    //     }
+
+    //     int[] freqArr = new int[maxCandy - minCandy + 1];
+    //     for(int candy : candyType){ 
+    //         freqArr[candy - minCandy] ++;
+    //     }
+
+    //     int uniqueCandyTypes = 0;
+    //     for(int freq : freqArr){
+    //         if(freq > 0){
+    //             uniqueCandyTypes += 1;
+    //         }
+    //     }
+
+
+
+    //     return Math.min(uniqueCandyTypes, candyType.length / 2);
+    // }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // //Solving on 29 Nov 2025:
 
-    // //intuition 1: Have a hashSet to count the different types of candies. Then find maxCandiesAllowed = candyType / 2.
+    // //intuition 1 (30.96%): Have a hashSet to count the different types of candies. Then find maxCandiesAllowed = candyType / 2.
     // //Then find maxCandyTypesPossible = Math.min(set.size, maxCandiesAllowed)
     // public int distributeCandies(int[] candyType) {
     //     HashSet<Integer> candyTypeSet = new HashSet<>();
