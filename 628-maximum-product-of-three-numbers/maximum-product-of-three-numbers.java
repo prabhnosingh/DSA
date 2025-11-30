8e626d097c6d1f2110ea53194ce7d3a32b173e4f
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-11-30 14:49:41 -0500

    Time: 2 ms (99.72%) | Memory: 48 MB (30.50%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -2,23 +2,74 @@ class Solution {
 
     //Solving on 30 Nov 2025:
 
-    //intuition 1: To make the product maximum we have to choose numbers that give max product.
-    //Sort the array, if it has negatives, the smallest numbers (big negatives) will be at extreme
+    //intuition 2(without sorting): To make the product maximum we have to choose numbers that give max product.
+    //If it has negatives, the smallest numbers (big negatives) will be at extreme
     //left and the largest numbers (big positives) will be at extreme right. 
 
     //So we can compute two different products and return the maximum of them:
         //Two left extreme nums * 1 right extreme nums 
         //Three right extreme nums 
 
+
+    //The logic of finding maxProd from two different scenarios remain the same but we do not sort the array,
+        //and instead find the all the different five numbers (max1 (nums[nLen - 1]), max2 (nums[nLen - 2]), 
+        //max3 (nums[nLen - 3]), min1 (nums[0]) and min2 (nums[1]))
+
+
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);  
-        int nLen = nums.length;
+        int min1 = Integer.MAX_VALUE, min2 = min1, max1 = Integer.MIN_VALUE, max2 = max1, max3 = max1;
 
-        int case1Prod = nums[0] * nums[1] * nums[nLen - 1];
-        int case2Prod = nums[nLen - 1] * nums[nLen - 2] * nums[nLen - 3];
+        for(int num : nums){
+            if(num <= min1){
+                min2 = min1;
+                min1 = num;
+                // min2 = min1;
+            }
+            else if(num <= min2){
+                min2 = num;
+            }
 
-        return Math.max(case1Prod, case2Prod);
+            if(num >= max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num >= max2){
+                max3 = max2;
+                max2 = num;
+            }
+            else if(num >= max3){
+                max3 = num;
+            }
+        }
+
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+
+
     }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 30 Nov 2025:
+
+    // //intuition 1 (sorting): To make the product maximum we have to choose numbers that give max product.
+    // //Sort the array, if it has negatives, the smallest numbers (big negatives) will be at extreme
+    // //left and the largest numbers (big positives) will be at extreme right. 
+
+    // //So we can compute two different products and return the maximum of them:
+    //     //Two left extreme nums * 1 right extreme nums 
+    //     //Three right extreme nums 
+
+    // public int maximumProduct(int[] nums) {
+    //     Arrays.sort(nums);  
+    //     int nLen = nums.length;
+
+    //     int case1Prod = nums[0] * nums[1] * nums[nLen - 1];
+    //     int case2Prod = nums[nLen - 1] * nums[nLen - 2] * nums[nLen - 3];
+
+    //     return Math.max(case1Prod, case2Prod);
+    // }
 
 
 
