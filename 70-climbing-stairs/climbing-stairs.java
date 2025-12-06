dddf6d06abc777ee8cf15ef768e496c40c65b0c1
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-05 19:15:36 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (26.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,39 +1,123 @@
 class Solution {
 
-    //Re-solving on 02 Dec 2025:
+    //Re-solving on 05 Dec 2025:
 
-    //intuition 2(Space-optimized DP: using three pointers): At every step we have two options, 
-    //either to take 1 step or 2 step. 
-        //At each step i the number of possible steps to reach i are combinations to reach [i-1] 
-            //step + combinations to reach [i-2] step
-        //As from i-2 step we can go either i-1 (1 step) or i (2 steps), and from i-1 step 
-            //can only go to i (1 step). Hence, in total 3 ways. If we take 2 steps from i-1
-            //step, then we will reach i+1 step, which is out of bounds. 
-
-    //Combinations to reach at a particular stair depends on previous two stair combinations, becuase
-        //to reach a paricular stair, we can only come from previous two steps because we are only
-        //allowed to either take 1 step or 2 steps
-
-    // When i == 0, there are total of 1 ways to reach at top: There is only 1 way to stay at top.
-        //Doing nothing is also 1 way
-
+    //intuition 1 (dp array): 
+    //Total ways to reach top by taking i steps will be the addition of (total ways to reach i - 1 steps) + (total
+        //ways to reach i - 2 steps). Because there are two ways to reach ith step:
+            //1. Either jump 1 from step i - 1
+            //2. Or jump 2 from step i - 2 (jumpt form "i - 2 to i - 1 to i" is already covered in the total ways to jump from i - 1)
     public int climbStairs(int n) {
-        int prevPrev = 1; //n == 0
-        int prev = 1; //n == 1
-        if(n == 1) {
-            return 1;
-        }
-        int curr = prevPrev + prev;
-        for(int i = 3; i <= n; i ++){
-            prevPrev = prev;
-            prev = curr;
-            curr = prevPrev + prev;
+        //dp[i] will represent the total ways to climb to top when steps are i
+
+        int[] dp = new int[n + 1]; 
+
+        // dp[0] = 1;
+        if(n == 1) return 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < n + 1; i ++){
+            dp[i] += dp[i - 1];
+            dp[i] += dp[i - 2];
+
         }
 
-        return curr;
-        
+        return dp[n];
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Re-solving on 02 Dec 2025:
+
+    // //intuition 2(Space-optimized DP: using three pointers): At every step we have two options, 
+    // //either to take 1 step or 2 step. 
+    //     //At each step i the number of possible steps to reach i are combinations to reach [i-1] 
+    //         //step + combinations to reach [i-2] step
+    //     //As from i-2 step we can go either i-1 (1 step) or i (2 steps), and from i-1 step 
+    //         //can only go to i (1 step). Hence, in total 3 ways. If we take 2 steps from i-1
+    //         //step, then we will reach i+1 step, which is out of bounds. 
+
+    // //Combinations to reach at a particular stair depends on previous two stair combinations, becuase
+    //     //to reach a paricular stair, we can only come from previous two steps because we are only
+    //     //allowed to either take 1 step or 2 steps
+
+    // // When i == 0, there are total of 1 ways to reach at top: There is only 1 way to stay at top.
+    //     //Doing nothing is also 1 way
+
+    // public int climbStairs(int n) {
+    //     int prevPrev = 1; //n == 0
+    //     int prev = 1; //n == 1
+    //     if(n == 1) {
+    //         return 1;
+    //     }
+    //     int curr = prevPrev + prev;
+    //     for(int i = 3; i <= n; i ++){
+    //         prevPrev = prev;
+    //         prev = curr;
+    //         curr = prevPrev + prev;
+    //     }
+
+    //     return curr;
+        
+
+    // }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
