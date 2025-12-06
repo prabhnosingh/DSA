fc4e1ca3dfb37d83ab7088ee912e06382f3ce692
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-05 23:36:24 -0500

    Time: 0 ms (100.00%) | Memory: 46.2 MB (45.74%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -1,12 +1,14 @@
 class Solution {
-    //intuition 1:
+    //intuition 1: Start from index 0, the first index to match the condition is the answer
     public int smallestEqual(int[] nums) {
         int minIdx = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i ++){
             if(i % 10 == nums[i]){
-                minIdx = Math.min(minIdx, i);
+                // minIdx = Math.min(minIdx, i);
+                return i;
             }
         }
-    return minIdx == Integer.MAX_VALUE ? -1 : minIdx;
+    // return minIdx == Integer.MAX_VALUE ? -1 : minIdx;
+    return -1;
     }
 }
