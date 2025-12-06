5de3d9fc01375b0af18316502651f98602be7a24
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-06 10:18:31 -0500

    Time: 0 ms (100.00%) | Memory: 42.7 MB (40.31%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,39 +1,26 @@
 class Solution {
+    //Re-solving on 06 Dec 2025:
+
+    //intuition 1: 
+    //Maintain a dp array that tracks the money stolen till now. At each step we have two choices, either to steal the 
+        //house or not steal the house. 
+    //dp[i] = Math.max(dp[i - 2] + nums[i], dp[i-1]) => dp[i-1] is when we do not want to rob the current house at i
+    // At last return max (dp[n-1], dp[n-2])
     public int rob(int[] nums) {
-
-//    if (nums.length == 0) {
-//             return 0;
-//         }
-//         if (nums.length == 1) {
-//             return nums[0];
-//         }
-
-//         int now = 0;
-//         int last = 0;
-
-//         for(int i = 0; i < nums.length; i++){
-//             int temp = last;
-//             last = now;
-//             now = Math.max(now, nums[i] + temp);
-//         }
-//         return now;
-//     }
-    
-//****************************************************************** */
-         if(nums.length==0) return 0;
-         if(nums.length==1) return nums[0];
-        
-        int[] dp = new int[nums.length];
+        //dp[i] will represent the max money stolen till now
+        int nLen = nums.length;
+        if(nLen == 1) return nums[0];
+        int[] dp = new int[nLen];
 
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        dp[1] = Math.max(nums[1], nums[0]);
 
-        for(int i = 2; i < nums.length; i++){
-
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);            
+        for(int i = 2; i < nLen; i ++){
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
 
-        return dp[nums.length-1];
-    }
+        return Math.max(dp[nLen - 1], dp[nLen - 2]);
 
+
+    }
 }
