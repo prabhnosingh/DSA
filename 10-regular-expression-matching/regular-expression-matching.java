4c46f900e1a3b51c64fd77cbaffa5b388a46b381
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-10 20:51:34 -0500

    Time: 5 ms (25.12%) | Memory: 44.6 MB (9.75%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,7 +1,7 @@
 class Solution {
 
     //Solving on 10 Dec 2025
-    //intuition 2 (brute force): 
+    //intuition 2 (Top-Down memoization solution using a cache): use a hashmap for caching your results 
         //"a*" -> means possible strings that this pattern could match to are ["", "a", "aa", "aaa", .....]
             //empty string because 'a' character could be repeated 0 times
         //".*" -> means possible strings that this pattern could match to are ["", ".", "..", "..." (triple dot), .....]
@@ -21,14 +21,19 @@ class Solution {
             //
 
     public boolean isMatch(String str, String pat) {
-      
-        return dfs(str, pat, 0, 0);        
+        HashMap<String, Boolean> cacheMap = new HashMap<>(); 
+        return dfs(str, pat, cacheMap, 0, 0);        
     }
 
     //i is the pointer for string str
     //j is the pointer for string pat
-    private boolean dfs(String str, String pat, int i, int j){
+    private boolean dfs(String str, String pat, HashMap<String, Boolean> cacheMap, int i, int j){
         //base cases
+        String cacheKey = i + "," + j;
+        if(cacheMap.containsKey(cacheKey)){
+            return cacheMap.get(cacheKey);
+        }
+
         if(i >= str.length() && j >= pat.length()){
             return true;
         }
@@ -42,20 +47,84 @@ class Solution {
 
         //checking if the j + 1 char in pat is a "*"
         if(j + 1 < pat.length() && pat.charAt(j + 1) == '*'){
-            // return (dfs(str, pat, i, j + 2) || //choose a star only in case of a match
-            //        (matchVar && dfs(str, pat, i + 1, j)));   //not choosing a star and skipping to next char 
-            return ((matchVar && dfs(str, pat, i + 1, j)) || //choose a star only in case of a match
-                   dfs(str, pat, i, j + 2));   //not choosing a star and skipping to next char 
+
+            boolean cacheVal = ((matchVar && dfs(str, pat, cacheMap, i + 1, j)) || //choose a star only in case of a match
+                               dfs(str, pat, cacheMap, i, j + 2));   //not choosing a star and skipping to next char 
+
+            cacheMap.put(cacheKey, cacheVal);
+            return cacheVal;
         }
 
         if(matchVar){
-            return dfs(str, pat, i + 1, j + 1);
+            boolean cacheVal = dfs(str, pat, cacheMap, i + 1, j + 1);
+            cacheMap.put(cacheKey, cacheVal);
+            return cacheVal;
         }
 
+        cacheMap.put(cacheKey, false);
         return false;
 
 
     }
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 10 Dec 2025
+    // //intuition 2 (brute force): 
+    //     //"a*" -> means possible strings that this pattern could match to are ["", "a", "aa", "aaa", .....]
+    //         //empty string because 'a' character could be repeated 0 times
+    //     //".*" -> means possible strings that this pattern could match to are ["", ".", "..", "..." (triple dot), .....]
+
+    //     //Brute force:
+    //         //anytime we encounter a start we are going to have 2 different decisions:
+    //             //1. choose to use the star and repeat the preceding character
+    //             //2. do not repeat the star and move to the next character in pattern
+    //         //TC: O(2^n) where n is the length of the string 
+        
+    //     //DP (Top-down memoization):
+    //         //we can solve this problem by DP using a cache and bring down the TC to O(n.m) where n and m are the lens of
+    //             //string and pattern
+
+
+    //     //DP (Bottom-up: Full DP solution):
+    //         //
+
+    // public boolean isMatch(String str, String pat) {
+      
+    //     return dfs(str, pat, 0, 0);        
+    // }
+
+    // //i is the pointer for string str
+    // //j is the pointer for string pat
+    // private boolean dfs(String str, String pat, int i, int j){
+    //     //base cases
+    //     if(i >= str.length() && j >= pat.length()){
+    //         return true;
+    //     }
+
+    //     if(j >= pat.length()){ //pattern is fully traversed but str is still not traversed
+    //         return false;
+    //     }
+
+    //     //if string character matches with pattern character or string character is a "." (dot) then matchVar will be true
+    //     boolean matchVar = (i < str.length() && (str.charAt(i) == pat.charAt(j) || pat.charAt(j) == '.'));
+
+    //     //checking if the j + 1 char in pat is a "*"
+    //     if(j + 1 < pat.length() && pat.charAt(j + 1) == '*'){
+    //         return ((matchVar && dfs(str, pat, i + 1, j)) || //choose a star only in case of a match
+    //                dfs(str, pat, i, j + 2));   //not choosing a star and skipping to next char 
+    //     }
+
+    //     if(matchVar){
+    //         return dfs(str, pat, i + 1, j + 1);
+    //     }
+
+    //     return false;
+
+
+    // }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
