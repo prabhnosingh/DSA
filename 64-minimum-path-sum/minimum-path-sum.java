8f48e44f3e7ec411289e567d78ab365a3ec8425f
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-14 13:17:38 -0500

    Time: 4 ms (62.28%) | Memory: 50.3 MB (32.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,49 +1,129 @@
 class Solution {
 
-    //Solving on 10 Dec 2025:
+    //Solving on 14 Dec 2025:
 
-    //intuition 1 (DP: Bottom-Up Solution): 
-        //Solve with 2D dynamic programming matrix
-        //We can store the minimum sum possible for each cell in the matrix
-        //Since we can only move in the right or down direction, we can only approach a particular cell
-            //from left (i, j-1) or top (i-1, j).
-        //Base case: 
-            //Fill the first row and first col with positive infinity
-        //Recurrence relation:
-            //For any cell consider the minimum of its top and left cells.
-            //dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+    //intuition 1: 2D DP : Bottom up Tabulation : Answer at 0,0
+        //As we can only move down or right, there are only two ways to approach any cell, from top or from left.
         
+        //Base cases:
+            //In last row we can only move right and in last col we can only move down, so fill them by adding 
+                //the previous sum stored in dp array.
+             
+        //Recurrence relation: 
+            //dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j](down), dp[i][j + 1](up))
 
     public int minPathSum(int[][] grid) {
-        //dp[i][j] represents the minimum sum possible to reach i-1, j-1
-        //Therefore, to reach m-1, n-1 we need m+1 x n+1 size matrix
-
+        //dp[i][j] represents the minimum path sum possible from i, j to bottom right m-1, n-1
+        //dp[0][0] will represent the minimum path sum possible from 0,0 to bottom right m-1, n-1
+        //Therefore, we need a 2D matrix of size m x n
+        
         int rows = grid.length;
         int cols = grid[0].length;
 
-        int[][] dp = new int[rows + 1][cols + 1];
+        int[][] dp = new int[rows][cols];
 
-        //filling first col with infinity
-        for(int i = 0; i < rows + 1; i ++){
-            dp[i][0] = Integer.MAX_VALUE;
+        //base cases
+        dp[rows - 1][cols - 1] = grid[rows - 1][cols - 1];
+
+        //filling last col
+        for(int i = rows - 2; i >=0; i --){
+            dp[i][cols - 1] = dp[i + 1][cols - 1] + grid[i][cols - 1];
         }
 
-        //filling first row with infinity
-        for(int j = 0; j < cols + 1; j ++){
-            dp[0][j] = Integer.MAX_VALUE;
+        //filling last row
+        for(int j = cols - 2; j >= 0; j --){
+            dp[rows - 1][j] = dp[rows - 1][j + 1] + grid[rows - 1][j];
         }
-        
-        for(int i = 1; i < rows + 1; i ++){
-            for(int j = 1; j < cols + 1; j ++){
-                if(i == 1 && j == 1) {
-                    dp[1][1] = grid[0][0];
-                    continue;
-                }
-                dp[i][j] = grid[i - 1][j - 1] + Math.min(dp[i-1][j], dp[i][j-1]);
-                // System.out.println("i : " + i + ", j : " + j + " = "+ dp[i][j]);
+
+
+        for(int i = rows - 2; i >= 0; i --){
+            for(int j = cols - 2; j >= 0; j --){
+                dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
             }
         }
 
-        return dp[rows][cols];
+
+        return dp[0][0];
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 10 Dec 2025:
+
+    // //intuition 1 (DP: Bottom-Up Solution): 
+    //     //Solve with 2D dynamic programming matrix
+    //     //We can store the minimum sum possible for each cell in the matrix
+    //     //Since we can only move in the right or down direction, we can only approach a particular cell
+    //         //from left (i, j-1) or top (i-1, j).
+    //     //Base case: 
+    //         //Fill the first/extra row and first/extra col with positive infinity
+    //     //Recurrence relation:
+    //         //For any cell consider the minimum of its top and left cells.
+    //         //dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        
+    //     //We need m+1 x n+1 size matrix to use 1-based indexing trick to avoid boundary checks
+    //         //"dp[0][*] and dp[*][0] are sentinel rows/columns set to infinity."
+
+    
+    // //TC: O(n x m)
+    // //SC: O(n x m)
+
+    // public int minPathSum(int[][] grid) {
+    //     //dp[i][j] stores the minimum path sum to reach cell i-1, j-1 index in the grid
+
+    //     int rows = grid.length;
+    //     int cols = grid[0].length;
+
+    //     int[][] dp = new int[rows + 1][cols + 1];
+
+    //     //filling first col with infinity
+    //     for(int i = 0; i < rows + 1; i ++){
+    //         dp[i][0] = Integer.MAX_VALUE;
+    //     }
+
+    //     //filling first row with infinity
+    //     for(int j = 0; j < cols + 1; j ++){
+    //         dp[0][j] = Integer.MAX_VALUE;
+    //     }
+        
+    //     for(int i = 1; i < rows + 1; i ++){
+    //         for(int j = 1; j < cols + 1; j ++){
+    //             if(i == 1 && j == 1) { //because 1,1 is a starting cell and Math.min would have given an infinity
+    //                 dp[1][1] = grid[0][0]; 
+    //                 continue;
+    //             }
+    //             dp[i][j] = grid[i - 1][j - 1] + Math.min(dp[i-1][j], dp[i][j-1]);
+    //             // System.out.println("i : " + i + ", j : " + j + " = "+ dp[i][j]);
+    //         }
+    //     }
+
+    //     return dp[rows][cols];
+    // }
 }
