1444d389ccb77fde66e720af3c256b23481c9582
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-14 19:48:01 -0500

    Time: 19 ms (87.27%) | Memory: 53.8 MB (48.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,58 +1,149 @@
 class Solution {
 
-    //Solving on 11 Dec 2025:
+    //Solving on 14 Dec 2025:
 
-    //intuition 1 (DP : 2D: Bottom-Up Solution):
-        //We can have a 2D matrix to store longest subsequence's length till now in each cell
-        
-        //Base case: 
-            //Set the extra row/col to -INF 
+    //intuition 1 (DP : 2D: Bottom-Up Tabulation Solution: DP on strings patter)
+        //Base cases:
+            //Fill first row with 0: This is true as given 0 length of text1, the 
+                //only possible LCS is an empty string and hence, 0
+            //Fill the first col with 0: This is true as given 0 length of text2, the
+                //only possible LCS is an empty string and hence, 0
         //Recurrence relation:
-            //if text1[i-1] == text2[j-1] => add 1 to the previous diagnol cell in dp (dp[i-1][j-1]) 
-                //doing this in order to avoid any duplicate summations. As adding to diagonal ensures that
-                    //there was some character, other than current one, that was matched.           
-            //else dp[i][j] = max(top + left)
-                //top and left are being considered because we need to make sure if any of the previous
-                    //addition to the length have resulted in an increase of longest subsequence's length,
-                    //and consider the max.
-
+            //If current character from both the strings matches, then the subproblem reduces
+                //to finding LCS from both of the remaining strings after not considering
+                //the matching character. => dp[i][j] = 1 + dp[i-1][j-1]
+            //If current character from both the strings does not match, then we have two scenarios:
+                //1. Skip the current character from text1 and look for LCS in remaining text1 with original text2
+                //2. Skip the current character from text2 and look for LCS in remaining text2 with original text1
+                //And then take the max out of these two as we need LCS
+            //=> dp[i][j] = Math.max(dp[i-1][j](reducing text1 length), dp[i][j-1](reducing text2 length))
 
     public int longestCommonSubsequence(String text1, String text2) {
-        //dp[i][j] represents the longest common subsequence between two strings till i length of text1 
+        //dp[i][j] represents longest common subsequence till i length of text1
             //and j length of text2
-        //Assuming length of text1 is tLen1 and length of text2 is tLen2, to find longest common subsequence 
-            //between both these strings, we need a dp matrix of tLen1+1 x tLen2+1 size
+        //dp[text1.length()][text2.length()] will represent LCS for both the strings
+        //Therefore, we need a 2D matrix of size text1.length()+1 x text2.length()+1
 
-        int rows = text1.length();
-        int cols = text2.length();
+        int rows = text1.length() + 1;
+        int cols = text2.length() + 1;
 
-        int[][] dp = new int[rows + 1][cols + 1];
+        int[][] dp = new int[rows][cols];
 
-        for(int j = 0; j < cols + 1; j ++){
+
+        //base cases
+        //filling first row
+        for(int j = 0; j < cols; j ++){
             dp[0][j] = 0;
         }
 
-        for(int i = 0; i < rows + 1; i ++){
+        //filling first col
+        for(int i = 0; i < rows; i ++){
             dp[i][0] = 0;
         }
 
-        for(int i = 1; i < rows + 1; i ++){
-            for(int j = 1; j < cols + 1; j ++){
-                int match = text1.charAt(i - 1) == text2.charAt(j - 1) ? 1 : 0;
-                // if(i == 1 && j == 1){
-                //     dp[1][1] = match;
-                //     continue;
-                // }
-                if(match == 1){
-                    dp[i][j] = match + dp[i - 1][j - 1];
+
+        for(int i = 1; i < rows; i ++){
+            for(int j = 1; j < cols; j ++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); 
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[rows][cols];
+        return dp[rows - 1][cols - 1];
+
+
     
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 11 Dec 2025:
+
+    // //intuition 1 (DP : 2D: Bottom-Up Tabulation Solution):
+    //     //We can have a 2D matrix to store longest subsequence's length till now in each cell
+        
+    //     //Base case: 
+    //         //Set the extra row/col to 0
+    //     //Recurrence relation:
+    //         //if text1[i-1] == text2[j-1] => add 1 to the previous diagnol cell in dp (dp[i-1][j-1]) 
+    //             //doing this in order to avoid any duplicate summations. As adding to diagonal ensures that
+    //                 //there was some character, other than current one, that was matched. 
+    //             //Reson: If one character gets matched, then we are left with i-1 length of text1 and j-1 length 
+    //                 //of text2 to compare
+    //         //else dp[i][j] = max(top + left)
+    //             //top and left are being considered because we need to make sure if any of the previous
+    //                 //addition to the length have resulted in an increase of longest subsequence's length,
+    //                 //and consider the max.
+
+
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     //dp[i][j] represents the longest common subsequence between two strings till i length of text1 
+    //         //and j length of text2
+    //     //Assuming length of text1 is tLen1 and length of text2 is tLen2, to find longest common subsequence 
+    //         //between both these strings, we need a dp matrix of tLen1+1 x tLen2+1 size
+
+    //     int rows = text1.length();
+    //     int cols = text2.length();
+
+    //     int[][] dp = new int[rows + 1][cols + 1];
+
+    //     for(int j = 0; j < cols + 1; j ++){
+    //         dp[0][j] = 0;
+    //     }
+
+    //     for(int i = 0; i < rows + 1; i ++){
+    //         dp[i][0] = 0;
+    //     }
+
+    //     for(int i = 1; i < rows + 1; i ++){
+    //         for(int j = 1; j < cols + 1; j ++){
+    //             int match = text1.charAt(i - 1) == text2.charAt(j - 1) ? 1 : 0;
+    //             // if(i == 1 && j == 1){
+    //             //     dp[1][1] = match;
+    //             //     continue;
+    //             // }
+    //             if(match == 1){
+    //                 dp[i][j] = match + dp[i - 1][j - 1];
+    //             }
+    //             else{
+    //                 dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); 
+    //             }
+    //         }
+    //     }
+
+    //     return dp[rows][cols];
+    
+    // }
 }
