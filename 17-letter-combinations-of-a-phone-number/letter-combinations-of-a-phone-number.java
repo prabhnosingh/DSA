e6013c042332cbc0c63b4a619c93204a35100427
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-16 10:28:16 -0500

    Time: 1 ms (56.48%) | Memory: 43.3 MB (53.43%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,80 +1,233 @@
 class Solution {
+
+    //Re-solving on 16 Dec 2025:
+
+    //intuition 1(backtracking dfs):
+        //Have a map for numbers and their alphabets. Then traverse over the digits recursively.  
+
+        //Base case: when the length of currComb becomes equal to digits, add it to combinations.
+
     public List<String> letterCombinations(String digits) {
-        char[] digitsArray = digits.toCharArray();
-        HashMap<String, String> alphabetsMap = new HashMap<>();
-        List<String> res = new ArrayList<>();
-        int idx = 0;
-        // if(digits.length() == 0){
-        //     return res;
-        // }
-        for(char ch : digitsArray){
-            switch(ch){
-                case '2':
-                    alphabetsMap.put("2", "abc");
-                    break;
-                case '3':
-                    alphabetsMap.put("3", "def");
-                    break;
+        List<String> combs = new ArrayList<>();
+        HashMap<Character, String> digitsMap = new HashMap<>();
 
-                case '4':
-                    alphabetsMap.put("4", "ghi");
-                    break;
-                 
-                case '5':
-                    alphabetsMap.put("5", "jkl");
-                    break;
-                 
-                case '6':
-                    alphabetsMap.put("6", "mno");
-                    break;
-                 
-                case '7':
-                    alphabetsMap.put("7", "pqrs");
-                    break;
- 
-                case '8':
-                    alphabetsMap.put("8", "tuv");
-                    break;
- 
-                case '9':
-                    alphabetsMap.put("9", "wxyz");
-                    break;
- 
-            }
+        digitsMap.put('2', "abc");
+        digitsMap.put('3', "def");
+        digitsMap.put('4', "ghi");
+        digitsMap.put('5', "jkl");
+        digitsMap.put('6', "mno");
+        digitsMap.put('7', "pqrs");
+        digitsMap.put('8', "tuv");
+        digitsMap.put('9', "wxyz");
 
-        }
-        
-        dfs(digits, new ArrayList<>(), alphabetsMap, res, 0);
-        return res;
 
-        
+        dfs(digits, 0, combs, new StringBuilder(), digitsMap);
+        return combs;
+
     }
 
-    private void dfs(String digits, List<Character> currCombination, HashMap<String, String> alphabetsMap, List<String> res, int start){
-                
-            if(currCombination.size() == digits.length()){
-                if(digits.length() == 0){
-                    return;
-                }
-                String temp = "";
-                for(char c : currCombination){
-                    temp += c;
-                }
-                res.add(temp);
-                return;
-            }
+    private void dfs(String digits, int currIdx, List<String> combs, StringBuilder currComb, HashMap<Character, String> digitsMap){
+        
+        if(currComb.length() == digits.length()){
+            combs.add(new String(currComb));
+            return;
+        }
 
-            for(char ch : alphabetsMap.get(digits.charAt(start) + "").toCharArray()){
-                currCombination.add(ch);
-                dfs(digits, currCombination, alphabetsMap, res, start + 1);
-                currCombination.remove(currCombination.size() - 1);
-                
-            }
+        char currNumChar = digits.charAt(currIdx);
+        String currString = digitsMap.get(currNumChar);
 
-                
+        for(int i = 0; i < currString.length(); i ++){
+            currComb.append(currString.charAt(i));
+            
+            dfs(digits, currIdx + 1, combs, currComb, digitsMap);
 
+            //reverting the last added character
+            currComb.setLength(currComb.length() - 1); //slightly more efficient as it involves less internal array copying 
+                //than deleteCharAt
+            // currComb.deleteCharAt(currComb.size() - 1);
 
         }
 
+    }
+
+
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ////////////////////////////////////////////////////////////////////////////////
+    
+    //intuition 1(Iterative): length of each combinatin will be equal to digits's length. Map every number to its corresponding 
+    //characters as a String. Then for each digit in digits add the corresponding characters to the ans. It will be like
+    //having a "ans" list of strings, then for 2 add "a", "b", "c" to the list. Then for 3 add "d" to all current strings in ans
+    //"ad", "bd", "cd" and so on.
+
+    // public List<String> letterCombinations(String digits) {
+    //     HashMap<String, String> digitsMap = new HashMap<>();
+    //     List<String> ans = new ArrayList<>();
+
+    //     if(digits.length() == 0){
+    //         return ans;
+    //     }
+
+    //     digitsMap.put("2", "abc");
+    //     digitsMap.put("3", "def");
+    //     digitsMap.put("4", "ghi");
+    //     digitsMap.put("5", "jkl");
+    //     digitsMap.put("6", "mno");
+    //     digitsMap.put("7", "pqrs");
+    //     digitsMap.put("8", "tuv");
+    //     digitsMap.put("9", "wxyz");
+
+
+
+    //     // System.out.println(digits.charAt(0));
+    //     // System.out.println(digitsMap.get(2 + ""));
+    //     // System.out.println(digitsMap.get(digits.charAt(0)));
+    //     for(char ch : digitsMap.get(String.valueOf(digits.charAt(0))).toCharArray()){ //initializing ans with characters of 
+    //     //first digit of digits
+    //         ans.add(ch + "");
+    //     }
+    //     for(int currDigitsIdx = 1; currDigitsIdx < digits.length(); currDigitsIdx ++){
+    //         List<String> tempAns = new ArrayList<>();
+    //         for(char ch : digitsMap.get(String.valueOf(digits.charAt(currDigitsIdx))).toCharArray()){
+    //             for(String s : ans){
+    //                 s += ch;
+    //                 tempAns.add(s);
+    //             }
+    //         }
+    //         ans = tempAns;
+    //     }
+    //     return ans;
+
+    // }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    //intuition 2(Iterative without HashMap):
+    // public List<String> letterCombinations(String digits) {
+        
+    //     List<String> ans = new ArrayList<>();
+
+    //     if(digits.length() == 0){
+    //         return ans;
+    //     }
+    //     ans.add("");
+
+    //     String[] digitToChar = {
+    //         "", "", "abc", "def", "ghi", "jkl", 
+    //         "mno", "qprs", "tuv", "wxyz"
+    //     };
+
+    //     for(char digit : digits.toCharArray()){
+    //         List<String> temp = new ArrayList<>();
+    //         for(String currStr : ans){
+    //             for(char ch : digitToChar[digit - '0'].toCharArray()){ //digit - '0' gives an integer as output
+    //                 // currStr += ch; //don't use this as this updates currStr for next iterations as well
+    //                 temp.add(currStr + ch);
+    //             }
+    //         }
+    //         ans = temp;
+    //     }
+    //     return ans;
+    // } 
+
+
+    
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // //intuition 3(dfs):
+    // public List<String> letterCombinations(String digits) {
+    //     List<String> ans = new ArrayList<>();
+
+    //     if(digits.length() == 0){
+    //         return ans;
+    //     }
+    //     String[] digitToChar = {
+    //         "", "", "abc", "def", "ghi", "jkl", 
+    //         "mno", "qprs", "tuv", "wxyz"
+    //     };
+
+    //     dfs(digits, 0, new StringBuilder(), digitToChar, ans);
+    //     return ans;
+    // }
+
+    // private void dfs(String digits, int currDigitsIdx, StringBuilder currString, String[] digitToChar, List<String> ans){
+    //     //we do not need an explicit check for currDigitsIdx as any string's length cannot exceed the length of digits,
+    //     //therefore the out of bounds condition is automatically handled
+    //     if(currString.length() == digits.length()){ //return when the currString length is same as length of digits
+    //         ans.add(currString.toString());
+    //         return;
+    //     }
+
+    //     for(char ch : digitToChar[digits.charAt(currDigitsIdx) - '0'].toCharArray()){
+    //         currString.append(ch);
+    //         dfs(digits, currDigitsIdx + 1, currString, digitToChar, ans);
+    //         currString.deleteCharAt(currString.length() - 1); //removing the last added character
+
+    //     }
+
+    // }
+
 }
+
+
+
+
+
+
+
+
+
+
+
