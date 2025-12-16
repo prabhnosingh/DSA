b04fab54d38870e6f14decc5b626a0e515129c6e
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-16 13:17:14 -0500

    Time: 669 ms (5.03%) | Memory: 43.1 MB (63.33%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,7 +1,32 @@
 class Solution {
     //Solving on 16 Dec 2025:
 
-    //intuition 1: (Backtracking DFS):
+    //intuition 2 (improved version of intuition 1): Backtracking DFS: decision tree on nums
+        //At each index currIdx, we decide how nums[currIdx] contributes to the final sum.
+        //For every number, we have exactly two choices:
+            //1. Add the number to the current sum
+            //2. Subtract the number from the current sum
+        
+        //1️⃣ What does one level of recursion represent?
+            //Each level represents fixing the sign (+ or -) for nums[currIdx].
+            //Moving to dfs(currIdx + 1) means we have already decided the sign for all numbers
+                //before currIdx and are now deciding for the next number.
+
+        //2️⃣ What are my choices at that level?   
+            //At each level, there are exactly two recursive branches:
+                //i. Take nums[currIdx] as +nums[currIdx]
+                //ii. Take nums[currIdx] as -nums[currIdx]
+
+        //3️⃣ When do I stop?
+            //When currIdx == nums.length, all numbers have been assigned a sign.
+            //At this point, if currSum == target, we have found one valid way.
+            //Return 1 for a valid way, otherwise return 0.
+
+        //The final answer is the sum of valid ways from all branches of the recursion tree.
+
+
+
+    //intuition 1 (self-thought): (Backtracking DFS):
 
     //1️⃣ What does one level of recursion represent?
         //Each digit from nums being processed 
@@ -17,7 +42,8 @@ class Solution {
         //When currIdx becomes equalt to nums.length. At this point, check if currSum is equal to target, 
             //if yes, increment your variable
 
-
+    //TC: O(2^n): where n is the length of nums
+    //SC: O(n): Length of recursive stack
     public int findTargetSumWays(int[] nums, int target) {
         return dfs(nums, 0, 0, target);
     }
