66bcf76460b25eadfabddd8c33ae3d39be6a46f0
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-17 07:57:53 -0500

    Time: 0 ms (100.00%) | Memory: 43.6 MB (13.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -6,64 +6,71 @@ class Solution {
     //Solving on 13 Dec 2025:
 
     //intuition 1: 2D DP - Bottom up Tabulation - Answer at 0,0 
-        //As the robot can only move down or right, for any cell (i, j) we can get the unique paths 
-            //by adding up the unique paths from right and down neighboring cells.
-        
-        //Recurrence relation: 
-            //dp[i][j] = dp[i+1][j](down) + dp[i][j+1](right)
-
         //Base case:
-            //Fill last row and last col with 1, given that any of the cell is not an obstacle. 
-            //If an obstacle is at that index, fill 0 there. Before filling any cell check if its
-                //next index contains 0, if yes fill 0. As if there is an obstacle in last row
-                //or last col, all the preceding cells will have 0s   
-        
-        //Before filling any normal cell, check if it is an obstacle, if yes, simply skip it
+            //For any index in last row and last col, there is only 1 way to reach bottom
+                //right, if there is no obstacle in betweeen. Have a flag to know if there
+                //is an obstacle in last row and col and make all the preceding cells as 0
+
+        //Recurrence relation:  
+            //For any index the number of unqiue paths is the sum of number of unique paths 
+                //from right cell and bottom cell.
+            //If the current cell is an obstacle in the obstacleGrid, simply skip it
         
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        //dp[i][j] represents number of unique paths from i, j to reach bottom right
-        //dp[0][0] will represent number of unique paths from 0,0 to reach bottom right (m-1, n-1)
-        //Therefore, we need a m x n 2D matrix 
+        //dp[i][j] represents number of unique paths to reach bottom right from i,j 
+            //while ony going down and right and avoiding any obstacles
+        //dp[0][0] represents number of  unique paths to reach bottom right from 0,0
+            //while only going down and right and avoiding any obstacles
+        
+        //The maximum cell that we want to reach is rows-1, cols-1, therefore we need
+            //a 2D matrix of size rows x cols
 
         int rows = obstacleGrid.length;
         int cols = obstacleGrid[0].length;
 
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[rows-1][cols-1] == 1) return 0;
-
         int[][] dp = new int[rows][cols];
 
-        //base case
-        dp[rows-1][cols-1] = 1;
+        //base cases
+        if(obstacleGrid[rows-1][cols-1] == 1) return 0;
+
+        dp[rows-1][cols-1] = 1; //there is one way to stay on bottom right cell
+
         //filling last col
+        boolean obstacleFound = false;
         for(int i = rows - 2; i >= 0; i --){
-            if(dp[i + 1][cols - 1] == 0 || obstacleGrid[i][cols - 1] == 1){
-                dp[i][cols - 1] = 0;
+            if(obstacleGrid[i][cols-1] == 1) obstacleFound = true;
+            if(obstacleFound){
+                dp[i][cols-1] = 0;
+                continue;
             }
-            else{
-                dp[i][cols-1] = 1;
-            }
+            dp[i][cols-1] = 1;
 
         }
 
         //filling last row
+        obstacleFound = false;
         for(int j = cols - 2; j >= 0; j --){
-            if(dp[rows - 1][j + 1] == 0 || obstacleGrid[rows - 1][j] == 1){
-                dp[rows - 1][j] = 0;
+            if(obstacleGrid[rows-1][j] == 1) obstacleFound = true;
+            if(obstacleFound){
+                dp[rows-1][j] = 0;
+                continue;
             }
-            else{
-                dp[rows - 1][j] = 1;
-            }
+            dp[rows-1][j] = 1;
         }
 
-        for(int i = rows - 2; i  >= 0; i --){
+        for(int i = rows - 2; i >= 0; i --){
             for(int j = cols - 2; j >= 0; j --){
-                if(obstacleGrid[i][j] == 1) continue;
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
 
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
             }
         }
 
         return dp[0][0];
+
 
        
 
@@ -91,6 +98,95 @@ class Solution {
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Solving on 13 Dec 2025:
+
+//     //intuition 1: 2D DP - Bottom up Tabulation - Answer at 0,0 
+//         //As the robot can only move down or right, for any cell (i, j) we can get the unique paths 
+//             //by adding up the unique paths from right and down neighboring cells.
+        
+//         //Recurrence relation: 
+//             //dp[i][j] = dp[i+1][j](down) + dp[i][j+1](right)
+
+//         //Base case:
+//             //Fill last row and last col with 1, given that any of the cell is not an obstacle. 
+//             //If an obstacle is at that index, fill 0 there. Before filling any cell check if its
+//                 //next index contains 0, if yes fill 0. As if there is an obstacle in last row
+//                 //or last col, all the preceding cells will have 0s   
+        
+//         //Before filling any normal cell, check if it is an obstacle, if yes, simply skip it
+        
+//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+//         //start, destination and special condition 
+//         //dp[i][j] represents number of unique paths from i, j to reach bottom right without encountering obstacle cells
+//         //dp[0][0] will represent number of unique paths from 0,0 to reach bottom right (m-1, n-1)
+//         //Therefore, we need a m x n 2D matrix 
+
+//         int rows = obstacleGrid.length;
+//         int cols = obstacleGrid[0].length;
+
+//         if(obstacleGrid[0][0] == 1 || obstacleGrid[rows-1][cols-1] == 1) return 0;
+
+//         int[][] dp = new int[rows][cols];
+
+//         //base case
+//         dp[rows-1][cols-1] = 1;
+//         //filling last col
+//         for(int i = rows - 2; i >= 0; i --){
+//             if(dp[i + 1][cols - 1] == 0 || obstacleGrid[i][cols - 1] == 1){
+//                 dp[i][cols - 1] = 0;
+//             }
+//             else{
+//                 dp[i][cols-1] = 1;
+//             }
+
+//         }
+
+//         //filling last row
+//         for(int j = cols - 2; j >= 0; j --){
+//             if(dp[rows - 1][j + 1] == 0 || obstacleGrid[rows - 1][j] == 1){
+//                 dp[rows - 1][j] = 0;
+//             }
+//             else{
+//                 dp[rows - 1][j] = 1;
+//             }
+//         }
+
+//         for(int i = rows - 2; i  >= 0; i --){
+//             for(int j = cols - 2; j >= 0; j --){
+//                 if(obstacleGrid[i][j] == 1) continue;
+
+//                 dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+//             }
+//         }
+
+//         return dp[0][0];
+
+       
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // // If in the problem it was allowed 4-direction movement (no revisits in the same path), it would have become a 
     //     //"graph path-finding" problem with visited-state tracking, which is solved with backtracking / DFS.
 
