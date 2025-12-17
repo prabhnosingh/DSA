142f6fbe9e3a3d34967180360cd0eb86759633f3
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-17 07:09:41 -0500

    Time: 0 ms (100.00%) | Memory: 42.2 MB (30.64%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,52 +3,49 @@ class Solution {
     //Re-solving on 13 Dec 2025:
 
     //intuition 2: DP on Grid problem - 2D Dp - Bottom up Tabulation solution (solution at 0,0)
-        //Approach where answer comes at 0,0 works for all DP on grid problems but approach where
-            //answer comes at bottom right, do not work for some problems
+        //As the robot can only move down and right, we can use already computed unique paths 
+            //of bottom and right neighboring cells and add them so that we get unique path count
+            //for our current cell.
         
-        //To get the solution at 0,0 we start from bottom right (m-1, n-1). 
-        //Base case:
-            //dp[m-1][n-1] = 1 as there is one way to reach bottom right from m-1,n-1, i.e. by doing nothing
-                //ans staying there
-            //for any cell in last col, we only have 1 way to reach bottom right, therefore, fill last
-                //col with 1 s
-            //for any cell in last row, we only have 1 way to reach bottom rigth, therefore, fill last 
-                //row with 1 s
-        
-        //Recurrence relation:
-            //for any cell we have two options, either to move right or move down, therefore number of 
-                //ways to reach bottom right from i, j will be "number of ways to reach bottom right from 
-                //i,j 's right neighboring cell" + "number of ways to reach bottom right from i,j 's below
-                //neighboring cell"
-            //dp[i][j] = dp[i+1][j](below) + dp[i][j+1](right)
-    
+        //Base case: 
+            //In the last column robot can only travel in down direction and not right (out of bounds)
+            //Therefore, the unique paths for all the cells in last col are 1
+            //In the last row robot can only travel in the right direction and not down (out of bounds)
+            //Therefore, the unique paths for all the cells in the last row are 1.
+
+        //Recurrence relation: 
+            //For any cell the number of unique pahts are : dp[i][j] = dp[i+1][j](down) + dp[i][j+1](right)    
     
     public int uniquePaths(int m, int n) {
-       //dp[i][j] represents the number of unique paths to reach m-1, n-1 location from i, j
-       //dp[0][0] will represent the number of unique paths to reach m-1, n-1 location
-       //Therefore, we need a 2D matrix of size m x n
-        
+        //dp[i][j] represents the number of unique paths from i,j to reach the bottom right (m-1, n-1) by
+            //travelling only down and right
+        //dp[0][0] will represent the number of unique paths from 0,0 to reach the bottom right by travelling
+            //only down and right
+        //Therefore, we need a dp matrix of size m x n.
+
         int[][] dp = new int[m][n];
 
+        //base cases
 
-        //filling last col with 1 s
-        for(int i = m-1; i >= 0; i --){
-            dp[i][n-1] = 1;
+        dp[m-1][n-1] = 1; //as there are 1 total ways to stay at m-1, n-1
+
+        //filling last row
+        for(int j = n-2; j >= 0; j --){
+            dp[m-1][j] = 1;
         }
 
-        //filling last row with 1 s
-        for(int j = n-1; j >= 0; j --){
-            dp[m-1][j] = 1;
+        //filling last col
+        for(int i = m-2; i >= 0; i --){
+            dp[i][n-1] = 1;
         }
 
         for(int i = m-2; i >= 0; i --){
             for(int j = n-2; j >= 0; j --){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
             }
         }
 
         return dp[0][0];
-
 
     }
 
@@ -79,6 +76,85 @@ class Solution {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-solving on 13 Dec 2025:
+
+//     //intuition 2: DP on Grid problem - 2D Dp - Bottom up Tabulation solution (solution at 0,0)
+//         //Approach where answer comes at 0,0 works for all DP on grid problems but approach where
+//             //answer comes at bottom right, do not work for some problems
+        
+//         //To get the solution at 0,0 we start from bottom right (m-1, n-1). 
+//         //Base case:
+//             //dp[m-1][n-1] = 1 as there is one way to reach bottom right from m-1,n-1, i.e. by doing nothing
+//                 //ans staying there
+//             //for any cell in last col, we only have 1 way to reach bottom right, therefore, fill last
+//                 //col with 1 s
+//             //for any cell in last row, we only have 1 way to reach bottom rigth, therefore, fill last 
+//                 //row with 1 s
+        
+//         //Recurrence relation:
+//             //for any cell we have two options, either to move right or move down, therefore number of 
+//                 //ways to reach bottom right from i, j will be "number of ways to reach bottom right from 
+//                 //i,j 's right neighboring cell" + "number of ways to reach bottom right from i,j 's below
+//                 //neighboring cell"
+//             //dp[i][j] = dp[i+1][j](below) + dp[i][j+1](right)
+    
+    
+//     public int uniquePaths(int m, int n) {
+//        //dp[i][j] represents the number of unique paths to reach m-1, n-1 location from i, j
+//        //dp[0][0] will represent the number of unique paths to reach m-1, n-1 location
+//        //Therefore, we need a 2D matrix of size m x n
+        
+//         int[][] dp = new int[m][n];
+
+
+//         //filling last col with 1 s
+//         for(int i = m-1; i >= 0; i --){
+//             dp[i][n-1] = 1;
+//         }
+
+//         //filling last row with 1 s
+//         for(int j = n-1; j >= 0; j --){
+//             dp[m-1][j] = 1;
+//         }
+
+//         for(int i = m-2; i >= 0; i --){
+//             for(int j = n-2; j >= 0; j --){
+//                 dp[i][j] = dp[i+1][j] + dp[i][j+1];
+//             }
+//         }
+
+//         return dp[0][0];
+
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //     //Re-solving on 13 Dec 2025:
 
 //     //intuition 1: 2D Dp - Bottom up Tabulation solution 
