9166aba9380da2cd40b135589f4a924dad3fb87d
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-21 07:24:59 -0500

    Time: 1 ms (99.78%) | Memory: 76.9 MB (78.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,17 +1,60 @@
 class Solution {
+    
+    //Resolving on 21 Dec 2025:
+        //intuition 1 (DP: Kadane's algo):
+
+            //We re-initialize currSum to 0 if it is negative because we don't want negatives to accumulate.
+            //-1 + (-3) = -4 and -4 is smaller than -1, so we don't want it
     public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
 
-        int[] dp = new int[nums.length];
-        int maxSum = nums[0];
-        dp[0] = nums[0];
-        for(int i = 1; i < nums.length; i ++){
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        for(int num : nums){
+            currSum += num;
 
-            if(dp[i] > maxSum){
-                maxSum = dp[i];
-            }
+            maxSum = Math.max(maxSum, currSum);
+
+            currSum = currSum < 0 ? 0 : currSum;
         }
         return maxSum;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+    
+    // public int maxSubArray(int[] nums) {
+
+    //     int[] dp = new int[nums.length];
+    //     int maxSum = nums[0];
+    //     dp[0] = nums[0];
+    //     for(int i = 1; i < nums.length; i ++){
+    //         dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+
+    //         if(dp[i] > maxSum){
+    //             maxSum = dp[i];
+    //         }
+    //     }
+    //     return maxSum;
 
 
 
@@ -30,5 +73,6 @@ class Solution {
         // }
 
         // return maxSum;
-    }
+    // }
+
 }
