4bf87445217ba7c82ba8ee82a4db973582fd816c
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2025-12-26 12:23:41 -0500

    Time: 16 ms (96.17%) | Memory: 43.8 MB (82.41%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,52 @@
+class Solution {
+    //Solving on 26 Dec 2025:
+
+    //intuition 1: (DP on Sum pattern: 0/1 Knapsack(subset sum))
+        //For any two subsets to have equal sum, they must have a sum = totalSum / 2
+        //So we calculate sum = totalSum / 2 and then iterate over the array. Now for every num
+            //we have two choices: Either to pick the number or not pick the number.
+
+        //"Partition into two equal subsets is equivalent to finding one subset whose sum is totalSum/2"
+        //"We reduce teh problem to checking whether a subset with sum totalSum/2 exists using 0/1
+            //knapsack DP, where each number is either taken or skipped exactly once"
+ 
+    public boolean canPartition(int[] nums) {
+        //d[s] reperesents whether we can form a sum s using some of the numbers processed so far (true/false)
+        
+        int totalSum = 0;
+
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        if(totalSum % 2 != 0){
+            return false;
+        }
+
+        int targetSum = totalSum / 2;
+
+        boolean[] dp = new boolean[targetSum + 1];
+
+        //base case
+        dp[0] = true;
+
+        for(int num : nums){
+            if(num > targetSum) return false;
+            for(int sum = targetSum; sum >= num; sum --){ 
+                if(dp[sum]){ //if a sum already exists, i.e., dp[sum] is already true, skip adding the current number (num)
+                    continue;
+                }
+                if(dp[sum - num]){ //if a complement of sum (sum - num) exists, then consider adding the current number (num).
+                    //By doing so, we mark that we can form a sum of 'sum' by adding num to the numbers encountered so far
+                    dp[sum] = true;
+                }
+                if(dp[targetSum]){ //if a sum equal to target has been acheived return true
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+}
