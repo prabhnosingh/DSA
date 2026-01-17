a61a37a61ce8241371fe64fdaf7abd7136f992bc
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-01-16 23:18:11 -0500

    Time: 85 ms (77.26%) | Memory: 46.5 MB (85.45%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,6 +1,7 @@
     //Solving on 14, 15 Jan 2026
 
-    //intuition 2 (dfs : cleaned versin): Graphs : DSU
+    //intuition 2 (dfs : cleaned version: Optimization): Graphs : Directed graph reachability + “run DFS/BFS from every node” 
+        //(aka “max reachable nodes in directed graph”)
         //The problem is to find maximum number of bombs in a single cluster
         //How to fetch edges: 
             //Have to make Adjacency matrix/list based on the radius of each bomb
@@ -124,6 +125,133 @@ class Solution {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Solving on 14, 15 Jan 2026
+
+//     //intuition 2 (dfs : cleaned versin): Graphs : Directed graph reachability + “run DFS/BFS from every node” 
+//         //(aka “max reachable nodes in directed graph”)
+//         //The problem is to find maximum number of bombs in a single cluster
+//         //How to fetch edges: 
+//             //Have to make Adjacency matrix/list based on the radius of each bomb
+//             //Go with adjacency list
+//             //For each bomb b1 check if it falls under the radius of any other bomb b2, if yes then
+//                 //there should be a directed edge between b2 -> b1.
+            
+//             //How to know if a point lies inside a circle:
+//                 //find the distance of the point from the center of the circle 
+//                     //and if the distance is less than equal to radius of the circle
+//                     //then the point lies on or inside the circle
+            
+//         //After forming an adjList, we can use DSU to find the largest cluster by finding the maximum
+//             //number of nodes having same ultimate parent
+
+
+        
+//         //DSU: DSU does not work in this question. DSU works good in unidrected graph problems. This is directed.
+//             //rank
+//                 //initialized to 0
+//             //finding ultimate parent
+//             //storing parents in parent array
+//                 //intialized to nodes themselves
+
+//         //DFS:
+//             //We can also run dfs on the adjList and find maximum nodes traversed in a single 
+//                 //dfs run by adding 1 to the count for each valid dfs call
+
+
+
+// class Solution {
+//     public int maximumDetonation(int[][] bombs) {
+//         HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+
+//         int numOfBombs = bombs.length;
+//         for(int i = 0; i < numOfBombs; i ++){
+//             adjList.put(i, new ArrayList<>());
+//         }
+
+//         for(int i = 0; i < numOfBombs; i ++){
+//             for(int j = 0; j < numOfBombs; j ++){
+//                 if(i == j) continue;
+
+//                 if(checkIfConnected(bombs, i, j)){ //if bombs are connected, form an edge in adjList
+//                     adjList.get(i).add(j); //i -> j directed edge
+//                 }
+
+//             }
+//         }
+
+
+//         // //printing adjList
+//         // for(int i = 0; i < numOfBombs; i ++){
+//         //     System.out.print(i + " -> ");
+//         //     for(int childBomb : adjList.get(i)){
+//         //         System.out.print(childBomb + " ");
+//         //     }
+//         //     System.out.println();
+//         // }
+
+//         int maxCluster = 1;
+//         for(int i = 0; i < numOfBombs; i ++){
+//             boolean[] visited = new boolean[numOfBombs];
+//             visited[i] = true;
+//             maxCluster = Math.max(maxCluster, dfs(i, adjList, visited));
+//         }
+//         return maxCluster;
+
+//     }
+
+//     private boolean checkIfConnected(int[][] bombs, int b1, int b2){
+//         int[] bomb1 = bombs[b1];
+//         int[] bomb2 = bombs[b2];
+
+//         //considering long instead of int to avoid overflow conditions
+//         long x1 = bomb1[0];
+//         long y1 = bomb1[1];
+//         long r1 = bomb1[2];
+
+//         long x2 = bomb2[0];
+//         long y2 = bomb2[1];
+//         long r2 = bomb2[2];
+
+//         //if distBetweenBombs is less than equal to either of the bomb's radius, then they are connected
+//         // double distBetweenBombs = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+//         double distBetweenBombs = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+
+//         // if(distBetweenBombs <= r1 || distBetweenBombs <= r2){
+//         if(distBetweenBombs <= r1 * r1){
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     private int dfs(int bomb, HashMap<Integer, List<Integer>> adjList, boolean[] visited){
+//         int nodesTraversed = 0;
+//         for(int childBomb : adjList.get(bomb)){
+//             if(!visited[childBomb]){
+//                 visited[childBomb] = true;
+//                 nodesTraversed += dfs(childBomb, adjList, visited);
+//             }
+//         }
+//         return 1 + nodesTraversed;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //     //Solving on 14, 15 Jan 2026
 
 //     //intuition 1: Graphs : DSU
