acb0027a9e8f70562fb53e56fc3c0f024dac8d37
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-02-01 08:51:24 -0500

    Time: 7 ms (17.40%) | Memory: 54.2 MB (14.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,4 +1,86 @@
 class Solution {
+
+    //Re-solving on 01 Feb 2026
+
+    //intuition 1: Graphs :
+        //This is a directed graph problem
+        //If a node i is a town just, then it should not have any children
+        //We need to build an adjacency list
+
+        //Traverse the trust list from 
+    public int findJudge(int n, int[][] trust) {
+        List<Integer>[] adjList = new ArrayList[n + 1];
+
+        for(int i = 1; i < n + 1; i ++){
+            adjList[i] = new ArrayList<>();
+        }
+
+        for(int[] trustNode : trust){
+            int node1 = trustNode[0];
+            int node2 = trustNode[1];
+
+            adjList[node1].add(node2);
+        }
+
+
+        for(int i = 1; i < n + 1; i ++){
+            List<Integer> childNodes = adjList[i];
+
+            if(childNodes.size() == 0){ //i node do not trust anyone. Now check if i node is trusted by everyone
+                if(isJudge(i, adjList, n)){
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+
+
+    private boolean isJudge(int probableJudge, List<Integer>[] adjList, int n){
+        for(int j = 1; j < n + 1; j ++){
+            if(j == probableJudge) continue;
+            if(!adjList[j].contains(probableJudge)){
+                return false;
+            }
+        }
+        return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
     //intuition 1: Have a set for storing not judge people, i.e. the people represented trust[i][0].
     //Also assign judge based on trust[0][1] and then verify for each trust[i]. If any trust[i] have a
     //different trusted person other than previously assigned judge, return -1
@@ -73,31 +155,31 @@ class Solution {
     // }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    static{
-        int i=0;
-        while(i<500){
-            findJudge(2,new int[][]{{1,2}});i++; //HotSpot JIT-compiles the method (findJudge) down to optimized native 
-            //code (because we exercised it 500 times). This warmup time is not included in leetcode's time calculations.
-            //Therefore, when the real execution starts, the optimized code is ran and the calculated time is less.
-        }
-    }
-    public static int findJudge(int n, int[][] trust) {
+    // static{
+    //     int i=0;
+    //     while(i<500){
+    //         findJudge(2,new int[][]{{1,2}});i++; //HotSpot JIT-compiles the method (findJudge) down to optimized native 
+    //         //code (because we exercised it 500 times). This warmup time is not included in leetcode's time calculations.
+    //         //Therefore, when the real execution starts, the optimized code is ran and the calculated time is less.
+    //     }
+    // }
+    // public static int findJudge(int n, int[][] trust) {
         
-        if(n == 1 && trust.length == 0) return 1;
-        int[] trustCount = new int[n + 1];
+    //     if(n == 1 && trust.length == 0) return 1;
+    //     int[] trustCount = new int[n + 1];
 
-        for(int[] edge : trust){
-            int a = edge[0],b = edge[1];
-            trustCount[a]--;
-            trustCount[b]++;
-        }
+    //     for(int[] edge : trust){
+    //         int a = edge[0],b = edge[1];
+    //         trustCount[a]--;
+    //         trustCount[b]++;
+    //     }
 
-        for(int i = 1; i <= n; i++){
-            if(trustCount[i] == n - 1){
-                return i;
-            }
-        }
+    //     for(int i = 1; i <= n; i++){
+    //         if(trustCount[i] == n - 1){
+    //             return i;
+    //         }
+    //     }
 
-        return -1;
-    }
+    //     return -1;
+    // }
 }
