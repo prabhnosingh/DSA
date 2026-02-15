d61dbe86b2dad7da15151cd148440b53f00d2eff
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-02-15 14:46:12 -0500

    Time: 0 ms (100.00%) | Memory: 45 MB (59.03%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,82 +1,31 @@
 class Solution {
+
+    //Re-solving on 15 Feb 2026
+
+    //intuition 1: DP : 1D
+        //Work backwards, start from top and see what options you have to reach there
+        //We can have a dp array to store the min cost to reach each stair
+
+        //then at each step we analayze and choose the minimum cost including current cost
+            //and minimum of minimum cost to reach previous two steps
+        //dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2])
     public int minCostClimbingStairs(int[] cost) {
-        
-        int len = cost.length;
-        int first = cost[len - 2];
-        int second = cost[len - 1];
+        //dp[i] will represent minimum cost needed to reach top given i steps
+        //dp[totalStairs-1] will represent min cost needed to reach top  
+        //therefore, we need dp array of length totalStairs
 
-        int i = len - 3;
-        while(i >= 0){
-            int temp = first;
+        int totalStairs = cost.length;
+        int[] dp = new int[totalStairs];
 
-            first = Math.min(cost[i] + first, cost[i] + second);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-            second = temp;
-            i --;
+        for(int i = 2; i < totalStairs; i ++){
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
         }
 
-        return Math.min(first, second);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int len = cost.length;
-        // int first = cost[len-1];
-        // int second = Math.min(cost[len - 2], first + cost[len - 2]); 
-        // int i = len - 3;
-
-        // while(i >=0){
-        //     int temp = second;
-        //     second = Math.min(cost[i] + first, cost[i] + second);
-        //     first = temp;
-        //     i --;
-
-        // }
-
-        // return Math.min(first, second);
-
-
-
-
-        
-//***************************************************************************************** */        
-        // int len = cost.length;
-        // int[] dp = new int[len];
-
-        // dp[len - 1] = cost[len - 1];
-        // dp[len - 2] = Math.min(cost[len - 2], cost[len - 2] + cost[len - 1]);
-        
-        // for(int i = len - 3; i >= 0; i--){
-        //     dp[i] = Math.min(cost[i] + dp[i+1], cost[i] + dp[i + 2]);
-        // }
-
-        // int ans = dp[0];
-        // if(dp[1] < dp[0]) ans = dp[1];
-        
-        // return ans;
+        return Math.min(dp[totalStairs - 1], dp[totalStairs - 2]); //this is becase we can jump 1
+        //or 2 steps from last or second last step to reach top, and we should take the minimum of the
+        //both
     }
 }
