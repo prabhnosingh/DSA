a3c563b05860fa97c6d757c5c2af77dd00339dfb
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-02-18 22:47:03 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (21.36%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,32 +1,51 @@
 class Solution {
 
-
-    public int computeMaxRob(int[] arr){
-
-        if(arr.length == 1){
-            return arr[0];
-        }
-        int n = arr.length;
-        int[] dp = new int[n];
-
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0], arr[1]);
-
-        for(int i = 2; i < n; i++){
-
-            dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
-        }
-
-        return dp[n-1];
-    }
+    //Re-solving on 18 Feb 2026
+    
+    //intuition 1: DP : 1D DP
+        //Implement the same solution as house robber I with additional check on first and 
+            //house. => This does not work as expected. Instead have a helper function that takes
+            //array as input and gives maximum robbable amount using House Robber I logic. Now
+            //pass original array once from 0 to n-2 and once from 1 to n-1 and finally take the
+            //maximum of the two. 
+        //So basically consider two scenarios, first one when we do not have the last element and
+            //the second one when we do not have the first element.
+        //House robber I:
+            //For each house we have two options, either to steal the current house + dp[i-2]
+                //and leave the dp[i-1] loot or take the dp[i-1] loot and skip the current house.
     public int rob(int[] nums) {
-        
-        if(nums.length == 1){
-            return nums[0];
+        //dp[i] represents maximum money robbed till house i
+        //dp[nums.length-1] will represent maximum money robbed till house nums.length-1
+        //Therefore, we need a dp array of length nums.length
+
+        if(nums.length == 1) return nums[0];
+
+        int skippingLast = maxRobbable(Arrays.copyOfRange(nums, 0, nums.length-1));
+        int skippingFirst = maxRobbable(Arrays.copyOfRange(nums, 1, nums.length));
+
+        return Math.max(skippingLast, skippingFirst);
+    }
+
+
+    private int maxRobbable(int[] nums){
+        int houses = nums.length;
+
+        if(houses == 1) return nums[0];
+
+        int[] dp = new int[houses];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        if(houses == 2) return dp[1];
+
+        for(int i = 2; i < houses; i ++){
+            int rob = dp[i-2] + nums[i];
+            int doNotRob = dp[i-1];
+
+            dp[i] = Math.max(rob, doNotRob);
         }
-        int one = computeMaxRob(Arrays.copyOfRange(nums, 0, nums.length - 1));
-        int two = computeMaxRob(Arrays.copyOfRange(nums, 1, nums.length));
-       return Math.max(one, two);
-        
+
+        return dp[houses - 1];
     }
 }
