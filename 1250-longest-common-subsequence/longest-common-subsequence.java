78f5946215c80180e41dbfa8b6b5c323a6bff953
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-03-03 08:54:27 -0500

    Time: 29 ms (18.41%) | Memory: 54.1 MB (33.62%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,61 +1,134 @@
 class Solution {
 
-    //Solving on 14 Dec 2025:
+    //Re-solving on 03 Mar 2025:
 
-    //intuition 1 (DP : 2D : Bottom-up Tabulation Solution : DP on strings pattern / DP on prefixes of strings)
-        //Recurrence relation: 
-            //We have two scenarios:
-                //1. When current char from text1 == current char from text2. In this case we increase our LCS
-                    //count by 1 and add it to dp state where both of the current chars were not added (dp[i-1][j-1])
-                    //This gives dp[i][j] = 1 + dp[i-1][j-1]
-                //2. When the current char from text1 != current char from text2. In this case we take max of two cases:
-                    //i. What if current char from text1 was not added, resulting in i-1 length of text1? What will be the 
-                        //dp state in that case? => dp[i-1][j]
-                    //ii. What if current char from text2 was not added, resulting in j-1 length of text2? What will be
-                        //the dp state in that case? => dp[i][j-1]
-                    //This gives dp[i][j] = Math.max(dp[i-1][j] + dp[i][j-1])
+    //intuition 2: Recursion + memoization 
+        //bigger problem: length of common subsequence given text1 string till
+            //idx text1.length-1 and text2 string till idx text2.length-1. 
+        //The parameters of the recursive function will be idx1, idx2 and return type
+            //should be int.
+        //Smaller sub-problem will be the find length of common subsequence while
+            //reducing the idx of each string till 0
 
-        //Base cases:
-            //Given empty text1, the LCS is 0, therefore, fill the first row with 0
-            //Given empty text2, the LCS is 0, therefore, fill the first col with 0
+        //Recurrence relation:
+            //expression: 
+                //f(idx1, idx2)
+
+            //exploring all possibilities:
+                //if characers from each string match 
+                    //i.e. text1.charAt(idx1) == text2.charAt(idx2)
+                //f(idx1-1, idx2-1) then reduce both idx1 and idx2 by 1 and return 1
+
+                //if characters from each string do not match
+                //then explore all possibilities:
+                    //f(idx1-1, idx2) reducing idx1 and keeping idx2 same
+                    //f(idx1, idx2-1) keeping idx1 same and reducing idx2
+                    //f(idx1-1, idx2-1) recucing both idx1 and idx2
+                
+            //return
+                //pick the max of all the possibilities and return
+
+        //base cases:
+            //if any idx goes below 0 (-1) return 0
+            //if both chars match , return 1
+
+
+        //TC:
+        //SC:
+
+        //memoization:
+            //variable parameters are idx1 and idx2
+            //therefore we can have a 2D dp matrix of size text1.length x text2.length
+            //here dp[i][j] will represent, longest common subsequence given text1 till 
+                //index i and text2 till index j
+            
    
 
     public int longestCommonSubsequence(String text1, String text2) {
-        //dp[i][j] represents LCS between text1 till i length and text2 till j length
-        //dp[tex1.length()][text2.length()] will represent LCS between text1 and text2
-        //Therefore, we need a 2D matrix of size text1.length()+1 x text2.length()+1
+        int[][] dp = new int[text1.length()][text2.length()];
 
-        int rows = text1.length() + 1;
-        int cols = text2.length() + 1;
-
-        int[][] dp = new int[rows][cols];
-
-        //filling base cases
-        
-        dp[0][0] = 0;
-
-        //filling first row
-        for(int j = 1; j < cols; j ++){
-            dp[0][j] = 0;
+        for(int i = 0; i < text1.length(); i ++){
+            Arrays.fill(dp[i], -1);
         }
-
-        //filling first col
-        for(int i = 1; i < rows; i ++){
-            dp[i][0] = 0;
-        }
-
-        for(int i = 1; i < rows; i ++){
-            for(int j = 1; j < cols; j ++){
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-
-            }
-        }
-
-        return dp[rows-1][cols-1];
-
+        return recurse(text1.length()-1, text2.length()-1, text1, text2, dp);
 
     }
+
+    private int recurse(int idx1, int idx2, String text1, String text2, int[][] dp){
+        if(idx1 == -1 || idx2 == -1) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        if(text1.charAt(idx1) == text2.charAt(idx2)){
+            return 1 + recurse(idx1-1, idx2-1, text1, text2, dp);
+        } 
+
+        int pos1 = recurse(idx1 - 1, idx2, text1, text2, dp);
+        int pos2 = recurse(idx1, idx2 - 1, text1, text2, dp);
+        // int pos3 = recurse(idx1-1, idx2-1, text1, text2); //not necessary 
+            //as already covered under pos1 and pos2
+
+        dp[idx1][idx2] = Math.max(pos1, pos2);
+        // return Math.max(Math.max(pos1, pos2), pos3);
+        return Math.max(pos1, pos2);
+        
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // //Re-solving on 03 Mar 2025:
+
+    // //intuition 1: recursion
+    //     //bigger problem: length of common subsequence given text1 string till
+    //         //idx text1.length-1 and text2 string till idx text2.length-1. 
+    //     //The parameters of the recursive function will be idx1, idx2 and return type
+    //         //should be int.
+    //     //Smaller sub-problem will be the find length of common subsequence while
+    //         //reducing the idx of each string till 0
+
+    //     //Recurrence relation:
+    //         //expression: 
+    //             //f(idx1, idx2)
+
+    //         //exploring all possibilities:
+    //             //if characers from each string match 
+    //                 //i.e. text1.charAt(idx1) == text2.charAt(idx2)
+    //             //f(idx1-1, idx2-1) then reduce both idx1 and idx2 by 1 and return 1
+
+    //             //if characters from each string do not match
+    //             //then explore all possibilities:
+    //                 //f(idx1-1, idx2) reducing idx1 and keeping idx2 same
+    //                 //f(idx1, idx2-1) keeping idx1 same and reducing idx2
+    //                 //f(idx1-1, idx2-1) recucing both idx1 and idx2
+                
+    //         //return
+    //             //pick the max of all the possibilities and return
+
+    //     //base cases:
+    //         //if any idx goes below 0 (-1) return 0
+    //         //if both chars match , return 1
+   
+
+    // public int longestCommonSubsequence(String text1, String text2) {
+       
+    //     return recurse(text1.length()-1, text2.length()-1, text1, text2);
+
+    // }
+
+    // private int recurse(int idx1, int idx2, String text1, String text2){
+    //     if(idx1 == -1 || idx2 == -1) return 0;
+    //     if(text1.charAt(idx1) == text2.charAt(idx2)){
+    //         return 1 + recurse(idx1-1, idx2-1, text1, text2);
+    //     } 
+
+    //     int pos1 = recurse(idx1 - 1, idx2, text1, text2);
+    //     int pos2 = recurse(idx1, idx2 - 1, text1, text2);
+    //     // int pos3 = recurse(idx1-1, idx2-1, text1, text2); //not necessary 
+    //         //as already covered under pos1 and pos2
+
+    //     // return Math.max(Math.max(pos1, pos2), pos3);
+    //     return Math.max(pos1, pos2);
+        
+    // }
 
 
 
@@ -87,6 +160,98 @@ class Solution {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Solving on 14 Dec 2025:
+
+//     //intuition 1 (DP : 2D : Bottom-up Tabulation Solution : DP on strings pattern / DP on prefixes of strings)
+//         //Recurrence relation: 
+//             //We have two scenarios:
+//                 //1. When current char from text1 == current char from text2. In this case we increase our LCS
+//                     //count by 1 and add it to dp state where both of the current chars were not added (dp[i-1][j-1])
+//                     //This gives dp[i][j] = 1 + dp[i-1][j-1]
+//                     //Suproblem reduces to finding the LCS on strings where both of the matching chars were not added
+//                 //2. When the current char from text1 != current char from text2. In this case we take max of two cases:
+//                     //i. What if current char from text1 was not added, resulting in i-1 length of text1? What will be the 
+//                         //dp state in that case? => dp[i-1][j] => We can do this as in a subsequence skipping charcters 
+//                         //is allowed.
+//                         //Subproblem reduces to finding the LCS on strings where text1's latest char was not added
+//                     //ii. What if current char from text2 was not added, resulting in j-1 length of text2? What will be
+//                         //the dp state in that case? => dp[i][j-1] => We can do this as in a subsequence skipping characters
+//                         //is allowed.
+//                         //Subproblem reduces to finding the LCS on strings where text2's latest char was not added
+//                     //This gives dp[i][j] = Math.max(dp[i-1][j] + dp[i][j-1])
+
+//         //Base cases:
+//             //Given empty text1, the LCS is 0, therefore, fill the first row with 0
+//             //Given empty text2, the LCS is 0, therefore, fill the first col with 0
+   
+
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         //dp[i][j] represents LCS between text1 till i length and text2 till j length
+//         //dp[tex1.length()][text2.length()] will represent LCS between text1 and text2
+//         //Therefore, we need a 2D matrix of size text1.length()+1 x text2.length()+1
+
+//         int rows = text1.length() + 1;
+//         int cols = text2.length() + 1;
+
+//         int[][] dp = new int[rows][cols];
+
+//         //filling base cases
+        
+//         dp[0][0] = 0;
+
+//         //filling first row
+//         for(int j = 1; j < cols; j ++){
+//             dp[0][j] = 0;
+//         }
+
+//         //filling first col
+//         for(int i = 1; i < rows; i ++){
+//             dp[i][0] = 0;
+//         }
+
+//         for(int i = 1; i < rows; i ++){
+//             for(int j = 1; j < cols; j ++){
+//                 if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i-1][j-1];
+//                 else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+
+//             }
+//         }
+
+//         return dp[rows-1][cols-1];
+
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //     //Solving on 14 Dec 2025:
 
 //     //intuition 1 (DP : 2D: Bottom-Up Tabulation Solution: DP on strings pattern / DP on prefixes of strings)
