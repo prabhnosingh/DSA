b40a8b508f199c4ac7e2773db340498db83fcc27
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-03-15 22:45:33 -0400

    Time: 5 ms (79.43%) | Memory: 44.9 MB (49.67%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,85 +1,183 @@
 class Solution {
-    //Re-solving on 07 Mar 2026:
+    //Re-solving on 15 Mar 2026:
 
     //This problem can be solved with DP/memoization because:
         //It is finding total number of expressions and not the actual expressions
         //Any particular state depends on already computed state
 
-    //intuition 2: dfs : backtracking : memoization  
-        //each number in nums have an option to be prefixed with either '+' or '-'
-        //we can explore all the possibilities to find which of these compute to target
-        //"for each nubmer, recursively choose either +nums[i] or -nums[i] and update 
-            //currSum accordingly" 
-        //base case:
-            //when currIdx becomes equal to length of nums:
-                //and currSum equal to target, then return 1 
-                //else return 0
 
-        //what if target is 0? -> only check if the currIdx becomes equal to nums length
 
-        
-        //memoization: the variables are currSum and currIdx
-            //max currSum can be total sum of nums
-            //max currIdx can be nums.length-1
 
-            //therefore we can have a 2d memo array of size totalSum + 1 x nums.length
+    //intuition 3: 2D DP
+        //Assuming P is the sum of numbers marked as + and N is the sum of numbers
+            //marked as -, we have P-N = target and P+N = totalSum
+        //This boils downt to 2P = totalSum + target => P = (totalSum + target)/2
 
-        //TC: O(2^n) : for each number we have two choices, either to choose it as positive
-            //or to choose it as negative
-            //"each of the n numbers creates 2 branches"
-            //"total leaf expressions = 2^n"
-        //Sc: O(n) : size of recursive stack. Height of the tree
+        //So the problem now becomes finding number of subsets that form a total sum
+            //of (totalSum + target) / 2
 
+        //Recurrence relation:
+            //There are two options, either to pick the current number or not pick it
+            //Pick it: We cannot pick the number if it is greater than current j
+                //if we choose the current number then our target reduces by j - currNum
+                    //and the number of available numbers also reduce by 1.
+                //Therefore, current dp state depends on previous dp state 
+                    //dp[i][j] = dp[i-1][j-currNum]
+            //Not pick it:
+                //if we do not choose the current number, then our target remains the same
+                    //and the number of available numbers reduce by 1.
+                //Therefore, current dp state depends on previous dp state 
+                    //dp[i][j] = dp[i-1][j]
+            
+            //we add result from both the options to get answer for our state
+
+            //dp[i][j] = dp[i-1][j-currNum] + dp[i-1][j]
+
+        //Base case:
+            //dp[0][0] = 1
+            //given 0 numbers and non-zero target, there are 0 ways to form the target
+                //fill the first row with 0s starting from column 1
+            //given 0 target and non-zero numbers, there is 1 way to form the target
+                //fill the first col with 1s starting from row 1
 
 
     public int findTargetSumWays(int[] nums, int target) {
+        //dp[i][j] will represent number of ways to form total sum j with first
+            //i numbers from nums
+        //dp[nums.length][(totalSum+target)/2] will represent number of ways to 
+            //form total sum (totalSum+target)/2 given all nums
         
+        //Therefore we need a 2D matrix of size nums.length+1 x (totalSum+target)/2+1
+       
         int totalSum = 0;
-        // for(int num : nums){
-        //     totalSum += num;
+        for(int num : nums) totalSum += num;
+
+        if(Math.abs(target) > totalSum) return 0; //not possible to form this target
+        if((totalSum + target) % 2 != 0) return 0; //because (totalSum + target)/2 
+            //must be an integer
+
+        int rows = nums.length + 1;
+        int cols = ((totalSum + target) / 2) + 1;
+
+        int[][] dp = new int[rows][cols];
+
+        //base cases
+        dp[0][0] = 1;
+
+        //filling first row
+        for(int j = 1; j < cols; j ++){
+            int i = 0;
+            dp[i][j] = 0;
+        }
+
+        // //filling first col -> not valid in case a number is a 0
+        // for(int i = 1; i < rows; i ++){
+        //     int j = 0;
+        //     dp[i][j] = 1;
         // }
-        // if(totalSum < target){
-        //     return 0;
-        // }
 
-        // int[][] dp = new int[totalSum+1][nums.length];
-        HashMap<String, Integer> memo = new HashMap<>();
 
-        // for(int i = 0; i < totalSum+1; i ++){
-        //     Arrays.fill(dp[i], -1);
-        // }
-        
-        return dfs(nums, target, 0, 0, memo);
+        for(int i = 1; i < rows; i ++){
+            // for(int j = 1; j < cols; j ++){
+            for(int j = 0; j < cols; j ++){ //to account for currNum = 0
+                int currNum = nums[i-1];
+                int pick = 0;
+                int notPick = 0;
 
-    }
+                if(currNum <= j){
+                    pick = dp[i-1][j-currNum];
+                }
+                
+                notPick = dp[i-1][j];
 
-    private int dfs(int[] nums, int target, int currSum, int currIdx, 
-        HashMap<String, Integer> memo){
-        // if(currSum == target) return 1;
-        String key = currSum + "_" + currIdx;
-        if(memo.containsKey(key)) return memo.get(key);
+                dp[i][j] = pick + notPick;
 
-        if(currIdx == nums.length){ //we should only check whether the target is met
-            //or not when the whole expression is traversed
-            if(currSum == target) return 1;
-            else return 0;
+            }
+        }
 
-        } 
+        return dp[rows-1][cols-1];
 
-        int currNum = nums[currIdx];
-        
-        //negative currNum
-        int option1 = dfs(nums, target, currSum + (currNum * -1), currIdx + 1, memo);
 
-        //positive currNum
-        int option2 = dfs(nums, target, currSum + currNum, currIdx + 1, memo);
-        
-        memo.put(key, option1 + option2);
 
-        return option1 + option2;
     }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-solving on 07 Mar 2026:
+
+//     //This problem can be solved with DP/memoization because:
+//         //It is finding total number of expressions and not the actual expressions
+//         //Any particular state depends on already computed state
+
+//     //intuition 2: dfs : backtracking : memoization  
+//         //each number in nums have an option to be prefixed with either '+' or '-'
+//         //we can explore all the possibilities to find which of these compute to target
+//         //"for each nubmer, recursively choose either +nums[i] or -nums[i] and update 
+//             //currSum accordingly" 
+//         //base case:
+//             //when currIdx becomes equal to length of nums:
+//                 //and currSum equal to target, then return 1 
+//                 //else return 0
+
+//         //what if target is 0? -> only check if the currIdx becomes equal to nums length
+
+        
+//         //memoization: the variables are currSum and currIdx
+//             //max currSum can be total sum of nums
+//             //max currIdx can be nums.length-1
+
+//             //therefore we can have a 2d memo array of size totalSum + 1 x nums.length
+
+//         //TC: O(2^n) : for recursion
+//         //for each number we have two choices, either to choose it as positive
+//             //or to choose it as negative
+//             //"each of the n numbers creates 2 branches"
+//             //"total leaf expressions = 2^n"
+
+//         //TC: O(n * rangeOfPossibleSums) 
+//             //since the sum can vary from -totalSum to +totalSum, states are:
+//                 //O(n * (2 * totalSum + 1))
+//         //Sc: O(n) : size of recursive stack + memo. Height of the tree
+
+
+
+//     public int findTargetSumWays(int[] nums, int target) {
+        
+//         int totalSum = 0;
+
+//         HashMap<String, Integer> memo = new HashMap<>();
+
+  
+//         return dfs(nums, target, 0, 0, memo);
+
+//     }
+
+//     private int dfs(int[] nums, int target, int currSum, int currIdx, 
+//         HashMap<String, Integer> memo){
+//         // if(currSum == target) return 1;
+//         String key = currSum + "_" + currIdx;
+//         if(memo.containsKey(key)) return memo.get(key);
+
+//         if(currIdx == nums.length){ //we should only check whether the target is met
+//             //or not when the whole expression is traversed
+//             if(currSum == target) return 1;
+//             else return 0;
+
+//         } 
+
+//         int currNum = nums[currIdx];
+        
+//         //negative currNum
+//         int option1 = dfs(nums, target, currSum + (currNum * -1), currIdx + 1, memo);
+
+//         //positive currNum
+//         int option2 = dfs(nums, target, currSum + currNum, currIdx + 1, memo);
+        
+//         memo.put(key, option1 + option2);
+
+//         return option1 + option2;
+//     }
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////
     // //Re-solving on 07 Mar 2026:
 
     // //intuition 1: dfs : backtracking   
