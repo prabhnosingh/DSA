7515258be17fbd13ef3a83bee81f8a0e22920741
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-03-17 08:37:09 -0400

    Time: 1 ms (85.24%) | Memory: 44.3 MB (85.29%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,102 +1,161 @@
 class Solution {
-    //Re-sovling on 16 Dec 2025:
+    //Re-sovling on 17 Mar 2026:
 
-    //intuition 2(improved intuition 2): (dfs: Backtracking: decision tree on parentheses: constrained branching) 
-        // A parentheses string is well-formed if:
-            // 1) At any point, closing parentheses do not exceed opening ones
-            // 2) Total number of '(' and ')' is exactly n each
+    //intuition 1 : dfs : Backtracking
+        //both opening and closing brackets should be of n count
+        //every combination must start with "("
+        //we cannot close any bracket that is not opened yet, therefore,
+            //keep track of opening brackets and make the option of
+            //closing a bracket available only if the number of open 
+            //brackets are greater than 0
+        //open brackets should not exceed n. Closing brackets will
+            //take care of itself
+        //given the above constraints, we have two options for each
+            //position, either to open the bracket or to close it.
         
-        // We build the string one character at a time.
-        // 1️⃣ What does one level of recursion represent?
-            // Each recursion level represents adding one parenthesis character to the current combination.
-        
-        // 2️⃣ What are the choices at that level?
-            // We have two possible choices:
-                //i. Add '(' if openCount < n
-                //ii. Add ')' if closeCount < openCount
-            // These conditions ensure the string remains valid while being built.
-        
-        // 3️⃣ When do we stop?
-            //When the length of the current combination becomes 2*n, meaning we have placed all parentheses correctly.
-        
-        // Backtracking invariant:
-            // After each recursive call, restore the state so that other choices can be explored independently.
-
-        //TC (exponential): O(Cn · n) = ~O(4^n / sqrt(n)) : "where Cn is the nth Catalan number"
-            //"The number of valid sequences of n pairs of parentheses is the n-th Catalan number"
-        //SC: O(2n)
-
-
-
-    //intuition 2: (dfs: Backtracking: decision tree on parentheses: constrained branching) : Without using hashMap
-        //A parentheses is well formed if all the sub parentheses are closed.
-        //"A parentheses string is well-formed if at any point the number of closing parentheses does not exceed 
-            //the number of opening ones, and both reach n at the end."
-        //At each level we will have two choices either to add a new opening parentheses (if n != openCount)
-            //or close a parenthesis (if n != openCount && closeCount < openCount).  Maintain an opening and
-            //closing count and do not close a parentheses if there is more open parentheses than close parentheses
-
-        //1. What does one level of recursion represents?
-            //A single parenthesis being processed
-            //currComb.append -> represents going one level deep in recursion
-        //2. What are my choices at that level?
-            //At each level, we have two choices:
-                //1. Either to close a parentheses (if openCount is greater than closeCount)
-                //2. Or, to open a parentheses (if n != openCount)
-        //3. When do I stop? (base case) 
-            //when n == openCount && n == closeCount or currComb.length() == n*2
-            //"When we have placed exactly n opening and n closing parentheses, we have formed a valid combination."
-
-        //TC: O(2^n) : For each step we have two decisions to make
-        //SC: o(n)
-    
     public List<String> generateParenthesis(int n) {
-        List<String> combs = new ArrayList<>();
-       
+        int open = 0;
+        int close = 0;
 
-        dfs(n, new StringBuilder(), combs, 0, 0);
+        List<String> combs = new ArrayList<>();
+
+        dfs(n, combs, new StringBuilder(), open, close);
 
         return combs;
 
     }
 
-    private void dfs(int n, StringBuilder currComb, List<String> combs, int openCount, int closeCount){
-        if(currComb.length() == n*2){
+    private void dfs(int n, List<String> combs, StringBuilder currComb, int open,
+    int close){
+        if(open == n && close == n){
             combs.add(new String(currComb));
             return;
         }
 
-
-        if(n > openCount){
-            //open a parentheses    
+        if(open < n){
             currComb.append('(');
-            openCount += 1;
-            
-            dfs(n, currComb, combs, openCount, closeCount);
-            
-            currComb.setLength(currComb.length() - 1);
-            openCount -= 1;
-
+            dfs(n, combs, currComb, open + 1, close);
+            currComb.deleteCharAt(currComb.length() - 1);
         }
 
-        if(n > closeCount && closeCount < openCount){
-            //close a parentheses    
+        if(close < n && open != 0 && open != close){
+            //do not close when open is 0
+            //do not close when open is equal to close, since there should be
+                //at least 1 open bracket to be closed
             currComb.append(')');
-            closeCount += 1;
-
-            dfs(n, currComb, combs, openCount, closeCount);
-        
-            currComb.setLength(currComb.length() - 1);
-            closeCount -= 1;
-        
+            dfs(n, combs, currComb, open, close + 1);
+            currComb.deleteCharAt(currComb.length() - 1);
         }
+
     }
+
+
+
+
 
 
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-sovling on 16 Dec 2025:
+
+//     //intuition 2(improved intuition 2): (dfs: Backtracking: decision tree on parentheses: constrained branching) 
+//         // A parentheses string is well-formed if:
+//             // 1) At any point, closing parentheses do not exceed opening ones
+//             // 2) Total number of '(' and ')' is exactly n each
+        
+//         // We build the string one character at a time.
+//         // 1️⃣ What does one level of recursion represent?
+//             // Each recursion level represents adding one parenthesis character to the current combination.
+        
+//         // 2️⃣ What are the choices at that level?
+//             // We have two possible choices:
+//                 //i. Add '(' if openCount < n
+//                 //ii. Add ')' if closeCount < openCount
+//             // These conditions ensure the string remains valid while being built.
+        
+//         // 3️⃣ When do we stop?
+//             //When the length of the current combination becomes 2*n, meaning we have placed all parentheses correctly.
+        
+//         // Backtracking invariant:
+//             // After each recursive call, restore the state so that other choices can be explored independently.
+
+//         //TC (exponential): O(Cn · n) = ~O(4^n / sqrt(n)) : "where Cn is the nth Catalan number"
+//             //"The number of valid sequences of n pairs of parentheses is the n-th Catalan number"
+//         //SC: O(2n)
+
+
+
+//     //intuition 2: (dfs: Backtracking: decision tree on parentheses: constrained branching) : Without using hashMap
+//         //A parentheses is well formed if all the sub parentheses are closed.
+//         //"A parentheses string is well-formed if at any point the number of closing parentheses does not exceed 
+//             //the number of opening ones, and both reach n at the end."
+//         //At each level we will have two choices either to add a new opening parentheses (if n != openCount)
+//             //or close a parenthesis (if n != openCount && closeCount < openCount).  Maintain an opening and
+//             //closing count and do not close a parentheses if there is more open parentheses than close parentheses
+
+//         //1. What does one level of recursion represents?
+//             //A single parenthesis being processed
+//             //currComb.append -> represents going one level deep in recursion
+//         //2. What are my choices at that level?
+//             //At each level, we have two choices:
+//                 //1. Either to close a parentheses (if openCount is greater than closeCount)
+//                 //2. Or, to open a parentheses (if n != openCount)
+//         //3. When do I stop? (base case) 
+//             //when n == openCount && n == closeCount or currComb.length() == n*2
+//             //"When we have placed exactly n opening and n closing parentheses, we have formed a valid combination."
+
+//         //TC: O(2^n) : For each step we have two decisions to make
+//         //SC: o(n)
+    
+//     public List<String> generateParenthesis(int n) {
+//         List<String> combs = new ArrayList<>();
+       
+
+//         dfs(n, new StringBuilder(), combs, 0, 0);
+
+//         return combs;
+
+//     }
+
+//     private void dfs(int n, StringBuilder currComb, List<String> combs, int openCount, int closeCount){
+//         if(currComb.length() == n*2){
+//             combs.add(new String(currComb));
+//             return;
+//         }
+
+
+//         if(n > openCount){
+//             //open a parentheses    
+//             currComb.append('(');
+//             openCount += 1;
+            
+//             dfs(n, currComb, combs, openCount, closeCount);
+            
+//             currComb.setLength(currComb.length() - 1);
+//             openCount -= 1;
+
+//         }
+
+//         if(n > closeCount && closeCount < openCount){
+//             //close a parentheses    
+//             currComb.append(')');
+//             closeCount += 1;
+
+//             dfs(n, currComb, combs, openCount, closeCount);
+        
+//             currComb.setLength(currComb.length() - 1);
+//             closeCount -= 1;
+        
+//         }
+//     }
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // //Re-sovling on 16 Dec 2025:
 
     // //intuition 1: (dfs: Backtracking: decision tree on parentheses: constrained branching)
