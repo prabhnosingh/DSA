e9edc076bac1879cdc9da32f40377c5ed14bf6ed
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-07-04 21:58:40 -0400

    Time: 0 ms (100.00%) | Memory: 46.4 MB (62.08%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -1,14 +1,72 @@
 class Solution {
-    //intuition 1: Start from index 0, the first index to match the condition is the answer
+    //Solving on 04 July 2026
+
+    //intuition 1 (brute force): Run a for loop and check the logic for all the numbers and
+        //return the first encountered index 
     public int smallestEqual(int[] nums) {
-        int minIdx = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i ++){
-            if(i % 10 == nums[i]){
-                // minIdx = Math.min(minIdx, i);
+            if(nums[i] == (i % 10)){
                 return i;
             }
         }
-    // return minIdx == Integer.MAX_VALUE ? -1 : minIdx;
-    return -1;
+
+        return -1;
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // //intuition 1 (brute force): Run a for loop and check the logic for all the numbers and
+    //     //return the first encountered index 
+    // public int smallestEqual(int[] nums) {
+    //     for(int i = 0; i < nums.length; i ++){
+    //         if(nums[i] == (i % 10)){
+    //             return i;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   ///////////////////////////////////////////////////////////////////////////////////////////// 
+    
+    
+    
+    
+    // //intuition 1: Start from index 0, the first index to match the condition is the answer
+    // public int smallestEqual(int[] nums) {
+    //     int minIdx = Integer.MAX_VALUE;
+    //     for(int i = 0; i < nums.length; i ++){
+    //         if(i % 10 == nums[i]){
+    //             // minIdx = Math.min(minIdx, i);
+    //             return i;
+    //         }
+    //     }
+    // // return minIdx == Integer.MAX_VALUE ? -1 : minIdx;
+    // return -1;
+    // }
 }
