35226bd8d3103626ac164d358b22a589e0c6dd99
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-06 23:02:25 -0400

    Time: 0 ms (100.00%) | Memory: 43.7 MB (56.81%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,46 +1,99 @@
 class Solution {
+
+    //Solving on 06 Aug 2026
+
+    //Intuition 1: Binary search
+        //We can observe that the array after rotating leads to two different sorted arrays
+        //Example in [4,5,6,7,0,1,2], [4,5,6,7] is first sorted array and [0,1,2] is second sorted array
+        //We apply binary search and compare the element at mid with left and right elements
+            //if nums[mid] > nums[right] 
+                //then that means that the minimum lies in the second sub array
+                //In this case we move left towards mid (left = mid + 1) - exclude the mid as 
+                    //it can never be our answer (smaller element exist at nums[right])
+            //if nums[mid] < nums[right] 
+                //then that means that the minimum lies towards left
+                //In this case we move right towards mid (right = mid) - keep the mid as it can 
+                    //be our answer
+            
+    
     public int findMin(int[] nums) {
+        
+        int left = 0;
+        int right = nums.length - 1;
 
-        int start = 0;
-        int end = nums.length-1;
-        if(end+1 ==1){
-            return nums[0];
-        }            
-       int min = nums[0];
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        while(left < right){
+            int mid = left + (right - left) / 2;
 
-            if( mid > 0 && nums[mid] < nums[mid-1] ){
-                return nums[mid];
-            }
+            int currNum = nums[mid];
 
-            if(nums[start] <= nums[mid] && nums[mid] > nums[end]){
-                start = mid+1;
+            if(currNum > nums[right]){
+                left = mid + 1;
             }
             else{
-                end = mid-1;
+                right = mid;
             }
         }
-        return nums[start];
-        
-        
-        
-        
-        
-        
-        
-        // int min = nums[0];
-        // while(left <= right){
-        //      int mid = left + (right-left)/2;
-            
-        //     if(nums[mid] < min){
-        //         min = nums[mid];
-        //         left = mid - 1;
-        //     }
-        //     else{
-        //         right = mid+1;
-        //     }
-        // }
-        // return min;
+        return nums[left];
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // //Solving on 13 Sept 2025
+
+    // //inutition 1: the array is divided in two parts. First part is where the elements of last part of original array are coming up and the other part (second
+    // //half), where the elements of the first part of original array are coming up. Our aim is to look for ending of first part or starting of second part.
+
+    // //1st region is from left to mid and 2nd region is from mid + 1 to right 
+    // //The smallest element will lie in the region where there is unsorted elements (due to max element coming before min element).
+    // //Apply binary search, compare if nums[mid] > nums[right], then it means that we are in the unsorted part as of now, and the min element lies in 
+    // //the 2nd region (towards right) so make left = mid + 1 and if nums[mid] < nums[right], then that means that we are in sorted part and min element
+    // //lies in region 1 so make right = mid - 1, until right becomes equal to left 
+    // // 
+    // public int findMin(int[] nums) {
+    //     int left = 0;
+    //     int right = nums.length - 1;
+        
+    //     while(left < right){
+    //         // int mid = right - (right - left) / 2;
+    //         int mid = (right + left) / 2;
+
+    //         // if(nums[mid] > nums[left]){
+    //         //     left = mid + 1;
+    //         // }
+    //         // else if(nums[left] < nums[right]){
+    //         //     right = mid - 1;
+    //         // }
+    //         if(nums[mid] <= nums[right]){ //mid to right is sorted
+    //             right = mid;
+    //         }
+    //         else{ //mid to right is unsorted
+    //             left = mid + 1;
+    //         }
+    //     }
+    //     return nums[left];
+    // }
 }
