6bb1f817e42b3b038ad8fb7b5110648188c7c98f
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-08 19:16:28 -0400

    Time: 0 ms (100.00%) | Memory: 47 MB (30.01%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,49 +14,177 @@
  * }
  */
 class Solution {
-    int depth = 1;
+    //Re-solving on 05 Nov 2025:
+    //intuition 1: BFS
+        //track the depth at each level until no other node is available
+        //use a queue 
+    //intuition 2: DFS
+        //search for depth in left and right subtree recursively and return the max
+            //of both
     public int maxDepth(TreeNode root) {
-        
-        //recursive DFS
-        // if(root == null){
-        //     return 0;
-        // }
+        if(root == null) return 0;
 
-        
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 
-        // int l = maxDepth(root.left);
-        // int r = maxDepth(root.right);
-        // return 1 + Math.max(l, r);
-//***************************************************** */
-
-        //Iterative BFS using a Queue
-
-        if(root == null){
-            return 0;
-        }
-        int height = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.add(root);
-
-        while(!q.isEmpty()){
-
-            height ++;
-            int size = q.size();
-
-            while(size-- > 0){
-                TreeNode temp = q.poll();
-
-                if(temp.left != null){
-                    q.add(temp.left);
-                }
-
-                if(temp.right != null){
-                    q.add(temp.right);
-                }
-            }
-
-        }
-        return height;
     }
+        
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-solving on 05 Nov 2025:
+
+//     //intuition 1: Track the depth at each node recursively and return until a null node is encountered, in which
+//     //case return currDepth - 1. For each node's recursive call return max of left and right recursive calls.
+
+//     // int maxDepth;
+//     public int maxDepth(TreeNode root) {
+//         // maxDepth = 0;
+
+//         return recurse(root);
+//     }
+
+//     private int recurse(TreeNode root){  
+//         if(root == null){
+//             return 0;
+//         }
+
+//         return 1 + Math.max(recurse(root.left), recurse(root.right));        
+//     }
+
+    
+//     // public int maxDepth(TreeNode root) {
+//     //     // maxDepth = 0;
+
+//     //     return recurse(root, 1);
+//     // }
+
+//     // private int recurse(TreeNode root, int currDepth){  
+//     //     if(root == null){
+//     //         return currDepth - 1;
+//     //     }
+
+//     //     return Math.max(recurse(root.left, currDepth + 1), recurse(root.right, currDepth + 1));        
+//     // }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-solving on 04 Nov 2025:
+
+//     //intuition 1: Track the depth at each node recursively and return until a leaf node is encountered 
+//     //i.e. boht right and left nodes are null
+
+//     int maxDepth;
+//     public int maxDepth(TreeNode root) {
+//         if(root == null){
+//             return 0;
+//         }
+//         maxDepth = 0;
+
+//         return dfs(root, 0);
+        
+//     }
+
+//     private int dfs(TreeNode root, int currDepth){
+//         if(root == null){
+//             return currDepth;
+//         }
+//         if(root.left == null && root.right == null){ //leaf node encountered
+//             maxDepth = Math.max(maxDepth, currDepth);
+//         }
+
+//         return Math.max(Math.max(maxDepth, dfs(root.left, currDepth + 1)),
+//          Math.max(maxDepth, dfs(root.right, currDepth + 1)));
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//     // int maxDep = 0;
+//     public int maxDepth(TreeNode root) {
+//         if(root == null){
+//             return 0;
+//         }
+//         // maxDep ++;
+//         int leftDep = maxDepth(root.left) + 1;
+//         int rightDep = maxDepth(root.right) + 1;
+//         return Math.max(leftDep, rightDep);
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////
+
+//BFS 
+
+// class Solution {
+//     public int maxDepth(TreeNode root){
+//         if(root ==  null){
+//             return 0;
+//         }
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         int depth = 0;
+//         while(!queue.isEmpty()){
+//             depth ++;
+//             int size = queue.size();
+//             while (size > 0){ // Traversing 1 level of tree at a time
+                
+//                 TreeNode tempNode = queue.remove();
+
+//                 if(tempNode.left != null){
+//                     queue.add(tempNode.left);
+//                 }
+                
+//                 if(tempNode.right != null){
+//                     queue.add(tempNode.right);
+//                 }
+//                 size --;
+//             }
+        
+//     }
+//     return depth;
+// }
+// }
