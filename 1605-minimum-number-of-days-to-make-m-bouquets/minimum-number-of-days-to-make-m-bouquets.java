e77c9ac876b79dec8d5422dbe094e4b16692d634
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-08 15:22:14 -0400

    Time: 21 ms (24.71%) | Memory: 79.1 MB (98.74%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,74 @@
+class Solution {
+    //Solving on 08 Aug 2026
+
+    //intuition 1:
+        //Brute force will be to check every day starting from 1 till maximum bloomday and
+            //return the first day to satisfy the making of m bouquets
+        //O(bloomDay.length * maxBloomDay)
+    
+    //intuition 2:
+        //Topic: Binary search
+        //Pattern: Binary search on answer
+        //Sub-pattern: Minimum feasible answer
+        
+        //The range of days to look in is [1, maxBoomDay]
+        //We will run binary search on this range and find the lower bound which
+            //makes m bouquets with k adjacent flowers per bouquet 
+
+        //The answer will be -1 if m*k > bloomday.length as we can only have at max 
+            //bloomday.length flowers
+
+        //TC: O(bloomDay.length * log maxBloomDay)
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        if((long)m*k > bloomDay.length) return -1;
+
+        int maxBloomDay = 0;    
+
+        for(int bd : bloomDay){
+            maxBloomDay = Math.max(maxBloomDay, bd);
+        }
+
+        int left = 1;
+        int right = maxBloomDay; //whe don't we do right = maxBloomDay - 1;? is it because we want to 
+            //include it in the range?
+        
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            if(isValidBloomDay(bloomDay, m, k, mid)){
+                right = mid; //currDay(mid) is good and able to make m bouquets with k flowers
+                //so keep it and find smaller day towards left
+            }
+            else{
+                left = mid + 1; //currDay(mid) is not good and not able to make m bouquets, so
+                //we need bigger day, hence look towards right while excluding mid
+            }
+        }
+        return left;
+
+    }
+
+    //O(bloomDay.length)
+    private boolean isValidBloomDay(int[] bloomDay, int m, int k, int currDay){
+        //apply sliding window
+
+        int currFlowers = 0;
+        for(int i = 0; i < bloomDay.length; i ++){
+            if(bloomDay[i] <= currDay){
+                currFlowers += 1; //window continues
+            }
+            else{ //window breaks
+                currFlowers = 0;
+            }
+
+            if(currFlowers == k){//requirement for 1 bouquet met
+                m -= 1; //1 bouquet made
+                currFlowers = 0; //currFlowers reset for next bouquet
+            }
+
+            if(m == 0) return true; //all bouquets complete
+        }
+        return false;
+    }
+}
