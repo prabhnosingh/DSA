7156bf73c93f3509d301653e234c80677f3bbfef
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-09 14:17:37 -0400

    Time: 1 ms (99.07%) | Memory: 46.8 MB (87.80%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -16,33 +16,74 @@
 class Solution {
     //Solving on 09 Aug 2026
 
-    //intuition 1: BFS
-        //Run a level order traversal and find the largest value along the way
+    //intuition 2: DFS
+        //have a global arralist to store max values at each level, where
+            //each index in the arraylist denotes a level
     public List<Integer> largestValues(TreeNode root) {
-        
-        List<Integer> largestValueList = new ArrayList<>();
 
-        if(root == null) return largestValueList;
+        List<Integer> largestValuesList = new ArrayList<>();
 
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
+        dfs(root, 0, largestValuesList);
 
-        while(!queue.isEmpty()){
-            int currQueueSize = queue.size();
-            int currMax = Integer.MIN_VALUE;
-            for(int i = 0; i < currQueueSize; i ++){
-                TreeNode currNode = queue.poll();
-
-                if(currNode.left != null) queue.offer(currNode.left);
-                if(currNode.right != null) queue.offer(currNode.right);
-
-                currMax = Math.max(currMax, currNode.val);
-            }
-
-            largestValueList.add(currMax);
-        }
-
-        return largestValueList;
+        return largestValuesList;    
 
     }
+
+    private void dfs(TreeNode root, int currDepth, List<Integer> largestValuesList){
+        if(root == null) return;
+
+        if(currDepth >= largestValuesList.size()){
+            largestValuesList.add(root.val);
+        }
+        else{
+            largestValuesList.set(currDepth, Math.max(largestValuesList.get(currDepth), root.val));
+        }
+
+        dfs(root.left, currDepth + 1, largestValuesList);
+        dfs(root.right, currDepth + 1, largestValuesList);
+
+        // return currDepth
+    }
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 09 Aug 2026
+
+    // //intuition 1: BFS
+    //     //Run a level order traversal and find the largest value along the way
+    // public List<Integer> largestValues(TreeNode root) {
+        
+    //     List<Integer> largestValueList = new ArrayList<>();
+
+    //     if(root == null) return largestValueList;
+
+    //     Queue<TreeNode> queue = new ArrayDeque<>();
+    //     queue.offer(root);
+
+    //     while(!queue.isEmpty()){
+    //         int currQueueSize = queue.size();
+    //         int currMax = Integer.MIN_VALUE;
+    //         for(int i = 0; i < currQueueSize; i ++){
+    //             TreeNode currNode = queue.poll();
+
+    //             if(currNode.left != null) queue.offer(currNode.left);
+    //             if(currNode.right != null) queue.offer(currNode.right);
+
+    //             currMax = Math.max(currMax, currNode.val);
+    //         }
+
+    //         largestValueList.add(currMax);
+    //     }
+
+    //     return largestValueList;
+
+    // }
 }
