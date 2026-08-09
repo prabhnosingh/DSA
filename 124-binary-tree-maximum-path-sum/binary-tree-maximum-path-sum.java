d04b8bf211ff2396699881a23938be32d14702d2
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-09 09:03:41 -0400

    Time: 1 ms (33.34%) | Memory: 46.5 MB (90.55%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -17,39 +17,82 @@ class Solution {
 
     //Solving on 08 Aug 2026
 
-    //intuition 1: DFS
+    //intuition 2: DFS (without global var)
         //smaller problem - find maximum path sum in left and right subtrees
         //at each node we have two options:
             //either to split and have total sum as root.val + leftMax + rightMax
-                //and use this value to update the global max
-                //if leftMax or rightMax are negatives then we simply consider then as
-                    //zeros as we don't want to decrease the sum
+                //and use this value to update the max in an array
             //or, to not to split and have total sum as root.val + max(leftMax, rightMax)
                 //this will be the value that we return to the parent node 
+            //if leftMax or rightMax are negatives then we simply consider them as
+                //zeros as we don't want to decrease the sum
 
-            
-    private int maxPathSum = Integer.MIN_VALUE;
+        //TC: O(n) : where n is the total number of nodes in the tree
+        //SC: O(h) : where h is the height of the tree
+    // private int maxPathSum = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-    
-        int rootSum = treeTraversal(root);
-        // maxPathSum = Math.max(maxPathSum, rootSum);
+        
+        int[] maxPathSum = new int[1];
+        maxPathSum[0] = Integer.MIN_VALUE;
+        int rootSum = treeTraversal(root, maxPathSum);
+        maxPathSum[0] = Math.max(maxPathSum[0], rootSum);
 
-        return maxPathSum;
+        return maxPathSum[0];
     }
 
-    private int treeTraversal(TreeNode root){
+    private int treeTraversal(TreeNode root, int[] maxPathSum){
         
         if(root == null) return 0;
 
-        int leftMaxSum = Math.max(0, treeTraversal(root.left));
-        int rightMaxSum = Math.max(0, treeTraversal(root.right));
+        int leftMaxSum = Math.max(0, treeTraversal(root.left, maxPathSum));
+        int rightMaxSum = Math.max(0, treeTraversal(root.right, maxPathSum));
 
         //split scenario
-        maxPathSum = Math.max(maxPathSum, root.val + leftMaxSum + rightMaxSum);
+        maxPathSum[0] = Math.max(maxPathSum[0], root.val + leftMaxSum + rightMaxSum);
 
         //return value to the parent
         return root.val + Math.max(leftMaxSum, rightMaxSum);
 
-    }   
+    }  
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////// 
+    // //intuition 1: DFS
+    //     //smaller problem - find maximum path sum in left and right subtrees
+    //     //at each node we have two options:
+    //         //either to split and have total sum as root.val + leftMax + rightMax
+    //             //and use this value to update the global max
+    //         //or, to not to split and have total sum as root.val + max(leftMax, rightMax)
+    //             //this will be the value that we return to the parent node 
+    //         //if leftMax or rightMax are negatives then we simply consider them as
+    //             //zeros as we don't want to decrease the sum
+
+    //     //TC: O(n) : where n is the total number of nodes in the tree
+    //     //SC: O(h) : where h is the height of the tree
+    // private int maxPathSum = Integer.MIN_VALUE;
+    // public int maxPathSum(TreeNode root) {
+    
+    //     int rootSum = treeTraversal(root);
+    //     maxPathSum = Math.max(maxPathSum, rootSum);
+
+    //     return maxPathSum;
+    // }
+
+    // private int treeTraversal(TreeNode root){
+        
+    //     if(root == null) return 0;
+
+    //     int leftMaxSum = Math.max(0, treeTraversal(root.left));
+    //     int rightMaxSum = Math.max(0, treeTraversal(root.right));
+
+    //     //split scenario
+    //     maxPathSum = Math.max(maxPathSum, root.val + leftMaxSum + rightMaxSum);
+
+    //     //return value to the parent
+    //     return root.val + Math.max(leftMaxSum, rightMaxSum);
+
+    // }   
 
 }
