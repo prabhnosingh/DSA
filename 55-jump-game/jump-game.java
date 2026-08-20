2bb083938c0d7605ed7e898e4675cf883cc3e061
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-20 08:06:37 -0400

    Time: 3 ms (20.98%) | Memory: 47.7 MB (92.17%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,56 +1,127 @@
 class Solution {
+
+    //Solving on 20 Aug 2026
+
+    //intuition 1: 
+        //Topic: Greedy
+        //Pattern:
+        //Sub-pattern:
+        
+        //At each step we have 2 options,
+            //either to jump at full capacity 
+            //or to jump at less capacity
+        
+        //start from the last index and fill a boolean dp array towards first index 
+            //if a jump from index i can help us reach to last index, then any index
+                //j that can reach to index i can help us reach to last index 
+
+        //dp invariant:
+            //given any index i, dp[i] will store if we can reach last index from index i
     public boolean canJump(int[] nums) {
         
-        // int len = nums.length;
-        int goalPost = nums.length - 1;
+        boolean[] dp = new boolean[nums.length];
+        int numsLen = nums.length;
+        dp[numsLen - 1] = true;
 
-        for(int i = nums.length - 2; i >= 0; i--){
-
-            if(nums[i] >= goalPost - i){
-                goalPost = i;
+        int lastTrueIdx = numsLen - 1;
+        for(int i = numsLen - 2; i >= 0; i --){
+            if(nums[i] > 0 && (i + nums[i]) >= lastTrueIdx){ //if nums[i] is greater than 1 and 
+                //by adding nums[i] to i we can reach lastTruIdx then we can jump to last
+                //index in some way
+                lastTrueIdx = i;
+                dp[i] = true;
             }
+            else dp[i] = false;
         }
 
-        if(goalPost == 0){
-            return true;
-        }
-        return false;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     if(nums.length == 1 ){
-    //         return true;
-    //     }
-    //     for(int i = 0; i< nums.length; i++){
-            
-    //         if(i == 0){
-    //             n[i] = nums[i];
-
-    //         }
-    //         else{
-    //             n[i] = nums[i] + i;
-    //         }
-
-    //   }
-    //   for(int ans : n){
-    //       if(ans == nums.length - 1){
-    //           return true;
-    //       }
-
-    //   }
-    //   return false;
+        return dp[0];
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+    // public boolean canJump(int[] nums) {
+        
+    //     // int len = nums.length;
+    //     int goalPost = nums.length - 1;
+
+    //     for(int i = nums.length - 2; i >= 0; i--){
+
+    //         if(nums[i] >= goalPost - i){
+    //             goalPost = i;
+    //         }
+    //     }
+
+    //     if(goalPost == 0){
+    //         return true;
+    //     }
+    //     return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // //     if(nums.length == 1 ){
+    // //         return true;
+    // //     }
+    // //     for(int i = 0; i< nums.length; i++){
+            
+    // //         if(i == 0){
+    // //             n[i] = nums[i];
+
+    // //         }
+    // //         else{
+    // //             n[i] = nums[i] + i;
+    // //         }
+
+    // //   }
+    // //   for(int ans : n){
+    // //       if(ans == nums.length - 1){
+    // //           return true;
+    // //       }
+
+    // //   }
+    // //   return false;
+
+
+    // }
 }
