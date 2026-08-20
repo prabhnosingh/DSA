ac2d359db87f4211b8d9e6aaea58ab3dd8088ccd
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-08-20 08:10:45 -0400

    Time: 2 ms (88.93%) | Memory: 48 MB (24.11%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,7 +2,7 @@ class Solution {
 
     //Solving on 20 Aug 2026
 
-    //intuition 1: 
+    //intuition 2 (without dp aray): 
         //Topic: DP
         //Pattern:
         //Sub-pattern:
@@ -16,33 +16,76 @@ class Solution {
                 //j that can reach to index i can help us reach to last index 
             //track the last true index using a variable and see if current nums[i] + i
                 //is greater than equal to lastTrueIdx
+            //at last see if lastTrueIdx == 0
 
-        //dp invariant:
-            //given any index i, dp[i] will store if we can reach last index from index i
+       
 
         //TC: O(n)
-        //SC: O(n)
+        //SC: O(1)
     public boolean canJump(int[] nums) {
         
-        boolean[] dp = new boolean[nums.length];
         int numsLen = nums.length;
-        dp[numsLen - 1] = true;
 
         int lastTrueIdx = numsLen - 1;
+
         for(int i = numsLen - 2; i >= 0; i --){
             if(nums[i] > 0 && (i + nums[i]) >= lastTrueIdx){ //if nums[i] is greater than 1 and 
                 //by adding nums[i] to i we can reach lastTruIdx then we can jump to last
                 //index in some way
                 lastTrueIdx = i;
-                dp[i] = true;
             }
-            else dp[i] = false;
         }
 
         return lastTrueIdx == 0 ? true : false;
 
 
     }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+    // //Solving on 20 Aug 2026
+
+    // //intuition 1: 
+    //     //Topic: DP
+    //     //Pattern:
+    //     //Sub-pattern:
+        
+    //     //At each step we have 2 options,
+    //         //either to jump at full capacity 
+    //         //or to jump at less capacity
+        
+    //     //start from the last index and fill a boolean dp array towards first index 
+    //         //if a jump from index i can help us reach to last index, then any index
+    //             //j that can reach to index i can help us reach to last index 
+    //         //track the last true index using a variable and see if current nums[i] + i
+    //             //is greater than equal to lastTrueIdx
+
+    //     //dp invariant:
+    //         //given any index i, dp[i] will store if we can reach last index from index i
+
+    //     //TC: O(n)
+    //     //SC: O(n)
+    // public boolean canJump(int[] nums) {
+        
+    //     boolean[] dp = new boolean[nums.length];
+    //     int numsLen = nums.length;
+    //     dp[numsLen - 1] = true;
+
+    //     int lastTrueIdx = numsLen - 1;
+    //     for(int i = numsLen - 2; i >= 0; i --){
+    //         if(nums[i] > 0 && (i + nums[i]) >= lastTrueIdx){ //if nums[i] is greater than 1 and 
+    //             //by adding nums[i] to i we can reach lastTruIdx then we can jump to last
+    //             //index in some way
+    //             lastTrueIdx = i;
+    //             dp[i] = true;
+    //         }
+    //         else dp[i] = false;
+    //     }
+
+    //     return dp[0];
+
+
+    // }
 
 
 
