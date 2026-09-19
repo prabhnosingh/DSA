cf5a5429611af612ea8f92562c96631e777f0598
Prabhnoor Singh <71306744+prabhnosingh@users.noreply.github.com>
2026-09-18 23:27:27 -0400

    Time: 2 ms (77.10%) | Memory: 50.2 MB (56.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,54 +1,68 @@
 class Solution {
 
-
-
-    //Re-solving on 21 Feb 2026:
-    //intuition 1: 2D DP: DP on grids
-        //for any non-boundary we have to reach bottom right, by either going right or
-            //by either going down.
-        //We will take the minimum of dp states of bottom and right cells and add it to
-            //current cells value
-        //Recurrence relation:
-            //dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+    //Re-solving on 18 Sept 2026
+    //intuition 1: Recursion (top-down + memoization)
+        //starting from 0,0 we have two options, either to move right or to move down, we can run
+            //recursive call to find which is minimum path to reach bottom path and proceed with 
+            //that one
         
-        //Base case:
-            //There is only one option to reach bottom right from last col and last row,
-                //i.e. by going down and right respectively. Therefore, add the numbers from
-                //bottom to top for last col and right to left for last row
+        //this looks like a dp problem. why dp?
+            //1. each state can be computed from 2 sub-states
+            //2. each state can have multiple overlapping paths, resulting in repeated computation
+                //of paths. Therefore, storing paths from each cell is helpful to avoid re-computation
+
+        //dp invariant:
+            //recurse(i,j) will represent minimum path to reach bottom right form i, j while only travelling 
+                //right and down
+            
+            //recurse(0, 0) will represent minimum path to reach bottom right from 0, 0
+
+
+        //recurrence relation:
+            //each state will depend on two sub-states with below relation
+
+            //recurse(i, j) = grid[i][j] + Math.min(recurse(i + 1, j), recurse(i, j + 1))
+
+
+        //base case:
+            //recurse(m-1, n-1) = grid[m-1][n-1]
+
+        //memoization:
+            //as each state is represented by two coordinates (i and j), therefore, we can
+                //have a 2D array to store the values of each state
+
+
+        //TC without memoization: O(2^ (m + n))
+        //TC with memoization: O(m x n)
+
+        //SC without memoization: O(log (m + n)) for recursive stack
+        //SC with memoization: O(m x n) + O(log (m + n))
 
     public int minPathSum(int[][] grid) {
-        //dp[i][j] will represent minimum sum of the path from i, j to bottom right
-        //dp[0][0] will represent minimum sum of the path from 0, 0 to bottom right
-        //Therefore, we need a 2D DP matrix of size rows x cols
 
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int[][] dp = new int[grid.length][grid[0].length];
 
-        int[][] dp = new int[rows][cols];
-
-        //base cases
-        dp[rows - 1][cols - 1] = grid[rows - 1][cols - 1];
-
-        //filling last row
-        for(int j = cols - 2; j >= 0; j --){
-            int i = rows - 1;
-            dp[i][j] = grid[i][j] + dp[i][j + 1];
-        }
-
-        //filling last col
-        for(int i = rows - 2; i >= 0; i --){
-            int j = cols - 1;
-            dp[i][j] = grid[i][j] + dp[i + 1][j];
-        }
-
-        for(int i = rows-2; i >= 0; i --){
-            for(int j = cols-2; j >= 0; j --){
-                dp[i][j] = Math.min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+        for(int i = 0; i < grid.length; i ++){
+            for(int j = 0; j < grid[0].length; j ++){
+                dp[i][j] = -1;
             }
         }
 
-        return dp[0][0];
+        return recurse(grid, 0, 0, dp);
 
+    }
+
+    private int recurse (int[][] grid, int i, int j, int[][] dp){
+
+        if(i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
+
+        if(i == grid.length - 1 && j == grid[0].length - 1) return grid[i][j];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        dp[i][j] = grid[i][j] + Math.min(recurse(grid, i + 1, j, dp), recurse(grid, i, j + 1, dp));
+
+        return dp[i][j];
 
     }
 
@@ -80,6 +94,83 @@ class Solution {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Re-solving on 21 Feb 2026:
+//     //intuition 1: 2D DP: DP on grids
+//         //for any non-boundary we have to reach bottom right, by either going right or
+//             //by either going down.
+//         //We will take the minimum of dp states of bottom and right cells and add it to
+//             //current cells value
+//         //Recurrence relation:
+//             //dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+        
+//         //Base case:
+//             //There is only one option to reach bottom right from last col and last row,
+//                 //i.e. by going down and right respectively. Therefore, add the numbers from
+//                 //bottom to top for last col and right to left for last row
+
+//     public int minPathSum(int[][] grid) {
+//         //dp[i][j] will represent minimum sum of the path from i, j to bottom right
+//         //dp[0][0] will represent minimum sum of the path from 0, 0 to bottom right
+//         //Maximum index that we will be referring to will be rows-1, cols-1. Therefore,
+//             //we need a 2D DP matrix of size rows x cols
+
+//         int rows = grid.length;
+//         int cols = grid[0].length;
+
+//         int[][] dp = new int[rows][cols];
+
+//         //base cases
+//         dp[rows - 1][cols - 1] = grid[rows - 1][cols - 1];
+
+//         //filling last row
+//         for(int j = cols - 2; j >= 0; j --){
+//             int i = rows - 1;
+//             dp[i][j] = grid[i][j] + dp[i][j + 1];
+//         }
+
+//         //filling last col
+//         for(int i = rows - 2; i >= 0; i --){
+//             int j = cols - 1;
+//             dp[i][j] = grid[i][j] + dp[i + 1][j];
+//         }
+
+//         for(int i = rows-2; i >= 0; i --){
+//             for(int j = cols-2; j >= 0; j --){
+//                 dp[i][j] = Math.min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //     //Solving on 17 Dec 2025:
 
 //     //intuition 1: 2D DP : Bottom up Tabulation : DP on grid pattern : Answer at 0,0
